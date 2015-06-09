00309853b421c75c7af106342ad8295dead38840
stalcup <stalcup@google.com>
2015-06-09 10:14:24 -0700

    Adds a command to transpile an example in debug mode.
    PiperOrigin-RevId: 95546887

---
+++ transpiler/javatests/com/google/j2cl/transpiler/readable/debug_transpile.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Copyright 2015 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License. You may
+# obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# Runs the transpiler in debug mode.
+#
+# Launch the script directly, not through Blaze.
+
+java_dir="third_party/java_src/j2cl/transpiler/javatests"
+examples_dir="$java_dir/com/google/j2cl/transpiler/readable/"
+transpiler_bin="blaze-bin/third_party/java_src/j2cl/j2cl"
+example_name=$1
+
+# Show commands as they occur
+set -x
+
+# Build the transpiler
+blaze build third_party/java_src/j2cl:j2cl &> /dev/null
+
+# Figure out where the referenced example lives
+example_dir=$examples_dir$example_name
+
+# Accumulate java files in this example and subdirs
+example_files=""
+for java_file in $(find $example_dir -name '*.java') ; do
+  example_files="$example_files$java_file "
+done
+
+# Transpile and debug it
+$transpiler_bin --debug -d /tmp/gen/$example_name $example_files
