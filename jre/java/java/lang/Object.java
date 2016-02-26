ebbfd48e3dcc6223e01ee07b5c32aa7b88aa81f6
goktug <goktug@google.com>
2016-02-25 18:32:31 -0800

    Make Object#getClass final.
    PiperOrigin-RevId: 115628607

--- jre/java/java/lang/Object.java
+++ jre/java/java/lang/Object.java
@@ -27,8 +27,7 @@ public class Object {
     return false;
   }
 
-  //TODO(dankurka): this method should actually be final, but because of GWT's String class it can not
-  public Class<?> getClass() {
+  public final Class<?> getClass() {
     // Super-source replaced.
     return null;
   }
