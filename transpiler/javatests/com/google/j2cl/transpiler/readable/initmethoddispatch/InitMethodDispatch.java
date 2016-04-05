73d9b4ef053e84b88b745fb1b35e599458a7bb31
stalcup <stalcup@google.com>
2016-04-05 16:26:18 -0700

    Adds tiny readable ex. for omitting $init() in some cascading ctors.
    PiperOrigin-RevId: 119104496

---
+++ transpiler/javatests/com/google/j2cl/transpiler/readable/initmethoddispatch/InitMethodDispatch.java
@@ -0,0 +1,16 @@
+package com.google.j2cl.transpiler.readable.initmethoddispatch;
+
+class InitMethodDispatch {
+  int value;
+
+  InitMethodDispatch() {
+    this(1);
+    // This will translate to a $ctor method that *DOES NOT* dispatch to $init() since doing so
+    // would be redundant (in light of the fact that it delegates to this()).
+  }
+
+  InitMethodDispatch(int i) {
+    value = i;
+    // This will translate to a $ctor method that *does* dispatch to $init().
+  }
+}
