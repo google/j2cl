e5dbaa7ed91afbb52f162e10dcfeb6db63915704
stalcup <stalcup@google.com>
2016-04-27 16:12:27 -0700

    Adding a regression test for naming of nested types inside native types.
    PiperOrigin-RevId: 120966050

---
+++ transpiler/javatests/com/google/j2cl/transpiler/integration/nestednativetype/Main.java
@@ -0,0 +1,16 @@
+package com.google.j2cl.transpiler.integration.nestednativetype;
+
+public class Main {
+
+  /**
+   * This proves there is no error by showing that no JSCompiler compile error occurred. If we
+   * improperly create the qualified names of types nested inside of native types then we will have
+   * created a name collision.
+   */
+  public static void main(String... args) {
+    NativeArray nativeArray = new NativeArray();
+    NativeArray2 nativeArray2 = new NativeArray2();
+
+    assert(nativeArray.getObject() != nativeArray2.getObject());
+  }
+}
---
+++ transpiler/javatests/com/google/j2cl/transpiler/integration/nestednativetype/NativeArray2.java
@@ -0,0 +1,13 @@
+package com.google.j2cl.transpiler.integration.nestednativetype;
+
+import jsinterop.annotations.JsOverlay;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+
+@JsType(isNative = true, name = "Array", namespace = JsPackage.GLOBAL)
+public final class NativeArray2 {
+  @JsOverlay
+  public final Object getObject() {
+    return new Object() {};
+  }
+}
