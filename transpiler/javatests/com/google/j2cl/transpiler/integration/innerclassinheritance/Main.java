3119571b5de51bb47e260e141e2bed403c951fbe
rluble <rluble@google.com>
2016-05-09 15:27:02 -0700

    Enable previously disabled integration test.
    PiperOrigin-RevId: 121884012

--- transpiler/javatests/com/google/j2cl/transpiler/integration/innerclassinheritance/Main.java
+++ transpiler/javatests/com/google/j2cl/transpiler/integration/innerclassinheritance/Main.java
@@ -1,29 +1,25 @@
 package com.google.j2cl.transpiler.integration.innerclassinheritance;
 
-/**
- * TODO: uncomment when class name collisions are supported.
- */
 public class Main {
   public static void main(String[] args) {
-    //    com.google.j2cl.transpiler.integration.innerclassinheritance.p1.A a =
-    //        new com.google.j2cl.transpiler.integration.innerclassinheritance.p1.A();
-    //    com.google.j2cl.transpiler.integration.innerclassinheritance.p1.A aa =
-    //        new com.google.j2cl.transpiler.integration.innerclassinheritance.p2.A();
-    //    assert (a.new B() instanceof
-    //        com.google.j2cl.transpiler.integration.innerclassinheritance.p1.A.B);
-    //    // TODO: new inner class is resolved statically, so aa.new B() should be instanceof
-    // p1.A.B,
-    //    // but not p2.A.B. what we are doing now is dynamically dispatched, so this may be a bug
+    com.google.j2cl.transpiler.integration.innerclassinheritance.p1.A a =
+        new com.google.j2cl.transpiler.integration.innerclassinheritance.p1.A();
+    com.google.j2cl.transpiler.integration.innerclassinheritance.p1.A aa =
+        new com.google.j2cl.transpiler.integration.innerclassinheritance.p2.A();
+    assert (a.new B()
+        instanceof com.google.j2cl.transpiler.integration.innerclassinheritance.p1.A.B);
+
+    assert (aa.new B()
+        instanceof com.google.j2cl.transpiler.integration.innerclassinheritance.p1.A.B);
+
+    // TODO: new inner class is resolved statically, so aa.new B() should be instanceof p1.A.B,
+    // but not p2.A.B. what we are doing now is dynamically dispatched, so this may be a bug
     // here.
-    //    assert (aa.new B() instanceof
-    //        com.google.j2cl.transpiler.integration.innerclassinheritance.p1.A.B);
-    //    assert !(aa.new B() instanceof
-    //        com.google.j2cl.transpiler.integration.innerclassinheritance.p2.A.B);
-    //    assert (new com.google.j2cl.transpiler.integration.innerclassinheritance.p2.A().new B()
-    // instanceof
-    //        com.google.j2cl.transpiler.integration.innerclassinheritance.p2.A.B);
-    //    assert (new com.google.j2cl.transpiler.integration.innerclassinheritance.p2.A().new C()
-    // instanceof
-    //        com.google.j2cl.transpiler.integration.innerclassinheritance.p1.A.C);
+    //assert !(aa.new B() instanceof
+    //    com.google.j2cl.transpiler.integration.innerclassinheritance.p2.A.B);
+    assert (new com.google.j2cl.transpiler.integration.innerclassinheritance.p2.A().new B()
+        instanceof com.google.j2cl.transpiler.integration.innerclassinheritance.p2.A.B);
+    assert (new com.google.j2cl.transpiler.integration.innerclassinheritance.p2.A().new C()
+        instanceof com.google.j2cl.transpiler.integration.innerclassinheritance.p1.A.C);
   }
 }
--- transpiler/javatests/com/google/j2cl/transpiler/integration/innerclassinheritance/p2/A.java
+++ transpiler/javatests/com/google/j2cl/transpiler/integration/innerclassinheritance/p2/A.java
@@ -1,7 +1,5 @@
 package com.google.j2cl.transpiler.integration.innerclassinheritance.p2;
 
-// TODO: uncomment when name collision is supported.
-//public class A extends com.google.j2cl.transpiler.integration.innerclassinheritance.p1.A {
-//  public class B extends com.google.j2cl.transpiler.integration.innerclassinheritance.p1.A.B {}
-//}
-public class A {}
+public class A extends com.google.j2cl.transpiler.integration.innerclassinheritance.p1.A {
+  public class B extends com.google.j2cl.transpiler.integration.innerclassinheritance.p1.A.B {}
+}
