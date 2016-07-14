559ce91e239247f85dcd180d396d8a722d5843ba
rluble <rluble@google.com>
2016-07-14 14:39:13 -0700

    Import: [2 changes from Gerrit]
    commit ca8ad7bc140bc23b237620c41d755ef787bd97b1
    Author: Roberto Lublinerman <rluble@google.com>
    Date:   Fri Jun 17 17:56:50 2016 -0700
    
        Do not honor JsNames if -generateJsInteropExports is not set.
    
        Members that were not exported due to missing -generateJsInteropExports
        were still being named according to their JsName, which allowed direct access
        through the name but did not prevent optimizing them away. Using not exported names
        would seem to work when run in -draft or SDM but would break in optimized mode, making
        it time consuming to detect these errors.
    
        After this patch only actually exported members get their JsNames making it a more
        consistent behaviour between draft and optimized compiles.
    
        Bug: #9369
        Bug-Link: http://github.com/gwtproject/gwt/issues/9369
        Change-Id: I0c2340db319ceb307a973a7c997d499b95125e20
    
    commit e1fcefa15e96a5d7450d9f874101226a72bbeefb
    Author: Goktug Gokdogan <goktug@google.com>
    Date:   Tue Jul 12 01:50:16 2016 -0700
    
        Makes Throwable.backingJsObject to not rely on exports.
    
        Change-Id: Iae5997b7bd49b205aa40e6c9999cdef2024ad535
    
    PiperOrigin-RevId: 127475065

--- jre/javatests/com/google/gwt/emultest/java/lang/JsExceptionViolator.native.js
+++ jre/javatests/com/google/gwt/emultest/java/lang/JsExceptionViolator.native.js
@@ -9,3 +9,13 @@ JsExceptionViolator.m_createJsException__java_lang_Object = function(wrapped) {
   JsExceptionViolator.$clinit();
   return JsException.$create__java_lang_Object(wrapped);
 };
+
+/**
+ * @param {Throwable} throwable
+ * @return {*}
+ * @public
+ */
+JsExceptionViolator.m_getBackingJsObject__java_lang_Throwable = function(throwable) {
+  JsExceptionViolator.$clinit();
+  return throwable.backingJsObject;
+};
