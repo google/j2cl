633e0fcc130ceb952b7839771dc585baf4e34e5a
rluble <rluble@google.com>
2016-08-01 15:20:53 -0700

    Automated g4 rollback of changelist 129031307.
    *** Reason for rollback ***
    
    The cl was only partially committed.
    
    *** Original change description ***
    
    Stop aliasing externs.
    
    Native JsTypes in JsPackage.GLOBAL namespace are considered externs, so they are not goog.required, instead they are directly aliased by a let definition.
    
    This approach does not work well if the extern was a @typedef and not a @constructor.
    
    This CL stops aliasing externs and instead locally renames definitions that would be conflicting with the externs.
    
    ***
    
    PiperOrigin-RevId: 129033392

---
+++ transpiler/java/com/google/j2cl/generator/visitors/ImportUtils.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.j2cl.generator.visitors;
+
+import com.google.common.collect.Lists;
+import com.google.j2cl.generator.visitors.ImportGatheringVisitor.ImportCategory;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Utility functions for Import.
+ */
+public class ImportUtils {
+  @SuppressWarnings("unchecked")
+  public static List<Import> getSortedImports(Map<ImportCategory, Set<Import>> importsByCategory) {
+    return sortedList(
+        union(
+            importsByCategory.get(ImportCategory.EAGER),
+            importsByCategory.get(ImportCategory.LAZY),
+            importsByCategory.get(ImportCategory.EXTERN),
+            importsByCategory.get(ImportCategory.SELF)));
+  }
+
+  public static <T extends Comparable<T>> List<T> sortedList(Set<T> set) {
+    List<T> sortedList = Lists.newArrayList(set);
+    Collections.sort(sortedList);
+    return sortedList;
+  }
+
+  @SuppressWarnings("unchecked")
+  public static <T> Set<T> union(Set<T>... elements) {
+    HashSet<T> union = new HashSet<>();
+    for (Set<T> element : elements) {
+      union.addAll(element);
+    }
+    return union;
+  }
+}
