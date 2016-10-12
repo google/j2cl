2582aa6c3762a3beaf4c76ac65b306895e09f238
goktug <goktug@google.com>
2016-10-12 11:58:54 -0700

    Beef up the multi-catch test
    PiperOrigin-RevId: 135944276

--- transpiler/javatests/com/google/j2cl/transpiler/integration/multiplecatch/Main.java
+++ transpiler/javatests/com/google/j2cl/transpiler/integration/multiplecatch/Main.java
@@ -6,16 +6,27 @@ package com.google.j2cl.transpiler.integration.multiplecatch;
 public class Main {
   public static void main(String... args) {
     try {
-      throwRuntimeException(); // So that the following assert isn't considered dead code.
+      throwRuntimeException(); // So assert isn't considered dead code by javac.
       assert false; // should have skipped past this with an exception.
     } catch (NullPointerException | ClassCastException e) {
       assert false; // The wrong exception type was caught.
     } catch (RuntimeException r) {
       // expected
     }
+
+    try {
+      throwIllegalArgumentException(); // So assert isn't considered dead code by javac.
+      assert false; // should have skipped past this with an exception.
+    } catch (NullPointerException | IllegalArgumentException e) {
+      // expected
+    }
   }
 
-  public static void throwRuntimeException() throws RuntimeException {
+  public static void throwRuntimeException() {
     throw new RuntimeException();
+  }
+
+  public static void throwIllegalArgumentException() {
+    throw new IllegalArgumentException();
   }
 }
