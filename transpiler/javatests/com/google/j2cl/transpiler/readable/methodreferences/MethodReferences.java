b741a51f1eae98ff1c540b41b059093ad8b149c7
rluble <rluble@google.com>
2016-11-02 18:40:26 -0700

    Correctly ignore method references until they are implemented.
    There are 4 different flavors of method references and the previous logic only handled 2 of them.
    
    PiperOrigin-RevId: 138022757

--- transpiler/javatests/com/google/j2cl/transpiler/readable/methodreferences/MethodReferences.java
+++ transpiler/javatests/com/google/j2cl/transpiler/readable/methodreferences/MethodReferences.java
@@ -1,16 +1,33 @@
 package com.google.j2cl.transpiler.readable.methodreferences;
 
 public class MethodReferences {
+
   interface Producer<T> {
     T produce();
+  }
+
+  interface Function<T, V> {
+    T apply(V v);
   }
 
   static Object m() {
     return new Object();
   }
 
-  void main() {
-    Producer<Object> objectFactory = Object::new;
-    objectFactory = MethodReferences::m;
+  public class Inner {
+
+    Object n() {
+      return new Object();
+    }
+
+    <T extends Comparable> void main() {
+      Producer<Object> objectFactory = Object::new;
+      objectFactory = MethodReferences::m;
+      objectFactory = super::toString;
+      objectFactory = this::n;
+      objectFactory = MethodReferences.this::toString;
+      objectFactory = MethodReferences.super::toString;
+      objectFactory = "Hi"::toString;
+    }
   }
 }
