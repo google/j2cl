14f5eda980460c3b094c93e8b80abc5e94f61aac
rluble <rluble@google.com>
2016-12-08 16:27:44 -0800

    Remove type variable references inside anonymous (arrow) functions.
    Closure template variables declared in the class or the enclosing method are not available inside the body of anonymous (arrow) functions.
    
    This CL expands the current fixup, which removed removed references to template variables declared by methods in the method body because of closure restrictions, to handle anonymous functions as well.
    
    PiperOrigin-RevId: 141499068

--- transpiler/java/com/google/j2cl/ast/visitors/FixTypeVariablesInMethods.java
+++ transpiler/java/com/google/j2cl/ast/visitors/FixTypeVariablesInMethods.java
@@ -17,9 +17,13 @@ package com.google.j2cl.ast.visitors;
 
 import static com.google.common.base.Preconditions.checkState;
 
+import com.google.common.base.Predicates;
 import com.google.common.collect.Lists;
 import com.google.j2cl.ast.AbstractRewriter;
+import com.google.j2cl.ast.AbstractVisitor;
 import com.google.j2cl.ast.CompilationUnit;
+import com.google.j2cl.ast.Expression;
+import com.google.j2cl.ast.FunctionExpression;
 import com.google.j2cl.ast.JsDocAnnotatedExpression;
 import com.google.j2cl.ast.Member;
 import com.google.j2cl.ast.Method;
@@ -27,41 +31,75 @@ import com.google.j2cl.ast.MethodDescriptor;
 import com.google.j2cl.ast.Node;
 import com.google.j2cl.ast.TypeDescriptor;
 import com.google.j2cl.ast.TypeDescriptors;
+import java.util.function.Predicate;
 
 /**
  * Temporary workaround for b/24476009.
  *
- * <p>Template declared in a method is not resolved inside the method by JSCompiler. To get rid of
- * the warnings, this class replaces cast to a type variable that is declared by a method with cast
- * to its bound.
+ * <p>1) Template variables declared on a method do not resolve inside the method body. Code inside
+ * the method body can only refer explicitly to template variables declared in the class
+ * declaration.
+ *
+ * <p>2) Template variables do not resolve within the body of anonymous functions.
+ *
+ * <p>To avoid warnings by JsCompiler this pass replaces these "unreferenceable" template variables
+ * by their bound. Note that the AST construct of interest is {@link JsDocAnnotatedExpression}
+ * because this is the only one that can result in a template variable reference in the output.
  */
 public class FixTypeVariablesInMethods extends NormalizationPass {
   @Override
   public void applyTo(CompilationUnit compilationUnit) {
-    compilationUnit.accept(new Rewriter());
+    // Rewrite method bodies to avoid references to template variables declared by the method (1).
+    compilationUnit.accept(
+        new AbstractRewriter() {
+          @Override
+          public Node rewriteJsDocAnnotatedExpression(JsDocAnnotatedExpression annotation) {
+            if (annotation.isDeclaration() || !getCurrentMember().isMethod()) {
+              return annotation;
+            }
+            TypeDescriptor castTypeDescriptor = annotation.getTypeDescriptor();
+            TypeDescriptor boundType =
+                replaceTypeVariableWithBound(
+                    castTypeDescriptor,
+                    typeDescriptor ->
+                        isTypeVariableDeclaredByMember(typeDescriptor, getCurrentMember()));
+            return JsDocAnnotatedExpression.newBuilder()
+                .setExpression(annotation.getExpression())
+                .setAnnotationType(boundType)
+                .build();
+          }
+        });
+
+    // Rewrite method bodies of anonymous functions to avoid references to template variables (2).
+    compilationUnit.accept(
+        new AbstractVisitor() {
+          @Override
+          public void exitFunctionExpression(FunctionExpression functionExpression) {
+            removeAllTypeVariables(functionExpression);
+          }
+        });
   }
 
-  private class Rewriter extends AbstractRewriter {
-    @Override
-    public Node rewriteJsDocAnnotatedExpression(JsDocAnnotatedExpression annotation) {
-      if (annotation.isDeclaration() || !getCurrentMember().isMethod()) {
-        return annotation;
-      }
-      TypeDescriptor castTypeDescriptor = annotation.getTypeDescriptor();
-      TypeDescriptor boundType =
-          replaceTypeVariableWithBound(castTypeDescriptor, (Method) getCurrentMember());
-      return JsDocAnnotatedExpression.newBuilder()
-          .setExpression(annotation.getExpression())
-          .setAnnotationType(boundType)
-          .build();
-    }
-
+  private void removeAllTypeVariables(Expression functionExpression) {
+    functionExpression.accept(
+        new AbstractRewriter() {
+          @Override
+          public Node rewriteJsDocAnnotatedExpression(JsDocAnnotatedExpression annotation) {
+            TypeDescriptor castTypeDescriptor = annotation.getTypeDescriptor();
+            TypeDescriptor boundType =
+                replaceTypeVariableWithBound(castTypeDescriptor, Predicates.alwaysTrue());
+            return JsDocAnnotatedExpression.newBuilder()
+                .setExpression(annotation.getExpression())
+                .setAnnotationType(boundType)
+                .build();
+          }
+        });
   }
 
   private TypeDescriptor replaceTypeVariableWithBound(
-      TypeDescriptor typeDescriptor, final Method method) {
+      TypeDescriptor typeDescriptor, Predicate<TypeDescriptor> shouldBeReplaced) {
     // If it is a type variable that is declared by the method, replace with its bound.
-    if (isTypeVariableDeclaredByMethod(typeDescriptor, method)) {
+    if (shouldBeReplaced.test(typeDescriptor)) {
       TypeDescriptor boundTypeDescriptor = typeDescriptor.getRawTypeDescriptor();
       return boundTypeDescriptor.isParameterizedType()
           ? boundTypeDescriptor.getRawTypeDescriptor()
@@ -70,10 +108,8 @@ public class FixTypeVariablesInMethods extends NormalizationPass {
     // If it is a JsFunction type, and the JsFunction method contains type variables, we have to
     // replace it with 'window.Function' to get rid of 'bad type annotation' error.
     if (typeDescriptor.isJsFunctionImplementation() || typeDescriptor.isJsFunctionInterface()) {
-      if (containsTypeVariableDeclaredByMethodInJsFunction(typeDescriptor, method)) {
-        // Match the nullability of the starting type descriptor.
-        return TypeDescriptors.toGivenNullability(
-            TypeDescriptors.NATIVE_FUNCTION, typeDescriptor.isNullable());
+      if (containsTypeVariableDeclaredByMethodInJsFunction(typeDescriptor, shouldBeReplaced)) {
+        return TypeDescriptors.NATIVE_FUNCTION;
       }
       return typeDescriptor;
     }
@@ -82,18 +118,19 @@ public class FixTypeVariablesInMethods extends NormalizationPass {
           typeDescriptor,
           Lists.transform(
               typeDescriptor.getTypeArgumentDescriptors(),
-              typeArgument -> replaceTypeVariableWithBound(typeArgument, method)));
+              typeArgument -> replaceTypeVariableWithBound(typeArgument, shouldBeReplaced)));
     }
     if (typeDescriptor.isArray()) {
       TypeDescriptor boundTypeDescriptor =
-          replaceTypeVariableWithBound(typeDescriptor.getLeafTypeDescriptor(), method);
+          replaceTypeVariableWithBound(typeDescriptor.getLeafTypeDescriptor(), shouldBeReplaced);
       return TypeDescriptors.getForArray(
           boundTypeDescriptor, typeDescriptor.getDimensions(), typeDescriptor.isNullable());
     }
     return typeDescriptor;
   }
 
-  private boolean isTypeVariableDeclaredByMethod(TypeDescriptor typeDescriptor, Member member) {
+  private static boolean isTypeVariableDeclaredByMember(
+      TypeDescriptor typeDescriptor, Member member) {
     // A JsFunction method uses @this tag to specify the type of 'this', which makes templates
     // unresolved inside the method.
     // TODO: double check if this is the same issue with b/24476009.
@@ -112,27 +149,28 @@ public class FixTypeVariablesInMethods extends NormalizationPass {
    * example, Foo<Bar<T>> contains type variable 'T'.
    */
   private boolean containsTypeVariableDeclaredByMethod(
-      TypeDescriptor typeDescriptor, Method method) {
-    if (isTypeVariableDeclaredByMethod(typeDescriptor, method)) {
+      TypeDescriptor typeDescriptor, Predicate<TypeDescriptor> shouldBeReplaced) {
+    if (shouldBeReplaced.test(typeDescriptor)) {
       return true;
     }
     if (typeDescriptor.isParameterizedType()) {
       for (TypeDescriptor typeArgumentTypeDescriptor :
           typeDescriptor.getTypeArgumentDescriptors()) {
-        if (containsTypeVariableDeclaredByMethod(typeArgumentTypeDescriptor, method)) {
+        if (containsTypeVariableDeclaredByMethod(typeArgumentTypeDescriptor, shouldBeReplaced)) {
           return true;
         }
       }
       return false;
     }
     if (typeDescriptor.isArray()) {
-      return containsTypeVariableDeclaredByMethod(typeDescriptor.getLeafTypeDescriptor(), method);
+      return containsTypeVariableDeclaredByMethod(
+          typeDescriptor.getLeafTypeDescriptor(), shouldBeReplaced);
     }
     return false;
   }
 
   private boolean containsTypeVariableDeclaredByMethodInJsFunction(
-      TypeDescriptor typeDescriptor, Method method) {
+      TypeDescriptor typeDescriptor, Predicate<TypeDescriptor> shouldBeReplaced) {
     checkState(
         typeDescriptor.isJsFunctionImplementation() || typeDescriptor.isJsFunctionInterface());
     MethodDescriptor jsFunctionMethodDescriptor =
@@ -140,12 +178,12 @@ public class FixTypeVariablesInMethods extends NormalizationPass {
     if (jsFunctionMethodDescriptor != null) {
       for (TypeDescriptor parameterTypeDescriptor :
           jsFunctionMethodDescriptor.getParameterTypeDescriptors()) {
-        if (containsTypeVariableDeclaredByMethod(parameterTypeDescriptor, method)) {
+        if (containsTypeVariableDeclaredByMethod(parameterTypeDescriptor, shouldBeReplaced)) {
           return true;
         }
       }
       return containsTypeVariableDeclaredByMethod(
-          jsFunctionMethodDescriptor.getReturnTypeDescriptor(), method);
+          jsFunctionMethodDescriptor.getReturnTypeDescriptor(), shouldBeReplaced);
     }
     return false;
   }
