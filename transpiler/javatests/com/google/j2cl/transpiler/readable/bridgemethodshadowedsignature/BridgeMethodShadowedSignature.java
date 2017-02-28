8c20ec5d606b78e91c1e3a1b1f5fc2a54990df65
stalcup <stalcup@google.com>
2017-02-28 14:02:07 -0800

    Adds a repro case for a missing bridge method.
    PiperOrigin-RevId: 148812786

---
+++ transpiler/javatests/com/google/j2cl/transpiler/readable/bridgemethodshadowedsignature/BridgeMethodShadowedSignature.java
@@ -0,0 +1,19 @@
+package com.google.j2cl.transpiler.readable.bridgemethodshadowedsignature;
+
+import java.util.function.Consumer;
+
+interface I<I1> {
+  String get(Consumer<? super I1> consumer);
+}
+
+abstract class B<B1, B2> implements I<B1> {
+  public String get(B2 consumer) {
+    return "B get B2";
+  }
+}
+
+class C<C1> extends B<C1, Consumer<? super C1>> implements I<C1> {
+  //Needs a bridge from the I.get(Consumer) signature to B.get(Object) implementation.
+}
+
+public class BridgeMethodShadowedSignature {}
