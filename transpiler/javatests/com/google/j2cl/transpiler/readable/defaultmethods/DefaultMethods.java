79a90265509fbe1ec158994137c2369578da4a56
rluble <rluble@google.com>
2017-03-16 17:46:44 -0700

    Add example for bad templates in default method construction.
    PiperOrigin-RevId: 150393466

--- transpiler/javatests/com/google/j2cl/transpiler/readable/defaultmethods/DefaultMethods.java
+++ transpiler/javatests/com/google/j2cl/transpiler/readable/defaultmethods/DefaultMethods.java
@@ -3,6 +3,7 @@ package com.google.j2cl.transpiler.readable.defaultmethods;
 public class DefaultMethods {
   interface List<T> {
     default void add(T elem) {}
+    default void remove(T[] elems) {}
   }
 
   static class AConcreteList<T> implements List<T> {}
