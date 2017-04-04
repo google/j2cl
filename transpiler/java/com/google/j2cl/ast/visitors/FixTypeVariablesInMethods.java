19f97dde84c43e4c57f5accbadb69f7fc33d11e7
rluble <rluble@google.com>
2017-04-03 21:59:49 -0700

    Small cleanup in FixTypeVariablesInMethods.
    PiperOrigin-RevId: 152096387

--- transpiler/java/com/google/j2cl/ast/visitors/FixTypeVariablesInMethods.java
+++ transpiler/java/com/google/j2cl/ast/visitors/FixTypeVariablesInMethods.java
@@ -26,11 +26,11 @@ import com.google.j2cl.ast.CompilationUnit;
 import com.google.j2cl.ast.Expression;
 import com.google.j2cl.ast.FunctionExpression;
 import com.google.j2cl.ast.JsDocAnnotatedExpression;
-import com.google.j2cl.ast.Member;
 import com.google.j2cl.ast.Method;
 import com.google.j2cl.ast.MethodDescriptor;
 import com.google.j2cl.ast.MultiExpression;
 import com.google.j2cl.ast.Node;
+import com.google.j2cl.ast.Type;
 import com.google.j2cl.ast.TypeDescriptor;
 import com.google.j2cl.ast.TypeDescriptors;
 import com.google.j2cl.ast.Variable;
@@ -57,53 +57,18 @@ public class FixTypeVariablesInMethods extends NormalizationPass {
   @Override
   public void applyTo(CompilationUnit compilationUnit) {
     // Rewrite method bodies to avoid references to template variables declared by the method (1).
-    compilationUnit.accept(
-        new AbstractRewriter() {
-          @Override
-          public Node rewriteJsDocAnnotatedExpression(JsDocAnnotatedExpression annotation) {
-            if (annotation.isDeclaration() || !getCurrentMember().isMethod()) {
-              return annotation;
-            }
-            TypeDescriptor castTypeDescriptor = annotation.getTypeDescriptor();
-            TypeDescriptor boundType =
-                replaceTypeVariableWithBound(
-                    castTypeDescriptor, this::isTypeVariableDeclaredByCurrentMember);
-            return JsDocAnnotatedExpression.newBuilder()
-                .setExpression(annotation.getExpression())
-                .setAnnotationType(boundType)
-                .build();
-          }
+    for (Type type : compilationUnit.getTypes()) {
+      for (Method method : type.getMethods()) {
+        MethodDescriptor methodDescriptor = method.getDescriptor();
+        Predicate<TypeDescriptor> shouldBeReplaced =
+            typeDescriptor ->
+                methodDescriptor
+                    .getTypeParameterTypeDescriptors()
+                    .contains(TypeDescriptors.toNonNullable(typeDescriptor));
 
-          @Override
-          public Expression rewriteFunctionExpression(FunctionExpression functionExpression) {
-            return replaceTypeInFunctionExpressionParameters(
-                functionExpression, this::isTypeVariableDeclaredByCurrentMember);
-          }
-
-          @Override
-          public VariableDeclarationFragment rewriteVariableDeclarationFragment(
-              VariableDeclarationFragment variableDeclarationFragment) {
-            Variable variable = variableDeclarationFragment.getVariable();
-            variable.setTypeDescriptor(
-                replaceTypeVariableWithBound(
-                    variable.getTypeDescriptor(), this::isTypeVariableDeclaredByCurrentMember));
-            return variableDeclarationFragment;
-          }
-
-          private boolean isTypeVariableDeclaredByCurrentMember(TypeDescriptor typeDescriptor) {
-            Member member = getCurrentMember();
-            // A JsFunction method uses @this tag to specify the type of 'this', which makes
-            // templates unresolved inside the method.
-            // TODO: double check if this is the same issue with b/24476009.
-            return typeDescriptor.isTypeVariable()
-                && member.isMethod()
-                && (((Method) member)
-                        .getDescriptor()
-                        .getTypeParameterTypeDescriptors()
-                        .contains(TypeDescriptors.toNonNullable(typeDescriptor))
-                    || (((Method) member).getDescriptor().isJsFunction()));
-          }
-        });
+        method.accept(new RewriteTypeVariablesInJsDocAnnotations(shouldBeReplaced));
+      }
+    }
 
     // Rewrite method bodies of anonymous functions to avoid references to template variables (2);
     // anonymous functions in the compiled output are a result of @JsFunction lambdas and
@@ -128,7 +93,7 @@ public class FixTypeVariablesInMethods extends NormalizationPass {
         });
   }
 
-  private FunctionExpression replaceTypeInFunctionExpressionParameters(
+  private static FunctionExpression replaceTypeInFunctionExpressionParameters(
       FunctionExpression functionExpression,
       Predicate<TypeDescriptor> isTypeVariableDeclaredByCurrentMember) {
     return AstUtils.replaceVariables(
@@ -149,33 +114,10 @@ public class FixTypeVariablesInMethods extends NormalizationPass {
   }
 
   private void removeAllTypeVariables(Node node) {
-    node.accept(
-        new AbstractRewriter() {
-          @Override
-          public Node rewriteJsDocAnnotatedExpression(JsDocAnnotatedExpression annotation) {
-            TypeDescriptor castTypeDescriptor = annotation.getTypeDescriptor();
-            TypeDescriptor boundType =
-                replaceTypeVariableWithBound(castTypeDescriptor, Predicates.alwaysTrue());
-            return JsDocAnnotatedExpression.newBuilder()
-                .setExpression(annotation.getExpression())
-                .setAnnotationType(boundType)
-                .build();
-          }
-
-          @Override
-          public VariableDeclarationFragment rewriteVariableDeclarationFragment(
-              VariableDeclarationFragment variableDeclarationFragment) {
-            Variable variable = variableDeclarationFragment.getVariable();
-            variable.setTypeDescriptor(
-                replaceTypeVariableWithBound(
-                    variable.getTypeDescriptor(), Predicates.alwaysTrue()));
-            return variableDeclarationFragment;
-          }
-        });
-
+    node.accept(new RewriteTypeVariablesInJsDocAnnotations(Predicates.alwaysTrue()));
   }
 
-  private TypeDescriptor replaceTypeVariableWithBound(
+  private static TypeDescriptor replaceTypeVariableWithBound(
       TypeDescriptor typeDescriptor, Predicate<TypeDescriptor> shouldBeReplaced) {
     switch (typeDescriptor.getKind()) {
       case TYPE_VARIABLE:
@@ -234,7 +176,7 @@ public class FixTypeVariablesInMethods extends NormalizationPass {
    * method. For a parameterized type descriptor, we check its type arguments recursively. For
    * example, Foo<Bar<T>> contains type variable 'T'.
    */
-  private boolean containsTypeVariableDeclaredByMethod(
+  private static boolean containsTypeVariableDeclaredByMethod(
       TypeDescriptor typeDescriptor, Predicate<TypeDescriptor> shouldBeReplaced) {
     if (shouldBeReplaced.test(typeDescriptor)) {
       return true;
@@ -255,7 +197,7 @@ public class FixTypeVariablesInMethods extends NormalizationPass {
     return false;
   }
 
-  private boolean containsTypeVariableDeclaredByMethodInJsFunction(
+  private static boolean containsTypeVariableDeclaredByMethodInJsFunction(
       TypeDescriptor typeDescriptor, Predicate<TypeDescriptor> shouldBeReplaced) {
     checkState(
         typeDescriptor.isJsFunctionImplementation() || typeDescriptor.isJsFunctionInterface());
@@ -272,5 +214,45 @@ public class FixTypeVariablesInMethods extends NormalizationPass {
           jsFunctionMethodDescriptor.getReturnTypeDescriptor(), shouldBeReplaced);
     }
     return false;
+  }
+
+  /**
+   * Replaces all occurrences or type variables that match {@code shouldBeReplaced} by their upper
+   * bounds raw types for types that will appear in JsDoc annotations.
+   */
+  private static class RewriteTypeVariablesInJsDocAnnotations extends AbstractRewriter {
+
+    private final Predicate<TypeDescriptor> shouldBeReplaced;
+
+    private RewriteTypeVariablesInJsDocAnnotations(Predicate<TypeDescriptor> shouldBeReplaced) {
+      this.shouldBeReplaced = shouldBeReplaced;
+    }
+
+    @Override
+    public Node rewriteJsDocAnnotatedExpression(JsDocAnnotatedExpression annotation) {
+      if (annotation.isDeclaration()) {
+        return annotation;
+      }
+      TypeDescriptor castTypeDescriptor = annotation.getTypeDescriptor();
+      TypeDescriptor boundType = replaceTypeVariableWithBound(castTypeDescriptor, shouldBeReplaced);
+      return JsDocAnnotatedExpression.newBuilder()
+          .setExpression(annotation.getExpression())
+          .setAnnotationType(boundType)
+          .build();
+    }
+
+    @Override
+    public Expression rewriteFunctionExpression(FunctionExpression functionExpression) {
+      return replaceTypeInFunctionExpressionParameters(functionExpression, shouldBeReplaced);
+    }
+
+    @Override
+    public VariableDeclarationFragment rewriteVariableDeclarationFragment(
+        VariableDeclarationFragment variableDeclarationFragment) {
+      Variable variable = variableDeclarationFragment.getVariable();
+      variable.setTypeDescriptor(
+          replaceTypeVariableWithBound(variable.getTypeDescriptor(), shouldBeReplaced));
+      return variableDeclarationFragment;
+    }
   }
 }
