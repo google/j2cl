33f67eb39c128ea60915d354f7dea88898c7888d
goktug <goktug@google.com>
2017-06-08 18:08:17 -0700

    Import: Fix some type issues in tests
    Removes type warnings from J2CL JRE tests.
    
    commit 9f949507a687e7f9d40aa1eae27958a1e69125ee
    Author: Goktug Gokdogan <goktug@google.com>
    Date:   Thu Jun 8 15:44:52 2017 -0700
    
        Fix some type issues in tests
    
        Change-Id: I9633d7ecb48396c0d0943c09e7b57d04aeb25b9f
    
    PiperOrigin-RevId: 158468293

--- jre/javatests/com/google/gwt/emultest/java/util/TreeMapViolator.native.js
+++ jre/javatests/com/google/gwt/emultest/java/util/TreeMapViolator.native.js
@@ -17,7 +17,7 @@
  * @return {void}
  * @public
  */
-TreeMapViolator.m_callAssertCorrectness__java_util_Map = function(map) {
+TreeMapViolator.m_callAssertCorrectness__java_util_TreeMap = function(map) {
   TreeMapViolator.$clinit();
   map.m_assertCorrectness___$pp_java_util();
-}
+};
