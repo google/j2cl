8825bcd08c966fe64ce9225590c6fda995bf0d17
tdeegan <tdeegan@google.com>
2017-06-22 11:12:07 -0700

    Add exports to the list of protected key words.
    See this example clash with goog.module exports:
    http://sponge/2f03696f-8f08-4d89-8419-8b40ad3727fd
    
    PiperOrigin-RevId: 159850225

--- transpiler/java/com/google/j2cl/generator/visitors/JsProtectedNames.java
+++ transpiler/java/com/google/j2cl/generator/visitors/JsProtectedNames.java
@@ -64,6 +64,7 @@ public class JsProtectedNames {
             "enum",
             "eval",
             "export",
+            "exports",
             "extends",
             "false",
             "final",
