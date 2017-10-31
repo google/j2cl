1ad08314fce832672d223c87aaef19bcd047df08
goktug <goktug@google.com>
2017-10-31 04:10:50 -0700

    Automated g4 rollback of changelist 173988828.
    *** Reason for rollback ***
    
    Breaks ritz
    
    *** Original change description ***
    
    Move Casts helper to java
    
    ***
    
    PiperOrigin-RevId: 174019666

--- jre/java/javaemul/internal/Casts.native.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {*} instance
- * @param {*} castType
- * @return {*}
- */
-Casts.$to = function(instance, castType) {
-  return Casts.$toInternal(
-      instance, /** @type {function(*):boolean} */ (castType.$isInstance), castType);
-};
