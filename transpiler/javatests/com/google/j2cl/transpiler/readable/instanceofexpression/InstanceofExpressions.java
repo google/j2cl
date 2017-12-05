51ddac881db9efc07705507f88747c246dcc3f4e
goktug <goktug@google.com>
2017-12-05 11:45:32 -0800

    Merges instanceof readables.
    Doesn't include jsinterop related ones. Not sure what to do with them tet.
    
    PiperOrigin-RevId: 177987257

--- transpiler/javatests/com/google/j2cl/transpiler/readable/instanceofexpression/InstanceofExpressions.java
+++ transpiler/javatests/com/google/j2cl/transpiler/readable/instanceofexpression/InstanceofExpressions.java
@@ -13,11 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.j2cl.transpiler.readable.instanceofboxedtype;
+package com.google.j2cl.transpiler.readable.instanceofexpression;
 
-public class InstanceofBoxedType {
-  @SuppressWarnings("unused")
-  public void test() {
+import java.io.Serializable;
+
+public class InstanceofExpressions {
+
+  public void testInstanceofClass() {
+    Object object = new InstanceofExpressions();
+    assert object instanceof InstanceofExpressions;
+    assert object instanceof Object;
+    assert !(object instanceof String);
+  }
+
+  public boolean testInstanceofInterface() {
+    Object o = new Object();
+    return o instanceof Serializable;
+  }
+
+  public void testInstanceofBoxedType() {
     Object b = new Integer(1);
 
     boolean a = b instanceof Byte;
@@ -29,5 +43,12 @@ public class InstanceofBoxedType {
     a = b instanceof Number;
     a = b instanceof Character;
     a = b instanceof Boolean;
+  }
+
+  public void testInstanceOfArray() {
+    Object object = new Object();
+
+    boolean a = object instanceof Object[];
+    boolean b = object instanceof Object[][];
   }
 }
