bf55ee6bc0cfa00c0beb0a6ce75c45757b466dd0
rluble <rluble@google.com>
2018-04-02 12:16:30 -0700

    Add (disabled) integration tests for octal characters.
    PiperOrigin-RevId: 191328236

--- transpiler/javatests/com/google/j2cl/transpiler/integration/stringescapecodes/Main.java
+++ transpiler/javatests/com/google/j2cl/transpiler/integration/stringescapecodes/Main.java
@@ -19,10 +19,30 @@ package com.google.j2cl.transpiler.integration.stringescapecodes;
  * Test String escape codes.
  */
 public class Main {
+
+  static final String DEL_CHARACTER = "" + (char) 127;
+
   public static void main(String... args) {
-    testGeneralEscapes();
+    testEmptyStringLiteral();
+    testSpecialEscapes();
     testUnicodeEscapes();
     testOctalEscapes();
+  }
+
+  private static void testEmptyStringLiteral() {
+    assert "".length() == 0;
+  }
+
+  private static void testSpecialEscapes() {
+    assert "\b".equals(String.valueOf((char) 8));
+    assert "\t".equals(String.valueOf((char) 9));
+    assert "\n".equals(String.valueOf((char) 10));
+    assert "\f".equals(String.valueOf((char) 12));
+    assert "\r".equals(String.valueOf((char) 13));
+    assert "\"".equals(String.valueOf((char) 34));
+    assert "\'".equals(String.valueOf((char) 39));
+    assert "\\".equals(String.valueOf((char) 92));
+    assert DEL_CHARACTER.equals(String.valueOf((char) 127));
   }
 
   private static void testUnicodeEscapes() {
@@ -30,21 +50,31 @@ public class Main {
     assert "\u0000".length() == 1;
   }
 
-  private static void testGeneralEscapes() {
-    assert "".length() == 0;
-    assert "\b".length() == 1;
-    assert "\f".length() == 1;
-    assert "\n".length() == 1;
-    assert "\r".length() == 1;
-    assert "\t".length() == 1;
-    assert "\\".length() == 1;
-    assert "\"".length() == 1;
-    assert "\'".length() == 1;
-  }
-
   private static void testOctalEscapes() {
     // TODO(b/34685920): uncomment when octal escape codes are transformed to avoid jscompiler
     // strict check.
+
+    // assert "\0".equals(String.valueOf((char) 0));
+    // assert "\1".equals(String.valueOf((char) 1));
+    // assert "\2".equals(String.valueOf((char) 2));
+    // assert "\3".equals(String.valueOf((char) 3));
+    // assert "\4".equals(String.valueOf((char) 4));
+    // assert "\5".equals(String.valueOf((char) 5));
+    // assert "\6".equals(String.valueOf((char) 6));
+    // assert "\7".equals(String.valueOf((char) 7));
+    // assert "\00".equals(String.valueOf((char) 0));
+    // assert "\01".equals(String.valueOf((char) 1));
+    // assert "\02".equals(String.valueOf((char) 2));
+    // assert "\03".equals(String.valueOf((char) 3));
+    // assert "\04".equals(String.valueOf((char) 4));
+    // assert "\05".equals(String.valueOf((char) 5));
+    // assert "\06".equals(String.valueOf((char) 6));
+    // assert "\07".equals(String.valueOf((char) 7));
+    // assert "\55".equals(String.valueOf((char) (5 * 8 + 5)));
+    // assert "\055".equals(String.valueOf((char) (5 * 8 + 5)));
+    // assert "\155".equals(String.valueOf((char) (1 * 64 + 5 * 8 + 5)));
+    // assert "\0155".equals(String.valueOf((char) (1 * 8+ 5) + "5"));
+
     // assert "\u001b[31m".equals("\033[31m");
   }
 }
