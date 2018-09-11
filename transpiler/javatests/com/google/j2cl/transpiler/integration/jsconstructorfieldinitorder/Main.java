089f86cd05482101c26aeaabbae10e55f6b0eebb
goktug <goktug@google.com>
2018-09-10 18:58:11 -0700

    Adds a repro for overriding values in constructor initialization.
    PiperOrigin-RevId: 212376691

---
+++ transpiler/javatests/com/google/j2cl/transpiler/integration/jsconstructorfieldinitorder/Main.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.j2cl.transpiler.integration.jsconstructorfieldinitorder;
+
+import jsinterop.annotations.JsConstructor;
+
+public class Main {
+
+  static class Foo extends Bar {
+    private String foo;
+    private int bar;
+
+    @JsConstructor
+    public Foo() {
+      super();
+    }
+
+    @Override
+    protected void initialize() {
+      super.initialize();
+      bar = 42;
+      foo = ""; // Note: "" chosen to detect potential 'falsey' mistake.
+    }
+  }
+
+  static class Bar {
+
+    @JsConstructor
+    public Bar() {
+      initialize();
+    }
+
+    protected void initialize() {}
+  }
+
+  public static void main(String... args) {
+    // Reproduces b/27484158
+    // assert new Foo().bar == 42;
+    assert new Foo().foo.equals("");
+  }
+}
