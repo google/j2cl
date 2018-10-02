abf58cef25fa21911272b9d85aa348dc6359f478
goktug <goktug@google.com>
2018-10-02 15:01:30 -0700

    Removes unnecessary recreation of divirtualization type map for each compilation unit.
    Also does some readability improvements.
    
    PiperOrigin-RevId: 215464812

--- transpiler/java/com/google/j2cl/ast/visitors/DevirtualizeMethodCalls.java
+++ transpiler/java/com/google/j2cl/ast/visitors/DevirtualizeMethodCalls.java
@@ -15,6 +15,7 @@
  */
 package com.google.j2cl.ast.visitors;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.j2cl.ast.AbstractRewriter;
 import com.google.j2cl.ast.AstUtils;
 import com.google.j2cl.ast.CompilationUnit;
@@ -27,7 +28,6 @@ import com.google.j2cl.ast.ThisReference;
 import com.google.j2cl.ast.TypeDescriptor;
 import com.google.j2cl.ast.TypeDescriptors;
 import com.google.j2cl.ast.TypeDescriptors.BootstrapType;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -35,6 +35,31 @@ import java.util.Map;
  * and their super classes and super interfaces to corresponding static method calls.
  */
 public class DevirtualizeMethodCalls extends NormalizationPass {
+
+  /**
+   * Mapping from the TypeDescriptor, whose instance methods should be devirtualized, to the
+   * TypeDescriptor that contains the devirtualized static methods that should be dispatched to.
+   *
+   * <p>The instance methods of unboxed types (Boolean, Double, String) are translated to
+   * corresponding static methods that are translated automatically by J2cl. Instance methods of
+   * Object and the super classes/interfaces of unboxed types are translated to the trampoline
+   * methods which are implemented in corresponding types (Objects, Numbers, etc.).
+   */
+  private final Map<TypeDescriptor, DeclaredTypeDescriptor> devirtualizeTargetByOriginType =
+      ImmutableMap.<TypeDescriptor, DeclaredTypeDescriptor>builder()
+          .put(TypeDescriptors.get().javaLangObject, BootstrapType.OBJECTS.getDescriptor())
+          .put(TypeDescriptors.get().javaLangBoolean, TypeDescriptors.get().javaLangBoolean)
+          .put(TypeDescriptors.get().javaLangDouble, TypeDescriptors.get().javaLangDouble)
+          .put(TypeDescriptors.get().javaLangString, TypeDescriptors.get().javaLangString)
+          .put(TypeDescriptors.get().javaLangNumber, BootstrapType.NUMBERS.getDescriptor())
+          .put(
+              TypeDescriptors.get().javaLangComparable.toRawTypeDescriptor(),
+              BootstrapType.COMPARABLES.getDescriptor())
+          .put(
+              TypeDescriptors.get().javaLangCharSequence,
+              BootstrapType.CHAR_SEQUENCES.getDescriptor())
+          .build();
+
   @Override
   public void applyTo(CompilationUnit compilationUnit) {
     compilationUnit.accept(
@@ -42,46 +67,20 @@ public class DevirtualizeMethodCalls extends NormalizationPass {
           @Override
           public Expression rewriteMethodCall(MethodCall methodCall) {
             MethodDescriptor targetMethodDescriptor = methodCall.getTarget();
+
             if (!targetMethodDescriptor.isPolymorphic()) {
               return methodCall;
             }
-            return devirtualize(methodCall);
-          }
 
-          /**
-           * Mapping from the TypeDescriptor, whose instance methods should be devirtualized, to the
-           * TypeDescriptor that contains the devirtualized static methods that should be dispatched
-           * to.
-           *
-           * <p>The instance methods of unboxed types (Boolean, Double, String) are translated to
-           * corresponding static methods that are translated automatically by J2cl. Instance
-           * methods of Object and the super classes/interfaces of unboxed types are translated to
-           * the trampoline methods which are implemented in corresponding types (Objects, Numbers,
-           * etc.).
-           */
-          private final Map<DeclaredTypeDescriptor, DeclaredTypeDescriptor>
-              devirtualizedMethodTargetTypeDescriptorByTypeDescriptor = new LinkedHashMap<>();
+            if (targetMethodDescriptor.isJsFunction()) {
+              return methodCall;
+            }
 
-          {
-            devirtualizedMethodTargetTypeDescriptorByTypeDescriptor.put(
-                TypeDescriptors.get().javaLangObject, BootstrapType.OBJECTS.getDescriptor());
-            devirtualizedMethodTargetTypeDescriptorByTypeDescriptor.put(
-                TypeDescriptors.get().javaLangBoolean, TypeDescriptors.get().javaLangBoolean);
-            devirtualizedMethodTargetTypeDescriptorByTypeDescriptor.put(
-                TypeDescriptors.get().javaLangDouble, TypeDescriptors.get().javaLangDouble);
-            devirtualizedMethodTargetTypeDescriptorByTypeDescriptor.put(
-                TypeDescriptors.get().javaLangString, TypeDescriptors.get().javaLangString);
-            devirtualizedMethodTargetTypeDescriptorByTypeDescriptor.put(
-                TypeDescriptors.get().javaLangNumber, BootstrapType.NUMBERS.getDescriptor());
-            devirtualizedMethodTargetTypeDescriptorByTypeDescriptor.put(
-                TypeDescriptors.get().javaLangComparable.toRawTypeDescriptor(),
-                BootstrapType.COMPARABLES.getDescriptor());
-            devirtualizedMethodTargetTypeDescriptorByTypeDescriptor.put(
-                TypeDescriptors.get().javaLangCharSequence,
-                BootstrapType.CHAR_SEQUENCES.getDescriptor());
-          }
+            DeclaredTypeDescriptor originType = targetMethodDescriptor.getEnclosingTypeDescriptor();
+            if (originType.isJsFunctionImplementation()) {
+              return AstUtils.devirtualizeMethodCall(methodCall, originType);
+            }
 
-          private MethodCall devirtualizeRegularMethodCall(MethodCall methodCall) {
             // Do *not* perform Object method devirtualization. The point with super method calls
             // is to *not* call the default version of the method on the prototype and instead call
             // the specific version of the method in the super class. If we were to perform Object
@@ -94,36 +93,14 @@ public class DevirtualizeMethodCalls extends NormalizationPass {
               return methodCall;
             }
 
-            TypeDescriptor enclosingTypeDescriptor =
-                methodCall.getTarget().getEnclosingTypeDescriptor().toRawTypeDescriptor();
-            if (devirtualizedMethodTargetTypeDescriptorByTypeDescriptor.containsKey(
-                enclosingTypeDescriptor)) {
-              return AstUtils.devirtualizeMethodCall(
-                  methodCall,
-                  devirtualizedMethodTargetTypeDescriptorByTypeDescriptor.get(
-                      enclosingTypeDescriptor));
-            }
-            return methodCall;
-          }
-
-          private MethodCall devirtualizeJsFunctionImplMethodCalls(MethodCall methodCall) {
-            DeclaredTypeDescriptor enclosingTypeDescriptor =
-                methodCall.getTarget().getEnclosingTypeDescriptor();
-            if (methodCall.getTarget().isJsFunction()) {
-              // Do not devirtualize the JsFunction method.
+            DeclaredTypeDescriptor targetType =
+                devirtualizeTargetByOriginType.get(originType.toRawTypeDescriptor());
+            if (targetType == null) {
               return methodCall;
             }
-            return AstUtils.devirtualizeMethodCall(methodCall, enclosingTypeDescriptor);
-          }
 
-          private MethodCall devirtualize(MethodCall methodCall) {
-            if (methodCall.getTarget().getEnclosingTypeDescriptor().isJsFunctionImplementation()) {
-              return devirtualizeJsFunctionImplMethodCalls(methodCall);
-            } else {
-              return devirtualizeRegularMethodCall(methodCall);
-            }
+            return AstUtils.devirtualizeMethodCall(methodCall, targetType);
           }
         });
   }
-
 }
