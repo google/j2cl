b85ee7f4978b818247ca040904d7f5462ed6734b
goktug <goktug@google.com>
2018-11-04 13:29:37 -0800

    Fix helloworld for open source.
    Without the entry point;
     - open source optimizes away everything by default
     - non open source compilation takes forever
    
    PiperOrigin-RevId: 220014547

--- samples/helloworld/java/com/google/j2cl/samples/helloworld/app.js
+++ samples/helloworld/java/com/google/j2cl/samples/helloworld/app.js
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-goog.module('j2cl.samples.helloworld');
+goog.module('j2cl.samples.app');
 
 // This is the entry point of our app.
 // Feel free to use any fancy new ES features, Closure compiler will take care
