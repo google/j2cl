eef73e2cf3383d4a5414de33d0ad2f9af8817a13
goktug <goktug@google.com>
2018-11-04 22:11:36 -0800

    Add the sample app to kokoro build
    This also updates the jobs to run the latest version of bazel since default one is old and and closure rules requires a newish version.
    
    PiperOrigin-RevId: 220043225

--- kokoro/bazel_build.sh
+++ kokoro/bazel_build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+chmod +x ${KOKORO_GFILE_DIR}/use_bazel.sh
+${KOKORO_GFILE_DIR}/use_bazel.sh latest
+bazel version
+
 # the repo is cloned under git/j2cl
 cd git/j2cl
 
-bazel build transpiler/java/...
-bazel build tools/java/...
+bazel build {transpiler,tools,jre,samples/helloworld}/java/...
