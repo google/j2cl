9630020173f9dc95ddda83e04cccbe05a9cf0628
rluble <rluble@google.com>
2019-01-30 18:54:32 -0800

    Revert of "Add suppression for checks related to static member override."
    PiperOrigin-RevId: 231710940

--- transpiler/java/com/google/j2cl/frontend/EnumMethodsCreator.java
+++ transpiler/java/com/google/j2cl/frontend/EnumMethodsCreator.java
@@ -33,6 +33,7 @@ import com.google.j2cl.ast.JsInfo;
 import com.google.j2cl.ast.Method;
 import com.google.j2cl.ast.MethodCall;
 import com.google.j2cl.ast.MethodDescriptor;
+import com.google.j2cl.ast.MethodDescriptor.ParameterDescriptor;
 import com.google.j2cl.ast.NullLiteral;
 import com.google.j2cl.ast.ReturnStatement;
 import com.google.j2cl.ast.RuntimeMethods;
@@ -152,7 +153,11 @@ public class EnumMethodsCreator {
         .setEnclosingTypeDescriptor(enumTypeDescriptor)
         .setName(VALUE_OF_METHOD_NAME)
         .setReturnTypeDescriptor(enumTypeDescriptor.toNonNullable())
-        .setParameterTypeDescriptors(TypeDescriptors.get().javaLangString.toNonNullable())
+        .setParameterDescriptors(
+            ParameterDescriptor.newBuilder()
+                .setTypeDescriptor(TypeDescriptors.get().javaLangString.toNonNullable())
+                .setJsOptional(isJsType)
+                .build())
         .setJsInfo(isJsType ? JsInfo.RAW : JsInfo.NONE)
         .build();
   }
