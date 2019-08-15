616de74e940b735c42839d9950427ddc6c48f319
Roberto Lublinerman <rluble@google.com>
2019-08-15 11:49:55 -0700

    Remove JsDocCasts that refer to out fo scope type variables.
    Some Java frontend (e.g. JavaC) do not infer types for type variables when not needed by the Java backend; in J2CL inferred types are used to give better typing information to JsCompiler and those types appear mostly as JsDocCastExpressions. This pass has changed to remove JsDocCastExpressions that point to out of scope template variables.
    
    PiperOrigin-RevId: 263610129

--- transpiler/java/com/google/j2cl/ast/visitors/NormalizeJsDocCastExpressions.java
+++ transpiler/java/com/google/j2cl/ast/visitors/NormalizeJsDocCastExpressions.java
@@ -15,57 +15,142 @@
  */
 package com.google.j2cl.ast.visitors;
 
-import com.google.common.collect.ImmutableList;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import com.google.j2cl.ast.AbstractRewriter;
+import com.google.j2cl.ast.AbstractVisitor;
 import com.google.j2cl.ast.CompilationUnit;
 import com.google.j2cl.ast.DeclaredTypeDescriptor;
 import com.google.j2cl.ast.Expression;
 import com.google.j2cl.ast.JsDocCastExpression;
+import com.google.j2cl.ast.Member;
+import com.google.j2cl.ast.Method;
+import com.google.j2cl.ast.Type;
 import com.google.j2cl.ast.TypeDescriptor;
+import com.google.j2cl.ast.TypeDescriptors;
 import com.google.j2cl.ast.TypeVariable;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Rewrites casts to {@code @type {Foo<?>}} where {@code ? extends Bar} as casts to {@code @type
- * {Foo<Bar>}} to avoid "unknown type" errors.
+ * {Foo<Bar>}} to avoid "unknown type" errors; and replaces out of scope type variables with
+ * wildcards.
  */
 public class NormalizeJsDocCastExpressions extends NormalizationPass {
+  private final Multimap<Member, TypeVariable> typeVariablesByMember = HashMultimap.create();
+
   @Override
   public void applyTo(CompilationUnit compilationUnit) {
+
+    // Collect all type variables that are in context in a given member.
+    compilationUnit.accept(
+        new AbstractVisitor() {
+          private final Deque<Set<TypeVariable>> typeVariablesInContext = new ArrayDeque<>();
+
+          {
+            typeVariablesInContext.add(new HashSet<>());
+          }
+
+          @Override
+          public boolean enterType(Type type) {
+            Set<TypeVariable> typeVariables =
+                new HashSet<>(type.getDeclaration().getTypeParameterDescriptors());
+            if (!type.isStatic()) {
+              typeVariables.addAll(typeVariablesInContext.peek());
+            }
+            typeVariablesInContext.push(typeVariables);
+            return true;
+          }
+
+          @Override
+          public void exitType(Type type) {
+            typeVariablesInContext.pop();
+          }
+
+          @Override
+          public boolean enterMember(Member member) {
+            pushEnclosingTypeVariables(member);
+            return true;
+          }
+
+          private void pushEnclosingTypeVariables(Member member) {
+            Set<TypeVariable> enclosingTypeVariables = new HashSet<>();
+            if (!member.isStatic()) {
+              // Add type variables from enclosing context.
+              enclosingTypeVariables.addAll(typeVariablesInContext.peek());
+            }
+            typeVariablesInContext.push(enclosingTypeVariables);
+          }
+
+          @Override
+          public boolean enterMethod(Method method) {
+            pushEnclosingTypeVariables(method);
+            typeVariablesInContext
+                .peek()
+                .addAll(method.getDescriptor().getTypeParameterTypeDescriptors());
+            return true;
+          }
+
+          @Override
+          public void exitMember(Member member) {
+            typeVariablesByMember.putAll(member, typeVariablesInContext.pop());
+          }
+        });
+
     compilationUnit.accept(
         new AbstractRewriter() {
           @Override
           public Expression rewriteJsDocCastExpression(JsDocCastExpression jsDocCastExpression) {
-            if (!(jsDocCastExpression.getTypeDescriptor() instanceof DeclaredTypeDescriptor)) {
-              return jsDocCastExpression;
+            TypeDescriptor castTypeDescriptor = jsDocCastExpression.getTypeDescriptor();
+            if (jsDocCastExpression.getTypeDescriptor() instanceof DeclaredTypeDescriptor) {
+
+              DeclaredTypeDescriptor castDeclaredTypeDescriptor =
+                  (DeclaredTypeDescriptor) jsDocCastExpression.getTypeDescriptor();
+
+              // If it is a parameterized type first replace wildcards with their bound top
+              // avoid "unknown type" errors.
+              if (castDeclaredTypeDescriptor.hasTypeArguments()) {
+                castTypeDescriptor =
+                    DeclaredTypeDescriptor.Builder.from(castDeclaredTypeDescriptor)
+                        .setTypeArgumentDescriptors(
+                            castDeclaredTypeDescriptor.getTypeArgumentDescriptors().stream()
+                                .map(NormalizeJsDocCastExpressions::replaceWildcardWithBound)
+                                .collect(toImmutableList()))
+                        .build();
+              }
             }
 
-            DeclaredTypeDescriptor castTypeDescriptor =
-                (DeclaredTypeDescriptor) jsDocCastExpression.getTypeDescriptor();
-
-            if (!castTypeDescriptor.hasTypeArguments()) {
-              return jsDocCastExpression;
-            }
-
+            // Replace out of bounds type variables that might have been left by the frontend
+            // if the inferrence was not needed for Java compilation.
             return JsDocCastExpression.Builder.from(jsDocCastExpression)
                 .setCastType(
-                    DeclaredTypeDescriptor.Builder.from(castTypeDescriptor)
-                        .setTypeArgumentDescriptors(
-                            castTypeDescriptor
-                                .getTypeArgumentDescriptors()
-                                .stream()
-                                .map(NormalizeJsDocCastExpressions::replaceWildcardWithBound)
-                                .collect(ImmutableList.toImmutableList()))
-                        .build())
+                    castTypeDescriptor.specializeTypeVariables(
+                        typeVariable ->
+                            replaceOutofScopeTypeVariable(getCurrentMember(), typeVariable)))
                 .build();
           }
         });
   }
 
+  /** Replaces wildcards with their bound. */
   private static TypeDescriptor replaceWildcardWithBound(TypeDescriptor typeDescriptor) {
     if (!(typeDescriptor instanceof TypeVariable)) {
       return typeDescriptor;
     }
     TypeVariable typeVariable = (TypeVariable) typeDescriptor;
     return typeVariable.isWildcardOrCapture() ? typeDescriptor.toRawTypeDescriptor() : typeVariable;
+  }
+
+  /** Replaces out of scope variables by a wilcard. */
+  private TypeDescriptor replaceOutofScopeTypeVariable(Member member, TypeVariable typeVariable) {
+    return typeVariable.isWildcardOrCapture()
+            || typeVariablesByMember.containsEntry(member, typeVariable)
+        ? typeVariable
+        : TypeVariable.createWildcardWithBound(TypeDescriptors.get().javaLangObject);
   }
 }
