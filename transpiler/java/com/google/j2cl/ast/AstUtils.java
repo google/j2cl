1f1aac46bb9ae5ff6f114a429129689d8b8938b1
Roberto Lublinerman <rluble@google.com>
2019-08-27 04:26:54 -0700

    Declare devirtualized instance argument ($thisArg) as non nullable.
    PiperOrigin-RevId: 265655535

--- transpiler/java/com/google/j2cl/ast/AstUtils.java
+++ transpiler/java/com/google/j2cl/ast/AstUtils.java
@@ -931,7 +931,7 @@ public class AstUtils {
     final Variable thisArg =
         Variable.newBuilder()
             .setName("$thisArg")
-            .setTypeDescriptor(method.getDescriptor().getEnclosingTypeDescriptor())
+            .setTypeDescriptor(method.getDescriptor().getEnclosingTypeDescriptor().toNonNullable())
             .setParameter(true)
             .setFinal(true)
             .build();
