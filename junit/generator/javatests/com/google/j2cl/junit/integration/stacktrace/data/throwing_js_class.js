f9d8ecad4202a24f1a2f59d79544a9648b3992b5
Goktug Gokdogan <goktug@google.com>
2019-10-09 09:00:58 -0700

    Remove unnecessary comment in native stack trace test and update the error message.
    PiperOrigin-RevId: 273756099

--- junit/generator/javatests/com/google/j2cl/junit/integration/stacktrace/data/throwing_js_class.js
+++ junit/generator/javatests/com/google/j2cl/junit/integration/stacktrace/data/throwing_js_class.js
@@ -28,13 +28,7 @@ class ThrowingJsClass {
   }
 
   method3() {
-    // We are throwing a Js error here instead of a Java exception, since
-    // this exposes a bug in JsCompiler (b/63400239) that does not account
-    // for removing the 'new' in the column number.
-    // We still use the text here that the Java exception would have produced
-    // so that our stack trace asserter does not need to deal with this
-    // special case.
-    throw new Error('java.lang.RuntimeException: __the_message__!');
+    throw new Error('Exception: __the_message__!');
   }
 }
 
