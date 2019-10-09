f26925c6f79f61cefa3b176c41902e229624f8b7
Goktug Gokdogan <goktug@google.com>
2019-10-09 12:59:50 -0700

    Add more stack trace deobfuscation test scenario for JsFunction.
    PiperOrigin-RevId: 273803158

--- junit/generator/javatests/com/google/j2cl/junit/integration/stacktrace/data/ThrowsInJsFunction.java
+++ junit/generator/javatests/com/google/j2cl/junit/integration/stacktrace/data/ThrowsInJsFunction.java
@@ -20,7 +20,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/** Integration test for throwing in a JsConstructor */
+/** Integration test for throwing in a JsFunction */
 @RunWith(JUnit4.class)
 public class ThrowsInJsFunction extends StacktraceTestBase {
   @JsFunction
@@ -30,13 +30,33 @@ public class ThrowsInJsFunction extends StacktraceTestBase {
 
   @Test
   public void test() {
+    executesFunction(this::methodRefJsFunction);
+  }
+
+  private void methodRefJsFunction() {
     executesFunction(
+        // Lambda JsFunction
         () -> {
-          throw new RuntimeException("__the_message__!");
+          executesFunction(
+              // Anonymous JsFunction
+              new MyFunction() {
+                @Override
+                public void run() {
+                  // Concrete JsFunction
+                  executesFunction(new MyFunctionImpl());
+                }
+              });
         });
   }
 
-  private void executesFunction(MyFunction myFunction) {
+  private static final class MyFunctionImpl implements MyFunction {
+    @Override
+    public void run() {
+      throw new RuntimeException("__the_message__!");
+    }
+  }
+
+  void executesFunction(MyFunction myFunction) {
     myFunction.run();
   }
 }
