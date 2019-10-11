0a2e0807f5a274e9bde4d95f31d7390be658bb8a
Roberto Lublinerman <rluble@google.com>
2019-10-10 17:20:06 -0700

    Refactor integration tests dealing with captures.
    Refactored the tests preserving their intention not their style. Will be adding more tests related to captures.
    
    PiperOrigin-RevId: 274073008

---
+++ transpiler/javatests/com/google/j2cl/transpiler/integration/captures/Main.java
@@ -0,0 +1,221 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.j2cl.transpiler.integration.captures;
+
+import static com.google.j2cl.transpiler.utils.Asserts.assertEquals;
+import static com.google.j2cl.transpiler.utils.Asserts.assertTrue;
+
+/** Test captures. */
+public class Main {
+  public static void main(String[] args) {
+    testVariableCapture();
+    testVariableCapture_nested();
+    testVariableCapture_indirect();
+    testParameterCapture();
+    testOuterCapture();
+    testOuterCapture_nested();
+    testOuterCapture_indirect();
+    testCaptures_constructor();
+    testCaptures_parent();
+    testCaptures_anonymous();
+  }
+
+  private static void testVariableCapture() {
+    final int variable = 1;
+    class Inner {
+      public int variable() {
+        return variable;
+      }
+    }
+    assertEquals(variable, new Inner().variable());
+  }
+
+  private static void testVariableCapture_nested() {
+    final int variable = 1;
+    class Outer {
+      class Inner {
+        public int variable() {
+          return variable;
+        }
+      }
+    }
+    assertEquals(variable, new Outer().new Inner().variable());
+  }
+
+  private static void testVariableCapture_indirect() {
+    int local = 3;
+    class ClassCapturingLocal {
+      int returnLocal() {
+        return local;
+      }
+    }
+
+    class ClassIndirectlyCapturingLocal {
+      int returnIndirectCapture() {
+        return new ClassCapturingLocal().returnLocal();
+      }
+    }
+    assertTrue(new ClassIndirectlyCapturingLocal().returnIndirectCapture() == 3);
+  }
+
+  private static void testCaptures_constructor() {
+    final int variable = 1;
+    // local class with non-default constructor and this() call
+    class Local {
+
+      public int field;
+
+      public Local(int a, int b) {
+        field = variable + a + b;
+      }
+
+      public Local(int a) {
+        this(a, variable);
+      }
+    }
+    int a = 10, b = 20;
+    assertEquals(variable + a + b, new Local(a, b).field);
+    assertEquals(variable + a + variable, new Local(a).field);
+  }
+
+  private static void testParameterCapture() {
+    class Outer {
+      int captureInInner(int parameter) {
+        class Inner {
+          public int parameter() {
+            return parameter;
+          }
+        }
+        return new Inner().parameter();
+      }
+    }
+
+    int parameter = 10;
+    assertEquals(parameter, new Outer().captureInInner(10));
+  }
+
+  private static void testOuterCapture() {
+    class Outer {
+      class Inner {
+        public Outer captured() {
+          return Outer.this;
+        }
+      }
+    }
+    Outer outer = new Outer();
+    assertEquals(outer, outer.new Inner().captured());
+  }
+
+  private static int ten = 10;
+
+  private static class Outer {
+    int outerField = ten;
+
+    class Intermediate {
+      class Inner {
+        public int captured() {
+          return outerField;
+        }
+      }
+    }
+  }
+
+  private static void testOuterCapture_nested() {
+    Outer outer = new Outer();
+    assertEquals(ten, outer.new Intermediate().new Inner().captured());
+  }
+
+  private static void testOuterCapture_indirect() {
+    class Outer {
+      class ClassCapturingOuter {
+        Outer capturedOuter() {
+          return Outer.this;
+        }
+      }
+
+      class ClassIndirectlyCapturingOuter {
+        Outer returnIndirectCapture() {
+          return new ClassCapturingOuter().capturedOuter();
+        }
+      }
+    }
+    Outer outer = new Outer();
+    assertEquals(outer, outer.new ClassIndirectlyCapturingOuter().returnIndirectCapture());
+  }
+
+  private static void testCaptures_parent() {
+    final int variable = 10;
+    class Parent {
+      int variablePlusPar(int parameter) {
+        return variable + parameter;
+      }
+    }
+
+    class AChild extends Parent {}
+
+    class AnotherChild extends Parent {
+      public AnotherChild() {
+        super();
+      }
+    }
+
+    int parameter = 15;
+    assertEquals(variable + parameter, new AChild().variablePlusPar(parameter));
+    assertEquals(variable + parameter, new AnotherChild().variablePlusPar(parameter));
+  }
+
+  interface AnonymousInterface {
+    void foo();
+  }
+
+  private abstract static class SomeClass {
+    public int f;
+
+    SomeClass(int f) {
+      this.f = f;
+    }
+
+    public int foo() {
+      return f;
+    }
+  }
+
+  public static void testCaptures_anonymous() {
+    final Object[] instances = new Object[3];
+    class Outer {
+      AnonymousInterface i =
+          new AnonymousInterface() {
+            Object cachedThis = this;
+
+            @Override
+            public void foo() {
+              instances[0] = this;
+              instances[1] = cachedThis;
+              instances[2] = Outer.this;
+            }
+          };
+    }
+
+    Outer outer = new Outer();
+    outer.i.foo();
+
+    assertTrue(instances[0] == outer.i);
+    assertTrue(instances[1] == outer.i);
+    assertTrue(instances[2] == outer);
+
+    assertTrue(new SomeClass(3) {}.foo() == 3);
+  }
+}
