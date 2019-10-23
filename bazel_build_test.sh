3572bf426a945bf8b5233d2f8981e638282390f4
Roberto Lublinerman <rluble@google.com>
2019-10-23 12:59:13 -0700

    Include compiling the transpiler and transpiling the jre in the presubmit for opensource.
    PiperOrigin-RevId: 276333542

--- bazel_build_test.sh
+++ bazel_build_test.sh
@@ -16,7 +16,7 @@
 # Script that can be used by CI server for testing j2cl builds.
 set -e
 
-bazel build :all tools/java/...
+bazel build :all {jre,transpiler,tools}/java/...
 
 # build hello world sample in its own workspace
 (cd samples/helloworld && bazel build src/main/...)
