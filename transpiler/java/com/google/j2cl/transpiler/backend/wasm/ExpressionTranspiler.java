2acba1f350aee3efd5f243c9086b99d09d034e93
Goktug Gokdogan <goktug@google.com>
2021-04-14 18:59:03 -0700

    [WASM] Nominal implementation of instanceof for arrays.
    PiperOrigin-RevId: 368552572

--- transpiler/java/com/google/j2cl/transpiler/backend/wasm/ExpressionTranspiler.java
+++ transpiler/java/com/google/j2cl/transpiler/backend/wasm/ExpressionTranspiler.java
@@ -190,8 +190,7 @@ final class ExpressionTranspiler {
           return false;
         }
 
-        // TODO(b/184675805): implement array cast expressions beyond this nominal
-        // implementation.
+        // TODO(b/180967010): implement full support of array cast.
         sourceBuilder.append("(ref.cast ");
         render(castExpression.getExpression());
         sourceBuilder.append(
@@ -248,16 +247,17 @@ final class ExpressionTranspiler {
       @Override
       public boolean enterInstanceOfExpression(InstanceOfExpression instanceOfExpression) {
         TypeDescriptor testTypeDescriptor = instanceOfExpression.getTestTypeDescriptor();
-        // TODO(b/184675805): implement array instanceof expressions beyond this nominal
-        // implementation.
-        if (!testTypeDescriptor.isInterface()) {
+        if (testTypeDescriptor.isClass() || testTypeDescriptor.isEnum()) {
           sourceBuilder.append("(ref.test ");
           render(instanceOfExpression.getExpression());
           sourceBuilder.append(
-              format(" (global.get %s))", environment.getRttGlobalName(testTypeDescriptor)));
+              format(
+                  " (global.get %s))",
+                  environment.getRttGlobalName(
+                      ((DeclaredTypeDescriptor) testTypeDescriptor).getTypeDeclaration())));
           return false;
         }
-        // TODO(b/170691636): handle interface instanceof expressions.
+        // TODO(rluble): handle interface and array instanceof expressions.
         renderUnimplementedExpression(instanceOfExpression);
         return false;
       }
