600fefafe3791d2608a7f9d096905f3895ffa534
Goktug Gokdogan <goktug@google.com>
2021-06-06 21:27:21 -0700

    Update Guava sample to do user visible output.
    PiperOrigin-RevId: 377837192

--- samples/guava/src/main/java/com/google/j2cl/samples/guava/app.js
+++ samples/guava/src/main/java/com/google/j2cl/samples/guava/app.js
@@ -15,4 +15,4 @@ goog.module('j2cl.samples.app');
 
 const HelloGuava = goog.require('com.google.j2cl.samples.guava.HelloGuava');
 
-HelloGuava.getHelloWorld();
+document.body.innerText = HelloGuava.getHelloWorld();
