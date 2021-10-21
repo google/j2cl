google/j2cl

commit 10b59f2a794958f197db774e0991ef9530132920

Add J2WASM sample app.
This also makes sure we have the correct wiring for open source.

PiperOrigin-RevId: 404868704

build_defs/internal_do_not_use/j2wasm_application.bzl
@@ -153,10 +153,6 @@ _j2wasm_application = rule(
         "binaryen_stage1_args": attr.string_list(),
         "binaryen_stage2_args": attr.string_list(),
         "transpiler_args": attr.string_list(),
-        "binaryen": attr.label(
-            cfg = "host",
-            executable = True,
-        ),
         "defines": attr.string_list(),
         "_jre": attr.label(default = Label("//build_defs/internal_do_not_use:j2wasm_jre")),
         "_j2cl_transpiler": attr.label(
@@ -169,7 +165,7 @@ _j2wasm_application = rule(
         "_binaryen": attr.label(
             cfg = "host",
             executable = True,
-            default = Label("//third_party/binaryen:wasm-opt"),
+            default = Label("//third_party:binaryen"),
         ),
     },
     outputs = {

jre/java/BUILD
@@ -133,6 +133,8 @@ j2wasm_library(
 closure_js_library(
     name = "j2wasm_js",
     srcs = ["super-wasm/j2wasm.js"],
+    # TODO(goktug): Remove after open source closure compiler update.
+    lenient = True,
     visibility = ["//:__pkg__"],
 )
 

samples/wasm/src/main/java/com/google/j2cl/samples/wasm/BUILD
@@ -1,0 +1,65 @@
+# Sample for J2WASM.
+#
+# Note that J2WASM is experimental and it is NOT production ready.
+#
+# It is mostly developed to help with the evolution of WASM GC specification and
+# what is released externally here is minimal working version to provide .wat files
+# for community experimentation and it doesn't reflect final workings of the product
+# nor the WASM spec.
+
+load("@io_bazel_rules_closure//closure:defs.bzl", "closure_js_library")
+load(
+    "@com_google_j2cl//build_defs:rules.bzl",
+    "j2cl_application",
+    "j2wasm_application",
+    "j2wasm_library",
+)
+
+package(licenses = ["notice"])
+
+# This is the bazel target that compiles our J2WASM library.
+# Since J2WASM currently does global compilation this only serves as a way
+# to collect sources and byte code required for j2wasm_application compilation.
+j2wasm_library(
+    name = "helloworld",
+    srcs = glob(["*.java"]),
+)
+
+# This is the bazel target that compiles and optimizes whole J2WASM app.
+# It provides couple of convenient targets:
+#  :app produces the app.wat and app.wasm that could be used for production.
+#  :app_dev produces app_dev.wat and app_dev.wasm as development version
+#
+# e.g.:
+#   $ bazel build samples/wasm/src/main/java/com/google/j2cl/samples/wasm:app
+#
+# Note that for .wat files (wasm binary output) to be available, you would
+# need to have a recent version of binaryen available in your path.
+j2wasm_application(
+    name = "app",
+    entry_points = ["com.google.j2cl.samples.wasm.HelloWorld.getHelloWorld"],
+    deps = [":helloworld"],
+)
+
+# Below is an example of the JS wiring of the wasm app
+
+closure_js_library(
+    name = "hellojs",
+    srcs = glob(["*.js"]),
+    lenient = True,
+    deps = ["@com_google_j2cl//:j2wasm_js"],
+)
+
+# This is the bazel target that serves your J2WASM app.
+#
+# Give it a try:
+#   $ bazel run samples/wasm/src/main/java/com/google/j2cl/samples/wasm:jsapp_dev_server
+#
+j2cl_application(
+    name = "jsapp",
+    entry_points = ["entry"],
+    extra_dev_resources = [
+        ":app.wasm",
+    ],
+    deps = [":hellojs"],
+)

third_party/BUILD
@@ -65,6 +65,11 @@ alias(
     actual = "@com_sun_tools_javac//jar",
 )
 
+sh_binary(
+    name = "binaryen",
+    srcs = ["binaryen.sh"],
+)
+
 alias(
     name = "auto_value",
     actual = "@com_google_auto_value",

build_test_samples.sh
@@ -20,3 +20,7 @@ set -e
 
 # Build Guava sample in its own workspace
 (cd samples/guava && bazel build src/main/...)
+
+# Build WASM sample in its own workspace
+# Note that since binaryen dep is not available, we only build the wat file.
+(cd samples/wasm && bazel build src/main/java/com/google/j2cl/samples/wasm:app.wat)

samples/wasm/WORKSPACE.oss
@@ -1,0 +1,15 @@
+workspace(name = "com_google_j2cl_samples_helloworld")
+
+# Load j2cl repository
+# If you are not developing with J2CL, you may want to use a remote repository
+# instead.
+local_repository(
+    name = "com_google_j2cl",
+    path = "../..",
+)
+
+load("@com_google_j2cl//build_defs:repository.bzl", "load_j2cl_repo_deps")
+load_j2cl_repo_deps()
+
+load("@com_google_j2cl//build_defs:rules.bzl", "setup_j2cl_workspace")
+setup_j2cl_workspace()

samples/wasm/src/main/java/com/google/j2cl/samples/wasm/HelloWorld.java
@@ -1,0 +1,25 @@
+/*
+ * Copyright 2021 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.j2cl.samples.wasm;
+
+import javaemul.internal.WasmExtern;
+
+/** A simple hello world example. */
+public class HelloWorld {
+  public static WasmExtern getHelloWorld() {
+    return "Hello from Java!".toJsString();
+  }
+}

samples/wasm/src/main/java/com/google/j2cl/samples/wasm/hello.js
@@ -1,0 +1,23 @@
+/*
+ * Copyright 2021 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+goog.module('entry');
+
+const j2wasm = goog.require('j2wasm');
+
+j2wasm.instantiateStreaming('app.wasm').then((instance) => {
+  document.body.innerText = instance.exports.getHelloWorld();
+});

commit fcf51cfe1a29632018818c2053a5299db770eeb5

Fix binaryen visibility issue introduced in https://github.com/google/j2cl/commit/10b59f2a794958f197db774e0991ef9530132920
PiperOrigin-RevId: 404882420

build_defs/internal_do_not_use/BUILD
@@ -74,3 +74,10 @@ alias(
     name = "J2clRta",
     actual = "//tools/java/com/google/j2cl/tools/rta:J2clRta_worker",
 )
+
+alias(
+    name = "binaryen",
+    actual = "//third_party:binaryen",
+)
+
+# bzl_librarys

build_defs/internal_do_not_use/j2wasm_application.bzl
@@ -165,7 +165,9 @@ _j2wasm_application = rule(
         "_binaryen": attr.label(
             cfg = "host",
             executable = True,
-            default = Label("//third_party:binaryen"),
+            default = Label(
+                "//build_defs/internal_do_not_use:binaryen",
+            ),
         ),
     },
     outputs = {