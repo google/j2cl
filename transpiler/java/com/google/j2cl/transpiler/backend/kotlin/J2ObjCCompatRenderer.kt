90e31acc9114a1d9f6f9c1ce63036b8dd386a6ef
Googler <noreply@google.com>
2023-01-18 20:26:32 -0800

    [J2KT] Improve rendering of special enum values in J2ObjCCompat.h, by taking a list of reserved C keywords and Obj-C prefixes from GitHub JetBrains/kotlin-native sources.
    PiperOrigin-RevId: 503051564

--- transpiler/java/com/google/j2cl/transpiler/backend/kotlin/J2ObjCCompatRenderer.kt
+++ transpiler/java/com/google/j2cl/transpiler/backend/kotlin/J2ObjCCompatRenderer.kt
@@ -134,20 +134,125 @@ private val FieldDescriptor.enumGetFunctionRenderer: Renderer<Source>
 private val FieldDescriptor.enumGetFunctionName: String
   get() = enclosingTypeDescriptor.typeDeclaration.objCName(forMember = true) + "_get_" + name!!
 
+// Taken from GitHub:
+// "JetBrains/kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/objcexport/ObjCExportNamer.kt"
+private val objCReservedPrefixes = setOf("alloc", "copy", "mutableCopy", "new", "init")
+
+// Taken from GitHub:
+// "JetBrains/kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/CAdapterGenerator.kt"
+internal val cKeywords =
+  setOf(
+    // Actual C keywords.
+    "auto",
+    "break",
+    "case",
+    "char",
+    "const",
+    "continue",
+    "default",
+    "do",
+    "double",
+    "else",
+    "enum",
+    "extern",
+    "float",
+    "for",
+    "goto",
+    "if",
+    "int",
+    "long",
+    "register",
+    "return",
+    "short",
+    "signed",
+    "sizeof",
+    "static",
+    "struct",
+    "switch",
+    "typedef",
+    "union",
+    "unsigned",
+    "void",
+    "volatile",
+    "while",
+    // C99-specific.
+    "_Bool",
+    "_Complex",
+    "_Imaginary",
+    "inline",
+    "restrict",
+    // C11-specific.
+    "_Alignas",
+    "_Alignof",
+    "_Atomic",
+    "_Generic",
+    "_Noreturn",
+    "_Static_assert",
+    "_Thread_local",
+    // Not exactly keywords, but reserved or standard-defined.
+    "and",
+    "not",
+    "or",
+    "xor",
+    "bool",
+    "complex",
+    "imaginary",
+
+    // C++ keywords not listed above.
+    "alignas",
+    "alignof",
+    "and_eq",
+    "asm",
+    "bitand",
+    "bitor",
+    "bool",
+    "catch",
+    "char16_t",
+    "char32_t",
+    "class",
+    "compl",
+    "constexpr",
+    "const_cast",
+    "decltype",
+    "delete",
+    "dynamic_cast",
+    "explicit",
+    "export",
+    "false",
+    "friend",
+    "inline",
+    "mutable",
+    "namespace",
+    "new",
+    "noexcept",
+    "not_eq",
+    "nullptr",
+    "operator",
+    "or_eq",
+    "private",
+    "protected",
+    "public",
+    "reinterpret_cast",
+    "static_assert",
+    "template",
+    "this",
+    "thread_local",
+    "throw",
+    "true",
+    "try",
+    "typeid",
+    "typename",
+    "using",
+    "virtual",
+    "wchar_t",
+    "xor_eq"
+  )
+
 private val FieldDescriptor.enumObjCName: String
   get() =
     CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, name!!).let { name ->
-      if (name.startsWith("new") || name.startsWith("copy")) "the" + name.titleCase
-      else
-        when (name) {
-          "default",
-          "continue",
-          "struct",
-          "delete",
-          "inline",
-          "register" -> name + "_"
-          else -> name
-        }
+      if (objCReservedPrefixes.any { name.startsWith(it) }) "the" + name.titleCase
+      else if (cKeywords.contains(name)) name + "_" else name
     }
 
 private val FieldDescriptor.enumGetExpressionRenderer: Renderer<Source>
