636168cfb7cdd1d0c8f01e52684e9c2cdd103327
Goktug Gokdogan <goktug@google.com>
2023-02-06 18:35:30 -0800

    Copy port of jbox2d to j2cl/samples.
    It only includes the relevant part that compiles with J2CL.
    
    PiperOrigin-RevId: 507639612

---
+++ samples/box2d/src/main/java/org/jbox2d/dynamics/joints/RopeJointDef.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Daniel Murphy
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 	* Redistributions of source code must retain the above copyright notice,
+ * 	  this list of conditions and the following disclaimer.
+ * 	* Redistributions in binary form must reproduce the above copyright notice,
+ * 	  this list of conditions and the following disclaimer in the documentation
+ * 	  and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+package org.jbox2d.dynamics.joints;
+
+import org.jbox2d.common.Vec2;
+
+/**
+ * Rope joint definition. This requires two body anchor points and a maximum lengths. Note: by
+ * default the connected objects will not collide. see collideConnected in b2JointDef.
+ * 
+ * @author Daniel Murphy
+ */
+public class RopeJointDef extends JointDef {
+
+  /**
+   * The local anchor point relative to bodyA's origin.
+   */
+  public final Vec2 localAnchorA = new Vec2();
+
+  /**
+   * The local anchor point relative to bodyB's origin.
+   */
+  public final Vec2 localAnchorB = new Vec2();
+
+  /**
+   * The maximum length of the rope. Warning: this must be larger than b2_linearSlop or the joint
+   * will have no effect.
+   */
+  public float maxLength;
+
+  public RopeJointDef() {
+    type = JointType.ROPE;
+    localAnchorA.set(-1.0f, 0.0f);
+    localAnchorB.set(1.0f, 0.0f);
+  }
+}
