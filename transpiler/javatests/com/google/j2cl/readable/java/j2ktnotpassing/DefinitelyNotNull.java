9cce06bc212979e4c8980984d3430e526d5009fe
Googler <noreply@google.com>
2023-03-15 10:33:57 -0700

    [J2KT] Reproduces problem with incorrect inference of definitely not-null types: <T & Any>, and another problem with type inference.
    PiperOrigin-RevId: 516865564

---
+++ transpiler/javatests/com/google/j2cl/readable/java/j2ktnotpassing/DefinitelyNotNull.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package j2ktnotpassing;
+
+import org.jspecify.nullness.NullMarked;
+import org.jspecify.nullness.Nullable;
+
+@NullMarked
+public class DefinitelyNotNull {
+  static class Ordering<T extends @Nullable Object> {
+    <S extends T> Ordering<S> reverse() {
+      throw new RuntimeException();
+    }
+
+    <S extends T> Ordering<@Nullable S> nullsLast() {
+      throw new RuntimeException();
+    }
+
+    static <E extends @Nullable Object> Ordering<? super E> reversed(Ordering<? super E> ordering) {
+      // The inferred type parameter for reverse() should be <E> and not <E & Any>
+      // See: b/268006049, b/272714235.
+      return ordering.reverse();
+    }
+  }
+
+  final class NullsFirstOrdering<T extends @Nullable Object> extends Ordering<@Nullable T> {
+    @SuppressWarnings("nullness")
+    final Ordering<? super T> ordering;
+
+    NullsFirstOrdering(Ordering<? super T> ordering) {
+      this.ordering = ordering;
+    }
+
+    @Override
+    public <S extends @Nullable T> Ordering<S> reverse() {
+      // Type inference problem detected in Guava.
+      return ordering.reverse().nullsLast();
+    }
+
+    @Override
+    @SuppressWarnings("nullness") // probably a bug in our checker?
+    public <S extends @Nullable T> Ordering<@Nullable S> nullsLast() {
+      // Type inference problem detected in Guava.
+      return ordering.nullsLast();
+    }
+  }
+}
