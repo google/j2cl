google/j2cl

commit 4fd4871fadca7b337ceb6921fcbffbba873a11f5

[WASM] Make sure String(StringBuilder/StringBuffer) constructors share the same optimizations in AbstractStringBuilder.toString for non-stringref.
PiperOrigin-RevId: 517186802

jre/java/super-wasm-alt/java/lang/String.java
@@ -151,9 +151,17 @@ public final class String implements Serializable, Comparable<String>, CharSeque
   }
 
   public String(StringBuffer stringBuffer) {
+    this((AbstractStringBuilder) stringBuffer);
+  }
+
+  public String(StringBuilder stringBuilder) {
+    this((AbstractStringBuilder) stringBuilder);
+  }
+
+  private String(AbstractStringBuilder builder) {
     offset = 0;
-    value = stringBuffer.shareValue();
-    count = stringBuffer.length();
+    value = builder.shareValue();
+    count = builder.length();
   }
 
   public String(int[] codePoints, int offset, int count) {
@@ -167,13 +175,6 @@ public final class String implements Serializable, Comparable<String>, CharSeque
       c += Character.toChars(codePoints[i], this.value, c);
     }
     this.count = c;
-  }
-
-  public String(StringBuilder stringBuilder) {
-    this.offset = 0;
-    this.count = stringBuilder.length();
-    this.value = new char[this.count];
-    stringBuilder.getChars(0, this.count, this.value, 0);
   }
 
   public char charAt(int index) {

jre/java/super-wasm-alt/java/lang/AbstractStringBuilder.java
@@ -32,10 +32,12 @@ abstract class AbstractStringBuilder {
   final char[] getValue() {
     return value;
   }
-  /*
-   * Returns the underlying buffer and sets the shared flag.
-   */
+
   final char[] shareValue() {
+    int wasted = value.length - count;
+    if (wasted >= 256 || (wasted >= INITIAL_CAPACITY && wasted >= (count >> 1))) {
+      return Arrays.copyOf(value, count);
+    }
     shared = true;
     return value;
   }
@@ -456,13 +458,7 @@ abstract class AbstractStringBuilder {
     if (count == 0) {
       return "";
     }
-    // Optimize String sharing for more performance
-    int wasted = value.length - count;
-    if (wasted >= 256 || (wasted >= INITIAL_CAPACITY && wasted >= (count >> 1))) {
-      return new String(value, 0, count);
-    }
-    shared = true;
-    return new String(0, count, value);
+    return new String(this);
   }
 
   public CharSequence subSequence(int start, int end) {

commit c74f1faaf2840679d38238effe19ee1805b4cc3b

[WASM] Fix visibility accidentally broken in https://github.com/google/j2cl/commit/4fd4871fadca7b337ceb6921fcbffbba873a11f5.
PiperOrigin-RevId: 517428089

jre/java/super-wasm-alt/java/lang/String.java
@@ -158,7 +158,7 @@ public final class String implements Serializable, Comparable<String>, CharSeque
     this((AbstractStringBuilder) stringBuilder);
   }
 
-  private String(AbstractStringBuilder builder) {
+  String(AbstractStringBuilder builder) {
     offset = 0;
     value = builder.shareValue();
     count = builder.length();