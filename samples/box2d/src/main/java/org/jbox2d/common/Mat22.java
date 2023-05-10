8f3ee29a22a97bb0e889ee5210fcb2ea52db1565
Kevin O'Connor <kevinoconnor@google.com>
2023-05-10 08:37:08 -0700

    Remove null check from box2d code
    The fields being null checked are final and are initialized in the constructor.
    They can never be null.
    
    PiperOrigin-RevId: 530917039

--- samples/box2d/src/main/java/org/jbox2d/common/Mat22.java
+++ samples/box2d/src/main/java/org/jbox2d/common/Mat22.java
@@ -560,8 +560,8 @@ public class Mat22 implements Serializable {
   public int hashCode() {
     final int prime = 31;
     int result = 1;
-    result = prime * result + ((ex == null) ? 0 : ex.hashCode());
-    result = prime * result + ((ey == null) ? 0 : ey.hashCode());
+    result = prime * result + ex.hashCode();
+    result = prime * result + ey.hashCode();
     return result;
   }
 
--- samples/box2d/src/main/java/org/jbox2d/common/Mat33.java
+++ samples/box2d/src/main/java/org/jbox2d/common/Mat33.java
@@ -211,9 +211,9 @@ public class Mat33 implements Serializable {
   public int hashCode() {
     final int prime = 31;
     int result = 1;
-    result = prime * result + ((ex == null) ? 0 : ex.hashCode());
-    result = prime * result + ((ey == null) ? 0 : ey.hashCode());
-    result = prime * result + ((ez == null) ? 0 : ez.hashCode());
+    result = prime * result + ex.hashCode();
+    result = prime * result + ey.hashCode();
+    result = prime * result + ez.hashCode();
     return result;
   }
 
