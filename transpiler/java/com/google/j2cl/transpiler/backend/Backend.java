google/j2cl

commit 142d92e988e7b4817bcb8f46a45b04e9c17d6354

[J2KT] Implement NormalizeNumberLiterals pass to use with Kotlin backend.
PiperOrigin-RevId: 542940834

transpiler/java/com/google/j2cl/transpiler/backend/Backend.java
@@ -126,6 +126,7 @@ import com.google.j2cl.transpiler.passes.NormalizeMethodParametersJ2kt;
 import com.google.j2cl.transpiler.passes.NormalizeMultiExpressions;
 import com.google.j2cl.transpiler.passes.NormalizeNativePropertyAccesses;
 import com.google.j2cl.transpiler.passes.NormalizeNullLiterals;
+import com.google.j2cl.transpiler.passes.NormalizeNumberLiterals;
 import com.google.j2cl.transpiler.passes.NormalizeOverlayMembers;
 import com.google.j2cl.transpiler.passes.NormalizeShifts;
 import com.google.j2cl.transpiler.passes.NormalizeStaticMemberQualifiers;
@@ -661,7 +662,6 @@ public enum Backend {
           () -> new NormalizeShifts(/* narrowAllToInt= */ true),
           InsertWideningPrimitiveConversionsJ2kt::new,
           InsertNarrowingPrimitiveConversionsJ2kt::new,
-          NormalizeBasicCastsJ2kt::new,
           ImplementBitLevelOperatorsJ2kt::new,
           InsertQualifierProjectionCasts::new,
           InsertNotNullAssertions::new,
@@ -674,6 +674,10 @@ public enum Backend {
           InsertStringConversionsJ2kt::new,
           NormalizeVariableInitialization::new,
           MakeVariablesFinal::new,
+
+          // Needs to run after NormalizeVariableInitialization.
+          NormalizeNumberLiterals::new,
+          NormalizeBasicCastsJ2kt::new,
 
           // Needs to run after NormalizeVarargParametersJ2kt.
           NormalizeMethodParametersJ2kt::new,

transpiler/java/com/google/j2cl/transpiler/ast/NumberLiteral.java
@@ -75,6 +75,14 @@ public class NumberLiteral extends Literal {
   }
 
   @Override
+  public Precedence getPrecedence() {
+    // Positive number literals have the highest precedence and never need to be parenthesized, but
+    // negative number can be seen as a prefix - on a positive number literal, so their precedence
+    // needs to be PREFIX.
+    return value.doubleValue() < 0 ? Precedence.PREFIX : Precedence.HIGHEST;
+  }
+
+  @Override
   public PrimitiveTypeDescriptor getTypeDescriptor() {
     return typeDescriptor;
   }

transpiler/java/com/google/j2cl/transpiler/backend/kotlin/ExpressionRenderer.kt
@@ -380,8 +380,6 @@ private fun Renderer.typeLiteralSource(typeLiteral: TypeLiteral): Source =
 private fun numberLiteralSource(numberLiteral: NumberLiteral): Source =
   when (numberLiteral.typeDescriptor.toUnboxedType()) {
     PrimitiveTypes.CHAR -> literalSource(numberLiteral.value.toChar())
-    PrimitiveTypes.BYTE -> literalSource(numberLiteral.value.toByte())
-    PrimitiveTypes.SHORT -> literalSource(numberLiteral.value.toShort())
     PrimitiveTypes.INT -> literalSource(numberLiteral.value.toInt())
     PrimitiveTypes.LONG -> literalSource(numberLiteral.value.toLong())
     PrimitiveTypes.FLOAT -> literalSource(numberLiteral.value.toFloat())

transpiler/java/com/google/j2cl/transpiler/backend/kotlin/Source.kt
@@ -15,7 +15,6 @@
  */
 package com.google.j2cl.transpiler.backend.kotlin
 
-import com.google.j2cl.transpiler.backend.kotlin.common.letIf
 import com.google.j2cl.transpiler.backend.kotlin.source.Source
 import com.google.j2cl.transpiler.backend.kotlin.source.commaAndNewLineSeparated
 import com.google.j2cl.transpiler.backend.kotlin.source.commaSeparated
@@ -32,12 +31,6 @@ fun literalSource(it: Boolean): Source = source("$it")
 fun literalSource(it: Char): Source = source(it.literalString)
 
 fun literalSource(it: String): Source = source(it.literalString)
-
-fun literalSource(it: Byte): Source =
-  literalSource(it.toInt()).letIf(it < 0, ::inRoundBrackets).functionCall("toByte")
-
-fun literalSource(it: Short): Source =
-  literalSource(it.toInt()).letIf(it < 0, ::inRoundBrackets).functionCall("toShort")
 
 fun literalSource(it: Int): Source = source("$it")
 

transpiler/java/com/google/j2cl/transpiler/passes/NormalizeNumberLiterals.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.j2cl.transpiler.passes;
+
+import static com.google.j2cl.transpiler.ast.TypeDescriptors.isPrimitiveByte;
+import static com.google.j2cl.transpiler.ast.TypeDescriptors.isPrimitiveShort;
+
+import com.google.j2cl.transpiler.ast.AbstractRewriter;
+import com.google.j2cl.transpiler.ast.CastExpression;
+import com.google.j2cl.transpiler.ast.CompilationUnit;
+import com.google.j2cl.transpiler.ast.Node;
+import com.google.j2cl.transpiler.ast.NumberLiteral;
+import com.google.j2cl.transpiler.ast.PrimitiveTypeDescriptor;
+
+/**
+ * Normalizes number literals by converting {@code byte} and {@code short} literals to {@code int}
+ * with a cast.
+ */
+public class NormalizeNumberLiterals extends NormalizationPass {
+
+  @Override
+  public void applyTo(CompilationUnit compilationUnit) {
+    compilationUnit.accept(
+        new AbstractRewriter() {
+          @Override
+          public Node rewriteNumberLiteral(NumberLiteral numberLiteral) {
+            PrimitiveTypeDescriptor literalTypeDescriptor = numberLiteral.getTypeDescriptor();
+            return isPrimitiveByte(literalTypeDescriptor) || isPrimitiveShort(literalTypeDescriptor)
+                ? CastExpression.newBuilder()
+                    .setExpression(NumberLiteral.fromInt(numberLiteral.getValue().intValue()))
+                    .setCastTypeDescriptor(literalTypeDescriptor)
+                    .build()
+                : numberLiteral;
+          }
+        });
+  }
+}

transpiler/javatests/com/google/j2cl/readable/java/compiletimeconstant/CompileTimeConstant.java
@@ -35,6 +35,12 @@ public class CompileTimeConstant<T> {
   public static final char H = 'A';
   public static final boolean I = G > 100;
 
+  public static final byte MIN_BYTE = -128;
+  public static final short MIN_SHORT = -32768;
+
+  public static final byte MIN_BYTE_WITH_CAST = (byte) -128;
+  public static final short MIN_SHORT_WITH_CAST = (short) -32768;
+
   // Note that currently this only compile time constant for Wasm purposes, not per JLS.
   public static final Class<?> classLiteral = CompileTimeConstant.class;
 

commit a026cd85b8da910a998d0bd82f46029da6ef47b5

[J2KT] Reorder Kotlin passes to avoid regression from https://github.com/google/j2cl/commit/142d92e988e7b4817bcb8f46a45b04e9c17d6354, and update BoxOverloads readable to include regressed case.
PiperOrigin-RevId: 543542840

transpiler/java/com/google/j2cl/transpiler/backend/Backend.java
@@ -655,6 +655,7 @@ public enum Backend {
           () -> new InsertBoxingConversions(/* areBooleanAndDoubleBoxed= */ true),
           NormalizeVarargParametersJ2kt::new,
           NormalizeFieldInitializationJ2kt::new,
+          NormalizeVariableInitialization::new,
           NormalizeLabels::new,
           NormalizeForStatements::new,
           NormalizeSwitchStatementsJ2kt::new,
@@ -662,6 +663,8 @@ public enum Backend {
           () -> new NormalizeShifts(/* narrowAllToInt= */ true),
           InsertWideningPrimitiveConversionsJ2kt::new,
           InsertNarrowingPrimitiveConversionsJ2kt::new,
+          NormalizeNumberLiterals::new,
+          NormalizeBasicCastsJ2kt::new,
           ImplementBitLevelOperatorsJ2kt::new,
           InsertQualifierProjectionCasts::new,
           InsertNotNullAssertions::new,
@@ -672,12 +675,7 @@ public enum Backend {
 
           // Needs to run after non-null assertions are inserted.
           InsertStringConversionsJ2kt::new,
-          NormalizeVariableInitialization::new,
           MakeVariablesFinal::new,
-
-          // Needs to run after NormalizeVariableInitialization.
-          NormalizeNumberLiterals::new,
-          NormalizeBasicCastsJ2kt::new,
 
           // Needs to run after NormalizeVarargParametersJ2kt.
           NormalizeMethodParametersJ2kt::new,

transpiler/javatests/com/google/j2cl/readable/java/j2kt/BoxOverloads.java
@@ -20,6 +20,13 @@ import org.jspecify.nullness.Nullable;
 
 @NullMarked
 public class BoxOverloads {
+  public BoxOverloads(double d) {
+    // Dispatches BoxOverloads(@Nullable Double)
+    this(Double.valueOf(d));
+  }
+
+  public BoxOverloads(@Nullable Double d) {}
+
   public static void nullableBoxedOverload(double d) {}
 
   public static void nullableBoxedOverload(@Nullable Double d) {}