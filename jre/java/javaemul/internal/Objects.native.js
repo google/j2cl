a06c4e25f28bd50dc94042b0a3100184dcd313a4
Goktug Gokdogan <goktug@google.com>
2023-09-14 07:10:56 -0700

    Fix strip tag.
    PiperOrigin-RevId: 565358088

--- jre/java/javaemul/internal/Objects.native.js
+++ jre/java/javaemul/internal/Objects.native.js
@@ -10,14 +10,6 @@ $Objects.throwTypeError = function() {
  */
 $Objects.assertEqualsAndHashCodePresentIfExpected = function(instance) {
   if (goog.DEBUG && instance.equalsAndHashCodeShouldBeAvailable) {
-    // copybara:begin_strip(not available in oss)
-    // This error occurs when `equals` is called on an Apps JSPB proto message
-    // but the monkey-patch which provides `equals` was not loaded. This
-    // typically means that you were passing a `jspb.Message` into J2CL as
-    // an `Object` and should instead use an ImmutableJS message.
-    //
-    // TODO(b/264934765): update this once j2cl proto types are heterogeneous.
-    // copybara:end_strip
     throw new Error('equals and hashcode expected but not defined.');
   }
 };
