9aee5680f3fea311447b9179c6b848b27b383bb6
Googler <noreply@google.com>
2024-01-30 14:24:11 -0800

    [J2KT] Add new integration test for J2KT Native <-> ObjC interop, and cover basic usages.
    PiperOrigin-RevId: 602840872

---
+++ transpiler/javatests/com/google/j2cl/integration/java/j2ktobjcinterop/DefaultNames.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2024 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package j2ktobjcinterop;
+
+public final class DefaultNames {
+  public DefaultNames() {}
+
+  public DefaultNames(int i) {}
+
+  public DefaultNames(int i, String s) {}
+
+  public void method() {}
+
+  public void method(boolean c) {}
+
+  public void method(char c) {}
+
+  public void method(byte b) {}
+
+  public void method(short s) {}
+
+  public void method(int i) {}
+
+  public void method(long l) {}
+
+  public void method(float f) {}
+
+  public void method(double d) {}
+
+  public void method(Object obj) {}
+
+  public void method(String obj) {}
+
+  public void method(String[] sa) {}
+
+  public void method(String[][] saa) {}
+
+  public void method(Cloneable c) {}
+
+  public void method(Number c) {}
+
+  public void method(Class<String> c) {}
+
+  public void method(Iterable<String> i) {}
+
+  public void method(int i, String s) {}
+
+  public <T> void genericMethod(T t) {}
+
+  public <T extends String> void genericMethod(T t) {}
+
+  public int field;
+}
