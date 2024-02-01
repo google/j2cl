gwtproject/gwt

Add Java 10 Optional.orElseThrow() (#9843)
Partial #9547

user/test/com/google/gwt/emultest/EmulJava10Suite.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest;
+
+import com.google.gwt.emultest.java10.util.OptionalDoubleTest;
+import com.google.gwt.emultest.java10.util.OptionalIntTest;
+import com.google.gwt.emultest.java10.util.OptionalLongTest;
+import com.google.gwt.emultest.java10.util.OptionalTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/** Test JRE emulations. */
+@RunWith(Suite.class)
+@SuiteClasses({
+        OptionalDoubleTest.class,
+        OptionalIntTest.class,
+        OptionalLongTest.class,
+        OptionalTest.class,
+})
+public class EmulJava10Suite {
+}

user/test/com/google/gwt/emultest/java10/util/OptionalDoubleTest.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest.java10.util;
+
+import com.google.gwt.emultest.java.util.EmulTestBase;
+
+import java.util.NoSuchElementException;
+import java.util.OptionalDouble;
+
+/**
+ * Tests for java.util.OptionalDouble Java 10 API emulation.
+ */
+public class OptionalDoubleTest extends EmulTestBase {
+  public void testOrElseThrow() {
+    try {
+      OptionalDouble.empty().orElseThrow();
+      fail("Expected NoSuchElementException from empty Optional: orElseThrow");
+    } catch (NoSuchElementException ignore) {
+      // expected
+    }
+
+    double value = OptionalDouble.of(10.0).orElseThrow();
+    assertEquals(10.0, value);
+  }
+}

user/test/com/google/gwt/emultest/java10/util/OptionalIntTest.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest.java10.util;
+
+import com.google.gwt.emultest.java.util.EmulTestBase;
+
+import java.util.NoSuchElementException;
+import java.util.OptionalInt;
+
+/**
+ * Tests for java.util.OptionalInt Java 10 API emulation.
+ */
+public class OptionalIntTest extends EmulTestBase {
+  public void testOrElseThrow() {
+    try {
+      OptionalInt.empty().orElseThrow();
+      fail("Expected NoSuchElementException from empty Optional: orElseThrow");
+    } catch (NoSuchElementException ignore) {
+      // expected
+    }
+
+    int value = OptionalInt.of(10).orElseThrow();
+    assertEquals(10, value);
+  }
+}

user/test/com/google/gwt/emultest/java10/util/OptionalLongTest.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest.java10.util;
+
+import com.google.gwt.emultest.java.util.EmulTestBase;
+
+import java.util.NoSuchElementException;
+import java.util.OptionalLong;
+
+/**
+ * Tests for java.util.OptionalLong Java 10 API emulation.
+ */
+public class OptionalLongTest extends EmulTestBase {
+  public void testOrElseThrow() {
+    try {
+      OptionalLong.empty().orElseThrow();
+      fail("Expected NoSuchElementException from empty Optional: orElseThrow");
+    } catch (NoSuchElementException ignore) {
+      // expected
+    }
+
+    long value = OptionalLong.of(10L).orElseThrow();
+    assertEquals(10L, value);
+  }
+}

user/test/com/google/gwt/emultest/java10/util/OptionalTest.java
@@ -1,0 +1,39 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest.java10.util;
+
+import com.google.gwt.emultest.java.util.EmulTestBase;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+/**
+ * Tests for java.util.Optional Java 10 API emulation.
+ */
+public class OptionalTest extends EmulTestBase {
+  @SuppressWarnings("OptionalOfRedundantMethod")
+  public void testOrElseThrow() {
+    try {
+      Optional.empty().orElseThrow();
+      fail("Expected NoSuchElementException from empty Optional: orElseThrow");
+    } catch (NoSuchElementException ignore) {
+      // expected
+    }
+
+    String value = Optional.of("value").orElseThrow();
+    assertEquals("value", value);
+  }
+}

google/j2cl

(cherry picked from commit a9aeadd1382da173389d5313d2d24f8cf38007f5)

jre/javatests/com/google/j2cl/jre/EmulJava10Suite.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Google Inc.
+ * Copyright 2023 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -15,21 +15,21 @@
  */
 package com.google.j2cl.jre;
 
+import com.google.j2cl.jre.java10.util.OptionalDoubleTest;
+import com.google.j2cl.jre.java10.util.OptionalIntTest;
+import com.google.j2cl.jre.java10.util.OptionalLongTest;
+import com.google.j2cl.jre.java10.util.OptionalTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-/** TestSuite for all of GWT's emul suites. */
+/** Test JRE emulations. */
 @RunWith(Suite.class)
 @SuiteClasses({
-  BigDecimalSuite.class,
-  BigIntegerSuite.class,
-  CollectionsSuite.class,
-  ConcurrentSuite.class,
-  EmulSuite.class,
-  EmulJava8Suite.class,
-  EmulJava9Suite.class,
-  EmulJava10Suite.class,
-  LangSuite.class,
+        OptionalDoubleTest.class,
+        OptionalIntTest.class,
+        OptionalLongTest.class,
+        OptionalTest.class,
 })
-public class AllTests {}
+public class EmulJava10Suite {
+}

jre/javatests/com/google/j2cl/jre/java10/util/OptionalDoubleTest.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.j2cl.jre.java10.util;
+
+import com.google.j2cl.jre.java.util.EmulTestBase;
+
+import java.util.NoSuchElementException;
+import java.util.OptionalDouble;
+
+/**
+ * Tests for java.util.OptionalDouble Java 10 API emulation.
+ */
+public class OptionalDoubleTest extends EmulTestBase {
+  public void testOrElseThrow() {
+    try {
+      OptionalDouble.empty().orElseThrow();
+      fail("Expected NoSuchElementException from empty Optional: orElseThrow");
+    } catch (NoSuchElementException ignore) {
+      // expected
+    }
+
+    double value = OptionalDouble.of(10.0).orElseThrow();
+    assertEquals(10.0, value);
+  }
+}

jre/javatests/com/google/j2cl/jre/java10/util/OptionalIntTest.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.j2cl.jre.java10.util;
+
+import com.google.j2cl.jre.java.util.EmulTestBase;
+
+import java.util.NoSuchElementException;
+import java.util.OptionalInt;
+
+/**
+ * Tests for java.util.OptionalInt Java 10 API emulation.
+ */
+public class OptionalIntTest extends EmulTestBase {
+  public void testOrElseThrow() {
+    try {
+      OptionalInt.empty().orElseThrow();
+      fail("Expected NoSuchElementException from empty Optional: orElseThrow");
+    } catch (NoSuchElementException ignore) {
+      // expected
+    }
+
+    int value = OptionalInt.of(10).orElseThrow();
+    assertEquals(10, value);
+  }
+}

jre/javatests/com/google/j2cl/jre/java10/util/OptionalLongTest.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.j2cl.jre.java10.util;
+
+import com.google.j2cl.jre.java.util.EmulTestBase;
+
+import java.util.NoSuchElementException;
+import java.util.OptionalLong;
+
+/**
+ * Tests for java.util.OptionalLong Java 10 API emulation.
+ */
+public class OptionalLongTest extends EmulTestBase {
+  public void testOrElseThrow() {
+    try {
+      OptionalLong.empty().orElseThrow();
+      fail("Expected NoSuchElementException from empty Optional: orElseThrow");
+    } catch (NoSuchElementException ignore) {
+      // expected
+    }
+
+    long value = OptionalLong.of(10L).orElseThrow();
+    assertEquals(10L, value);
+  }
+}

jre/javatests/com/google/j2cl/jre/java10/util/OptionalTest.java
@@ -1,0 +1,39 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.j2cl.jre.java10.util;
+
+import com.google.j2cl.jre.java.util.EmulTestBase;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+/**
+ * Tests for java.util.Optional Java 10 API emulation.
+ */
+public class OptionalTest extends EmulTestBase {
+  @SuppressWarnings("OptionalOfRedundantMethod")
+  public void testOrElseThrow() {
+    try {
+      Optional.empty().orElseThrow();
+      fail("Expected NoSuchElementException from empty Optional: orElseThrow");
+    } catch (NoSuchElementException ignore) {
+      // expected
+    }
+
+    String value = Optional.of("value").orElseThrow();
+    assertEquals("value", value);
+  }
+}