gwtproject/gwt

Add Java 9 Optional APIs (#9838)
Partial #9547

user/test/com/google/gwt/emultest/EmulJava9Suite.java
@@ -17,6 +17,10 @@ package com.google.gwt.emultest;
 
 import com.google.gwt.emultest.java9.util.ListTest;
 import com.google.gwt.emultest.java9.util.MapTest;
+import com.google.gwt.emultest.java9.util.OptionalDoubleTest;
+import com.google.gwt.emultest.java9.util.OptionalIntTest;
+import com.google.gwt.emultest.java9.util.OptionalLongTest;
+import com.google.gwt.emultest.java9.util.OptionalTest;
 import com.google.gwt.emultest.java9.util.SetTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -26,8 +30,12 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
         ListTest.class,
-        SetTest.class,
-        MapTest.class
+        MapTest.class,
+        OptionalDoubleTest.class,
+        OptionalIntTest.class,
+        OptionalLongTest.class,
+        OptionalTest.class,
+        SetTest.class
 })
 public class EmulJava9Suite {
 }

user/test/com/google/gwt/emultest/java9/util/OptionalDoubleTest.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest.java9.util;
+
+import com.google.gwt.emultest.java.util.EmulTestBase;
+
+import java.util.OptionalDouble;
+import java.util.stream.Stream;
+
+/**
+ * Tests for java.util.OptionalDouble Java 9 API emulation.
+ */
+public class OptionalDoubleTest extends EmulTestBase {
+  public void testIfPresentOrElse() {
+    int[] called = {0};
+    OptionalDouble.of(10.0).ifPresentOrElse(value -> {
+      assertEquals(10.0, value);
+      called[0]++;
+    }, () -> {
+      fail("should not call empty action");
+    });
+    assertEquals(1, called[0]);
+    called[0] = 0;
+    OptionalDouble.empty().ifPresentOrElse(ignore -> {
+      fail("Should not call present action");
+    }, () -> called[0]++);
+    assertEquals(1, called[0]);
+  }
+
+  public void testStream() {
+    assertEquals(0, OptionalDouble.empty().stream().count());
+    assertEquals(1, OptionalDouble.of(10.0).stream().count());
+
+    assertEquals(
+        new double[] {10.0, 100.0, 1000.0},
+        Stream.of(
+            OptionalDouble.of(10.0),
+            OptionalDouble.empty(),
+            OptionalDouble.of(100.0),
+            OptionalDouble.of(1000.0)
+        ).flatMapToDouble(OptionalDouble::stream).toArray()
+    );
+  }
+}

user/test/com/google/gwt/emultest/java9/util/OptionalIntTest.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest.java9.util;
+
+import com.google.gwt.emultest.java.util.EmulTestBase;
+
+import java.util.OptionalInt;
+import java.util.stream.Stream;
+
+/**
+ * Tests for java.util.OptionalInt Java 9 API emulation.
+ */
+public class OptionalIntTest extends EmulTestBase {
+  public void testIfPresentOrElse() {
+    int[] called = {0};
+    OptionalInt.of(10).ifPresentOrElse(value -> {
+      assertEquals(10, value);
+      called[0]++;
+    }, () -> {
+      fail("should not call empty action");
+    });
+    assertEquals(1, called[0]);
+    called[0] = 0;
+    OptionalInt.empty().ifPresentOrElse(ignore -> {
+      fail("Should not call present action");
+    }, () -> called[0]++);
+    assertEquals(1, called[0]);
+  }
+
+  public void testStream() {
+    assertEquals(0, OptionalInt.empty().stream().count());
+    assertEquals(1, OptionalInt.of(10).stream().count());
+
+    assertEquals(
+        new int[] {10, 100, 1000},
+        Stream.of(
+            OptionalInt.of(10),
+            OptionalInt.empty(),
+            OptionalInt.of(100),
+            OptionalInt.of(1000)
+        ).flatMapToInt(OptionalInt::stream).toArray()
+    );
+  }
+}

user/test/com/google/gwt/emultest/java9/util/OptionalLongTest.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest.java9.util;
+
+import com.google.gwt.emultest.java.util.EmulTestBase;
+
+import java.util.OptionalLong;
+import java.util.stream.Stream;
+
+/**
+ * Tests for java.util.OptionalLong Java 9 API emulation.
+ */
+public class OptionalLongTest extends EmulTestBase {
+  public void testIfPresentOrElse() {
+    int[] called = {0};
+    OptionalLong.of(10).ifPresentOrElse(value -> {
+      assertEquals(10, value);
+      called[0]++;
+    }, () -> {
+      fail("should not call empty action");
+    });
+    assertEquals(1, called[0]);
+    called[0] = 0;
+    OptionalLong.empty().ifPresentOrElse(ignore -> {
+      fail("Should not call present action");
+    }, () -> called[0]++);
+    assertEquals(1, called[0]);
+  }
+
+  public void testStream() {
+    assertEquals(0, OptionalLong.empty().stream().count());
+    assertEquals(1, OptionalLong.of(10).stream().count());
+
+    assertEquals(
+        new long[] {10, 100, 1000},
+        Stream.of(
+            OptionalLong.of(10),
+            OptionalLong.empty(),
+            OptionalLong.of(100),
+            OptionalLong.of(1000)
+        ).flatMapToLong(OptionalLong::stream).toArray()
+    );
+  }
+}

user/test/com/google/gwt/emultest/java9/util/OptionalTest.java
@@ -1,0 +1,86 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest.java9.util;
+
+import com.google.gwt.emultest.java.util.EmulTestBase;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * Tests for java.util.Optional Java 9 API emulation.
+ */
+public class OptionalTest extends EmulTestBase {
+  public void testIfPresentOrElse() {
+    int[] called = {0};
+    Optional.of("value").ifPresentOrElse(value -> {
+      assertEquals("value", value);
+      called[0]++;
+    }, () -> {
+      fail("should not call empty action");
+    });
+    assertEquals(1, called[0]);
+    called[0] = 0;
+    Optional.empty().ifPresentOrElse(ignore -> {
+      fail("Should not call present action");
+    }, () -> called[0]++);
+    assertEquals(1, called[0]);
+  }
+
+  public void testOr() {
+    Optional<String> or = Optional.of("value").or(() -> Optional.of("replacement"));
+    assertTrue(or.isPresent());
+    assertEquals("value", or.get());
+
+    or = Optional.<String>empty().or(() -> Optional.of("replacement"));
+    assertTrue(or.isPresent());
+    assertEquals("replacement", or.get());
+
+    or = Optional.of("value").or(() -> Optional.empty());
+    assertTrue(or.isPresent());
+    assertEquals("value", or.get());
+
+    or = Optional.<String>empty().or(() -> Optional.empty());
+    assertFalse(or.isPresent());
+
+    Optional<Object> empty = Optional.empty();
+    Optional<Object> present = Optional.of("asdf");
+    assertNPE("empty().or(null)", () -> {
+      empty.or(null);
+    });
+    assertNPE("present.or(null)", () -> {
+      present.or(null);
+    });
+    assertNPE("empty.or(() -> null)", () -> {
+      empty.or(() -> null);
+    });
+  }
+
+  public void testStream() {
+    assertEquals(0, Optional.empty().stream().count());
+    assertEquals(1, Optional.of("foo").stream().count());
+
+    assertEquals(
+        new String[] {"a", "b", "c"},
+        Stream.of(
+            Optional.of("a"),
+            Optional.empty(),
+            Optional.of("b"),
+            Optional.of("c")
+        ).flatMap(Optional::stream).toArray(String[]::new)
+    );
+  }
+}

google/j2cl

(cherry picked from commit 9b9e888bfc5c8991425da65b0e2f12577d0ac638)

jre/javatests/com/google/j2cl/jre/EmulJava9Suite.java
@@ -19,6 +19,10 @@ package com.google.j2cl.jre;
 import com.google.j2cl.jre.java9.util.ListTest;
 import com.google.j2cl.jre.java9.util.MapTest;
 import com.google.j2cl.jre.java9.util.SetTest;
+import com.google.j2cl.jre.java9.util.OptionalDoubleTest;
+import com.google.j2cl.jre.java9.util.OptionalIntTest;
+import com.google.j2cl.jre.java9.util.OptionalLongTest;
+import com.google.j2cl.jre.java9.util.OptionalTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -27,8 +31,12 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
         ListTest.class,
-        SetTest.class,
-        MapTest.class
+        MapTest.class,
+        OptionalDoubleTest.class,
+        OptionalIntTest.class,
+        OptionalLongTest.class,
+        OptionalTest.class,
+        SetTest.class
 })
 public class EmulJava9Suite {
 }

jre/javatests/com/google/j2cl/jre/java9/util/OptionalDoubleTest.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.j2cl.jre.java9.util;
+
+import com.google.j2cl.jre.java.util.EmulTestBase;
+
+import java.util.OptionalDouble;
+import java.util.stream.Stream;
+
+/**
+ * Tests for java.util.OptionalDouble Java 9 API emulation.
+ */
+public class OptionalDoubleTest extends EmulTestBase {
+  public void testIfPresentOrElse() {
+    int[] called = {0};
+    OptionalDouble.of(10.0).ifPresentOrElse(value -> {
+      assertEquals(10.0, value);
+      called[0]++;
+    }, () -> {
+      fail("should not call empty action");
+    });
+    assertEquals(1, called[0]);
+    called[0] = 0;
+    OptionalDouble.empty().ifPresentOrElse(ignore -> {
+      fail("Should not call present action");
+    }, () -> called[0]++);
+    assertEquals(1, called[0]);
+  }
+
+  public void testStream() {
+    assertEquals(0, OptionalDouble.empty().stream().count());
+    assertEquals(1, OptionalDouble.of(10.0).stream().count());
+
+    assertEquals(
+        new double[] {10.0, 100.0, 1000.0},
+        Stream.of(
+            OptionalDouble.of(10.0),
+            OptionalDouble.empty(),
+            OptionalDouble.of(100.0),
+            OptionalDouble.of(1000.0)
+        ).flatMapToDouble(OptionalDouble::stream).toArray()
+    );
+  }
+}

jre/javatests/com/google/j2cl/jre/java9/util/OptionalIntTest.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.j2cl.jre.java9.util;
+
+import com.google.j2cl.jre.java.util.EmulTestBase;
+
+import java.util.OptionalInt;
+import java.util.stream.Stream;
+
+/**
+ * Tests for java.util.OptionalInt Java 9 API emulation.
+ */
+public class OptionalIntTest extends EmulTestBase {
+  public void testIfPresentOrElse() {
+    int[] called = {0};
+    OptionalInt.of(10).ifPresentOrElse(value -> {
+      assertEquals(10, value);
+      called[0]++;
+    }, () -> {
+      fail("should not call empty action");
+    });
+    assertEquals(1, called[0]);
+    called[0] = 0;
+    OptionalInt.empty().ifPresentOrElse(ignore -> {
+      fail("Should not call present action");
+    }, () -> called[0]++);
+    assertEquals(1, called[0]);
+  }
+
+  public void testStream() {
+    assertEquals(0, OptionalInt.empty().stream().count());
+    assertEquals(1, OptionalInt.of(10).stream().count());
+
+    assertEquals(
+        new int[] {10, 100, 1000},
+        Stream.of(
+            OptionalInt.of(10),
+            OptionalInt.empty(),
+            OptionalInt.of(100),
+            OptionalInt.of(1000)
+        ).flatMapToInt(OptionalInt::stream).toArray()
+    );
+  }
+}

jre/javatests/com/google/j2cl/jre/java9/util/OptionalLongTest.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.j2cl.jre.java9.util;
+
+import com.google.j2cl.jre.java.util.EmulTestBase;
+
+import java.util.OptionalLong;
+import java.util.stream.Stream;
+
+/**
+ * Tests for java.util.OptionalLong Java 9 API emulation.
+ */
+public class OptionalLongTest extends EmulTestBase {
+  public void testIfPresentOrElse() {
+    int[] called = {0};
+    OptionalLong.of(10).ifPresentOrElse(value -> {
+      assertEquals(10, value);
+      called[0]++;
+    }, () -> {
+      fail("should not call empty action");
+    });
+    assertEquals(1, called[0]);
+    called[0] = 0;
+    OptionalLong.empty().ifPresentOrElse(ignore -> {
+      fail("Should not call present action");
+    }, () -> called[0]++);
+    assertEquals(1, called[0]);
+  }
+
+  public void testStream() {
+    assertEquals(0, OptionalLong.empty().stream().count());
+    assertEquals(1, OptionalLong.of(10).stream().count());
+
+    assertEquals(
+        new long[] {10, 100, 1000},
+        Stream.of(
+            OptionalLong.of(10),
+            OptionalLong.empty(),
+            OptionalLong.of(100),
+            OptionalLong.of(1000)
+        ).flatMapToLong(OptionalLong::stream).toArray()
+    );
+  }
+}

jre/javatests/com/google/j2cl/jre/java9/util/OptionalTest.java
@@ -1,0 +1,86 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.j2cl.jre.java9.util;
+
+import com.google.j2cl.jre.java.util.EmulTestBase;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * Tests for java.util.Optional Java 9 API emulation.
+ */
+public class OptionalTest extends EmulTestBase {
+  public void testIfPresentOrElse() {
+    int[] called = {0};
+    Optional.of("value").ifPresentOrElse(value -> {
+      assertEquals("value", value);
+      called[0]++;
+    }, () -> {
+      fail("should not call empty action");
+    });
+    assertEquals(1, called[0]);
+    called[0] = 0;
+    Optional.empty().ifPresentOrElse(ignore -> {
+      fail("Should not call present action");
+    }, () -> called[0]++);
+    assertEquals(1, called[0]);
+  }
+
+  public void testOr() {
+    Optional<String> or = Optional.of("value").or(() -> Optional.of("replacement"));
+    assertTrue(or.isPresent());
+    assertEquals("value", or.get());
+
+    or = Optional.<String>empty().or(() -> Optional.of("replacement"));
+    assertTrue(or.isPresent());
+    assertEquals("replacement", or.get());
+
+    or = Optional.of("value").or(() -> Optional.empty());
+    assertTrue(or.isPresent());
+    assertEquals("value", or.get());
+
+    or = Optional.<String>empty().or(() -> Optional.empty());
+    assertFalse(or.isPresent());
+
+    Optional<Object> empty = Optional.empty();
+    Optional<Object> present = Optional.of("asdf");
+    assertNPE("empty().or(null)", () -> {
+      empty.or(null);
+    });
+    assertNPE("present.or(null)", () -> {
+      present.or(null);
+    });
+    assertNPE("empty.or(() -> null)", () -> {
+      empty.or(() -> null);
+    });
+  }
+
+  public void testStream() {
+    assertEquals(0, Optional.empty().stream().count());
+    assertEquals(1, Optional.of("foo").stream().count());
+
+    assertEquals(
+        new String[] {"a", "b", "c"},
+        Stream.of(
+            Optional.of("a"),
+            Optional.empty(),
+            Optional.of("b"),
+            Optional.of("c")
+        ).flatMap(Optional::stream).toArray(String[]::new)
+    );
+  }
+}