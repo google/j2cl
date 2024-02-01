gwtproject/gwt

Add Java 10 Collections APIs (#9904)
Java 10 added copyOf static factory methods to produce immutable
List/Set/Map instances that contain no nulls.

Follow-up #9547

user/test/com/google/gwt/emultest/EmulJava10Suite.java
@@ -19,6 +19,9 @@ import com.google.gwt.emultest.java10.util.OptionalDoubleTest;
 import com.google.gwt.emultest.java10.util.OptionalIntTest;
 import com.google.gwt.emultest.java10.util.OptionalLongTest;
 import com.google.gwt.emultest.java10.util.OptionalTest;
+import com.google.gwt.emultest.java10.util.ListTest;
+import com.google.gwt.emultest.java10.util.MapTest;
+import com.google.gwt.emultest.java10.util.SetTest;
 import com.google.gwt.emultest.java10.util.stream.CollectorsTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -28,10 +31,13 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
         CollectorsTest.class,
+        ListTest.class,
+        MapTest.class,
         OptionalDoubleTest.class,
         OptionalIntTest.class,
         OptionalLongTest.class,
         OptionalTest.class,
+        SetTest.class,
 })
 public class EmulJava10Suite {
 }

google/j2cl

(cherry picked from commit f05b257fb480707aac412b227907a0383c913e70)

jre/javatests/com/google/j2cl/jre/EmulJava10Suite.java
@@ -19,6 +19,9 @@ import com.google.j2cl.jre.java10.util.OptionalDoubleTest;
 import com.google.j2cl.jre.java10.util.OptionalIntTest;
 import com.google.j2cl.jre.java10.util.OptionalLongTest;
 import com.google.j2cl.jre.java10.util.OptionalTest;
+import com.google.j2cl.jre.java10.util.ListTest;
+import com.google.j2cl.jre.java10.util.MapTest;
+import com.google.j2cl.jre.java10.util.SetTest;
 import com.google.j2cl.jre.java10.util.stream.CollectorsTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -28,10 +31,13 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
         CollectorsTest.class,
+        ListTest.class,
+        MapTest.class,
         OptionalDoubleTest.class,
         OptionalIntTest.class,
         OptionalLongTest.class,
         OptionalTest.class,
+        SetTest.class,
 })
 public class EmulJava10Suite {
 }