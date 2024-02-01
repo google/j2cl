gwtproject/gwt

Add Java 11 Optional.isEmpty() (#9853)
Partial #9547
Co-authored-by: Alexey Abashev <alexey@abashev.ru>

user/test/com/google/gwt/emultest/EmulJava11Suite.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest;
+
+import com.google.gwt.emultest.java11.util.OptionalDoubleTest;
+import com.google.gwt.emultest.java11.util.OptionalIntTest;
+import com.google.gwt.emultest.java11.util.OptionalLongTest;
+import com.google.gwt.emultest.java11.util.OptionalTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/** Test JRE emulations. */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        OptionalDoubleTest.class,
+        OptionalIntTest.class,
+        OptionalLongTest.class,
+        OptionalTest.class,
+})
+public class EmulJava11Suite {
+}

user/test/com/google/gwt/emultest/java11/util/OptionalDoubleTest.java
@@ -1,0 +1,30 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest.java11.util;
+
+import com.google.gwt.emultest.java.util.EmulTestBase;
+
+import java.util.OptionalDouble;
+
+/**
+ * Tests for java.util.OptionalDouble Java 11 API emulation.
+ */
+public class OptionalDoubleTest extends EmulTestBase {
+  public void testIsEmpty() {
+    assertTrue(OptionalDouble.empty().isEmpty());
+    assertFalse(OptionalDouble.of(78.9).isEmpty());
+  }
+}

user/test/com/google/gwt/emultest/java11/util/OptionalIntTest.java
@@ -1,0 +1,30 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest.java11.util;
+
+import com.google.gwt.emultest.java.util.EmulTestBase;
+
+import java.util.OptionalInt;
+
+/**
+ * Tests for java.util.OptionalInt Java 11 API emulation.
+ */
+public class OptionalIntTest extends EmulTestBase {
+  public void testIsEmpty() {
+    assertTrue(OptionalInt.empty().isEmpty());
+    assertFalse(OptionalInt.of(456).isEmpty());
+  }
+}

user/test/com/google/gwt/emultest/java11/util/OptionalLongTest.java
@@ -1,0 +1,30 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest.java11.util;
+
+import com.google.gwt.emultest.java.util.EmulTestBase;
+
+import java.util.OptionalLong;
+
+/**
+ * Tests for java.util.OptionalLong Java 11 API emulation.
+ */
+public class OptionalLongTest extends EmulTestBase {
+  public void testIsEmpty() {
+    assertTrue(OptionalLong.empty().isEmpty());
+    assertFalse(OptionalLong.of(123L).isEmpty());
+  }
+}

user/test/com/google/gwt/emultest/java11/util/OptionalTest.java
@@ -1,0 +1,30 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest.java11.util;
+
+import com.google.gwt.emultest.java.util.EmulTestBase;
+
+import java.util.Optional;
+
+/**
+ * Tests for java.util.Optional Java 11 API emulation.
+ */
+public class OptionalTest extends EmulTestBase {
+  public void testIsEmpty() {
+    assertTrue(Optional.empty().isEmpty());
+    assertFalse(Optional.of(this).isEmpty());
+  }
+}

google/j2cl

(cherry picked from commit d73d095eabcf18d565ee9eebacdaa2895ae77c52)

jre/javatests/com/google/j2cl/jre/EmulJava11Suite.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Google Inc.
+ * Copyright 2023 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -15,22 +15,20 @@
  */
 package com.google.j2cl.jre;
 
+import com.google.j2cl.jre.java11.util.OptionalDoubleTest;
+import com.google.j2cl.jre.java11.util.OptionalIntTest;
+import com.google.j2cl.jre.java11.util.OptionalLongTest;
+import com.google.j2cl.jre.java11.util.OptionalTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
 
-/** TestSuite for all of GWT's emul suites. */
+/** Test JRE emulations. */
 @RunWith(Suite.class)
-@SuiteClasses({
-  BigDecimalSuite.class,
-  BigIntegerSuite.class,
-  CollectionsSuite.class,
-  ConcurrentSuite.class,
-  EmulSuite.class,
-  EmulJava8Suite.class,
-  EmulJava9Suite.class,
-  EmulJava10Suite.class,
-  EmulJava11Suite.class,
-  LangSuite.class,
+@Suite.SuiteClasses({
+        OptionalDoubleTest.class,
+        OptionalIntTest.class,
+        OptionalLongTest.class,
+        OptionalTest.class,
 })
-public class AllTests {}
+public class EmulJava11Suite {
+}

jre/javatests/com/google/j2cl/jre/java11/util/OptionalDoubleTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Google Inc.
+ * Copyright 2023 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -13,24 +13,18 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.google.j2cl.jre;
+package com.google.j2cl.jre.java11.util;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+import com.google.j2cl.jre.java.util.EmulTestBase;
 
-/** TestSuite for all of GWT's emul suites. */
-@RunWith(Suite.class)
-@SuiteClasses({
-  BigDecimalSuite.class,
-  BigIntegerSuite.class,
-  CollectionsSuite.class,
-  ConcurrentSuite.class,
-  EmulSuite.class,
-  EmulJava8Suite.class,
-  EmulJava9Suite.class,
-  EmulJava10Suite.class,
-  EmulJava11Suite.class,
-  LangSuite.class,
-})
-public class AllTests {}
+import java.util.OptionalDouble;
+
+/**
+ * Tests for java.util.OptionalDouble Java 11 API emulation.
+ */
+public class OptionalDoubleTest extends EmulTestBase {
+  public void testIsEmpty() {
+    assertTrue(OptionalDouble.empty().isEmpty());
+    assertFalse(OptionalDouble.of(78.9).isEmpty());
+  }
+}

jre/javatests/com/google/j2cl/jre/java11/util/OptionalIntTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Google Inc.
+ * Copyright 2023 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -13,24 +13,18 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.google.j2cl.jre;
+package com.google.j2cl.jre.java11.util;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+import com.google.j2cl.jre.java.util.EmulTestBase;
 
-/** TestSuite for all of GWT's emul suites. */
-@RunWith(Suite.class)
-@SuiteClasses({
-  BigDecimalSuite.class,
-  BigIntegerSuite.class,
-  CollectionsSuite.class,
-  ConcurrentSuite.class,
-  EmulSuite.class,
-  EmulJava8Suite.class,
-  EmulJava9Suite.class,
-  EmulJava10Suite.class,
-  EmulJava11Suite.class,
-  LangSuite.class,
-})
-public class AllTests {}
+import java.util.OptionalInt;
+
+/**
+ * Tests for java.util.OptionalInt Java 11 API emulation.
+ */
+public class OptionalIntTest extends EmulTestBase {
+  public void testIsEmpty() {
+    assertTrue(OptionalInt.empty().isEmpty());
+    assertFalse(OptionalInt.of(456).isEmpty());
+  }
+}

jre/javatests/com/google/j2cl/jre/java11/util/OptionalLongTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Google Inc.
+ * Copyright 2023 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -13,24 +13,18 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.google.j2cl.jre;
+package com.google.j2cl.jre.java11.util;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+import com.google.j2cl.jre.java.util.EmulTestBase;
 
-/** TestSuite for all of GWT's emul suites. */
-@RunWith(Suite.class)
-@SuiteClasses({
-  BigDecimalSuite.class,
-  BigIntegerSuite.class,
-  CollectionsSuite.class,
-  ConcurrentSuite.class,
-  EmulSuite.class,
-  EmulJava8Suite.class,
-  EmulJava9Suite.class,
-  EmulJava10Suite.class,
-  EmulJava11Suite.class,
-  LangSuite.class,
-})
-public class AllTests {}
+import java.util.OptionalLong;
+
+/**
+ * Tests for java.util.OptionalLong Java 11 API emulation.
+ */
+public class OptionalLongTest extends EmulTestBase {
+  public void testIsEmpty() {
+    assertTrue(OptionalLong.empty().isEmpty());
+    assertFalse(OptionalLong.of(123L).isEmpty());
+  }
+}

jre/javatests/com/google/j2cl/jre/java11/util/OptionalTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Google Inc.
+ * Copyright 2023 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -13,24 +13,18 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.google.j2cl.jre;
+package com.google.j2cl.jre.java11.util;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+import com.google.j2cl.jre.java.util.EmulTestBase;
 
-/** TestSuite for all of GWT's emul suites. */
-@RunWith(Suite.class)
-@SuiteClasses({
-  BigDecimalSuite.class,
-  BigIntegerSuite.class,
-  CollectionsSuite.class,
-  ConcurrentSuite.class,
-  EmulSuite.class,
-  EmulJava8Suite.class,
-  EmulJava9Suite.class,
-  EmulJava10Suite.class,
-  EmulJava11Suite.class,
-  LangSuite.class,
-})
-public class AllTests {}
+import java.util.Optional;
+
+/**
+ * Tests for java.util.Optional Java 11 API emulation.
+ */
+public class OptionalTest extends EmulTestBase {
+  public void testIsEmpty() {
+    assertTrue(Optional.empty().isEmpty());
+    assertFalse(Optional.of(this).isEmpty());
+  }
+}