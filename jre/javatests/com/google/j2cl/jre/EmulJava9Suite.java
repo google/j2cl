gwtproject/gwt

Update Collections to support changes made in Java 9 (#9833)
See earlier review at https://gwt-review.googlesource.com/c/gwt/+/21501.

Build changes get us ready for missing Java 10 and 11 emulation as
well.

Partial #9547

user/test/com/google/gwt/emultest/EmulJava9Suite.java
@@ -1,0 +1,33 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest;
+
+import com.google.gwt.emultest.java9.util.ListTest;
+import com.google.gwt.emultest.java9.util.MapTest;
+import com.google.gwt.emultest.java9.util.SetTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/** Test JRE emulations. */
+@RunWith(Suite.class)
+@SuiteClasses({
+        ListTest.class,
+        SetTest.class,
+        MapTest.class
+})
+public class EmulJava9Suite {
+}

user/test/com/google/gwt/emultest/java9/util/ListTest.java
@@ -1,0 +1,141 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest.java9.util;
+
+import com.google.gwt.emultest.java.util.EmulTestBase;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Tests for java.util.List Java 9 API emulation.
+ */
+public class ListTest extends EmulTestBase {
+
+  public void testOf() {
+    assertIsImmutableListOf(List.of());
+    assertIsImmutableListOf(List.of("a"), "a");
+    assertIsImmutableListOf(
+        List.of("a", "b"),
+        "a", "b"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c"),
+        "a", "b", "c"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c", "d"),
+        "a", "b", "c", "d"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c", "d", "e"),
+        "a", "b", "c", "d", "e"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c", "d", "e", "f"),
+        "a", "b", "c", "d", "e", "f"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c", "d", "e", "f", "g"),
+        "a", "b", "c", "d", "e", "f", "g"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c", "d", "e", "f", "g", "h"),
+        "a", "b", "c", "d", "e", "f", "g", "h"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c", "d", "e", "f", "g", "h", "i"),
+        "a", "b", "c", "d", "e", "f", "g", "h", "i"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j"),
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"),
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"
+    );
+
+    // ensure that NPE is thrown if a value is null
+    assertNPE("of", () -> List.of((String) null));
+    assertNPE("of", () -> List.of("a", null));
+    assertNPE("of", () -> List.of("a", "b", null));
+    assertNPE("of", () -> List.of("a", "b", "c", null));
+    assertNPE("of", () -> List.of("a", "b", "c", "d", null));
+    assertNPE("of", () -> List.of("a", "b", "c", "d", "e", null));
+    assertNPE("of", () -> List.of("a", "b", "c", "d", "e", "f", null));
+    assertNPE("of", () -> List.of("a", "b", "c", "d", "e", "f", "g", null));
+    assertNPE("of", () -> List.of("a", "b", "c", "d", "e", "f", "g", "h", null));
+    assertNPE("of", () -> List.of("a", "b", "c", "d", "e", "f", "g", "h", "i", null));
+    assertNPE("of", () -> List.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", null));
+  }
+
+  protected static void assertIsImmutableListOf(List<String> list, String... contents) {
+    assertEquals(contents, list);
+
+    // quick test that the list impl is sane
+    if (contents.length == 0) {
+      assertFalse(list.iterator().hasNext());
+    } else {
+      Iterator<String> itr = list.iterator();
+      assertTrue(itr.hasNext());
+      assertEquals(contents[0], itr.next());
+      assertEquals(contents.length > 1, itr.hasNext());
+    }
+
+    // quick check that the list is immutable
+    try {
+      list.add("another item");
+      fail("List should be unmodifiable: add(T)");
+    } catch (UnsupportedOperationException ignored) {
+      // success
+    }
+
+    try {
+      list.remove(0);
+      fail("List should be unmodifiable: remove(int)");
+    } catch (UnsupportedOperationException ignored) {
+      // success
+    }
+
+    // if any, remove an item actually in the list
+    if (contents.length > 0) {
+      // Without any items, remove(T) defaults to iterating items present, so we only test from
+      // present items
+      try {
+        list.remove(contents[0]);
+        fail("List should be unmodifiable: remove(T)");
+      } catch (UnsupportedOperationException ignored) {
+        // success
+      }
+    }
+
+    // Remove an item that will not be in the list
+    try {
+      list.remove("not present");
+      fail("List should be unmodifiable: remove(T)");
+    } catch (UnsupportedOperationException ignored) {
+      // success
+    }
+
+    try {
+      list.clear();
+      fail("List should be unmodifiable: clear()");
+    } catch (UnsupportedOperationException ignored) {
+      // success
+    }
+  }
+}

user/test/com/google/gwt/emultest/java9/util/SetTest.java
@@ -1,0 +1,162 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.emultest.java9.util;
+
+import com.google.gwt.emultest.java.util.EmulTestBase;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Tests for java.util.Set Java 9 API emulation.
+ */
+public class SetTest extends EmulTestBase {
+
+  public void testOf() {
+    assertIsImmutableSetOf(Set.of());
+    assertIsImmutableSetOf(Set.of("a"), "a");
+    assertIsImmutableSetOf(
+        Set.of("a", "b"),
+        "a", "b"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c"),
+        "a", "b", "c"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c", "d"),
+        "a", "b", "c", "d"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c", "d", "e"),
+        "a", "b", "c", "d", "e"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c", "d", "e", "f"),
+        "a", "b", "c", "d", "e", "f"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c", "d", "e", "f", "g"),
+        "a", "b", "c", "d", "e", "f", "g"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c", "d", "e", "f", "g", "h"),
+        "a", "b", "c", "d", "e", "f", "g", "h"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c", "d", "e", "f", "g", "h", "i"),
+        "a", "b", "c", "d", "e", "f", "g", "h", "i"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j"),
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"),
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"
+    );
+
+    // ensure NPE if any element is null
+    assertNPE("Set.of(1)", () -> Set.of((String) null));
+    assertNPE("Set.of(2)", () -> Set.of("a", null));
+    assertNPE("Set.of(3)", () -> Set.of("a", "b", null));
+    assertNPE("Set.of(4)", () -> Set.of("a", "b", "c", null));
+    assertNPE("Set.of(5)", () -> Set.of("a", "b", "c", "d", null));
+    assertNPE("Set.of(6)", () -> Set.of("a", "b", "c", "d", "e", null));
+    assertNPE("Set.of(7)", () -> Set.of("a", "b", "c", "d", "e", "f", null));
+    assertNPE("Set.of(8)", () -> Set.of("a", "b", "c", "d", "e", "f", "g", null));
+    assertNPE("Set.of(9)", () -> Set.of("a", "b", "c", "d", "e", "f", "g", "h", null));
+    assertNPE("Set.of(10)", () -> Set.of("a", "b", "c", "d", "e", "f", "g", "h", "i", null));
+    assertNPE("Set.of(...)", () -> Set.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", null));
+
+    // ensure IAE if any element is duplicated
+    assertIAE("Set.of(2)", () -> Set.of("a", "a"));
+    assertIAE("Set.of(3)", () -> Set.of("a", "b", "a"));
+    assertIAE("Set.of(4)", () -> Set.of("a", "b", "c", "a"));
+    assertIAE("Set.of(5)", () -> Set.of("a", "b", "c", "d", "a"));
+    assertIAE("Set.of(6)", () -> Set.of("a", "b", "c", "d", "e", "a"));
+    assertIAE("Set.of(7)", () -> Set.of("a", "b", "c", "d", "e", "f", "a"));
+    assertIAE("Set.of(8)", () -> Set.of("a", "b", "c", "d", "e", "f", "g", "a"));
+    assertIAE("Set.of(9)", () -> Set.of("a", "b", "c", "d", "e", "f", "g", "h", "a"));
+    assertIAE("Set.of(10)", () -> Set.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "a"));
+    assertIAE("Set.of(...)", () -> Set.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "a"));
+  }
+
+  protected static void assertIsImmutableSetOf(Set<String> set, String... contents) {
+    assertEquals(contents.length, set.size());
+    for (int i = 0; i < contents.length; i++) {
+      assertTrue(set.contains(contents[i]));
+      assertFalse(set.contains(contents[i] + "nope"));
+    }
+
+    // quick test that the set impl is sane, aside from the above
+    if (contents.length == 0) {
+      assertFalse(set.iterator().hasNext());
+    } else {
+      Iterator<String> itr = set.iterator();
+      assertTrue(itr.hasNext());
+
+      assertContains(contents, itr.next());
+
+      assertEquals(contents.length > 1, itr.hasNext());
+    }
+
+    // quick check that the set is immutable
+    try {
+      set.add("another item");
+      fail("Set should be unmodifiable: add(T)");
+    } catch (UnsupportedOperationException ignored) {
+      // success
+    }
+
+    // if any, remove an item actually in the set
+    if (contents.length > 1) {
+      // Without any items, remove(T) defaults to iterating items present, so we only test from
+      // present items
+      try {
+        set.remove(contents[0]);
+        fail("Set should be unmodifiable: remove(T)");
+      } catch (UnsupportedOperationException ignored) {
+        // success
+      }
+    }
+
+    // Remove an item that will not be in the set
+    try {
+      set.remove("not present");
+      fail("Set should be unmodifiable: remove(T)");
+    } catch (UnsupportedOperationException ignored) {
+      // success
+    }
+
+    try {
+      set.clear();
+      fail("Set should be unmodifiable: clear()");
+    } catch (UnsupportedOperationException ignored) {
+      // success
+    }
+  }
+
+  private static void assertContains(String[] contents, String value) {
+    for (String item : contents) {
+      if (item.equals(value)) {
+        return;
+      }
+    }
+    fail("Failed to find '" + value + "' in " + Arrays.toString(contents));
+  }
+}

google/j2cl

(cherry picked from commit 3250dca76051493f17c2a39eca216cd55c7153fc)

jre/javatests/com/google/j2cl/jre/EmulJava9Suite.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Google Inc.
+ * Copyright 2023 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -15,20 +15,19 @@
  */
 package com.google.j2cl.jre;
 
+import com.google.j2cl.jre.java9.util.ListTest;
+import com.google.j2cl.jre.java9.util.MapTest;
+import com.google.j2cl.jre.java9.util.SetTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-/** TestSuite for all of GWT's emul suites. */
+/** Test JRE emulations. */
 @RunWith(Suite.class)
 @SuiteClasses({
-  BigDecimalSuite.class,
-  BigIntegerSuite.class,
-  CollectionsSuite.class,
-  ConcurrentSuite.class,
-  EmulSuite.class,
-  EmulJava8Suite.class,
-  EmulJava9Suite.class,
-  LangSuite.class,
+        ListTest.class,
+        SetTest.class,
+        MapTest.class
 })
-public class AllTests {}
+public class EmulJava9Suite {
+}

jre/javatests/com/google/j2cl/jre/java9/util/ListTest.java
@@ -1,0 +1,141 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.j2cl.jre.java9.util;
+
+import com.google.j2cl.jre.java.util.EmulTestBase;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Tests for java.util.List Java 9 API emulation.
+ */
+public class ListTest extends EmulTestBase {
+
+  public void testOf() {
+    assertIsImmutableListOf(List.of());
+    assertIsImmutableListOf(List.of("a"), "a");
+    assertIsImmutableListOf(
+        List.of("a", "b"),
+        "a", "b"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c"),
+        "a", "b", "c"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c", "d"),
+        "a", "b", "c", "d"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c", "d", "e"),
+        "a", "b", "c", "d", "e"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c", "d", "e", "f"),
+        "a", "b", "c", "d", "e", "f"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c", "d", "e", "f", "g"),
+        "a", "b", "c", "d", "e", "f", "g"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c", "d", "e", "f", "g", "h"),
+        "a", "b", "c", "d", "e", "f", "g", "h"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c", "d", "e", "f", "g", "h", "i"),
+        "a", "b", "c", "d", "e", "f", "g", "h", "i"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j"),
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"
+    );
+    assertIsImmutableListOf(
+        List.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"),
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"
+    );
+
+    // ensure that NPE is thrown if a value is null
+    assertNPE("of", () -> List.of((String) null));
+    assertNPE("of", () -> List.of("a", null));
+    assertNPE("of", () -> List.of("a", "b", null));
+    assertNPE("of", () -> List.of("a", "b", "c", null));
+    assertNPE("of", () -> List.of("a", "b", "c", "d", null));
+    assertNPE("of", () -> List.of("a", "b", "c", "d", "e", null));
+    assertNPE("of", () -> List.of("a", "b", "c", "d", "e", "f", null));
+    assertNPE("of", () -> List.of("a", "b", "c", "d", "e", "f", "g", null));
+    assertNPE("of", () -> List.of("a", "b", "c", "d", "e", "f", "g", "h", null));
+    assertNPE("of", () -> List.of("a", "b", "c", "d", "e", "f", "g", "h", "i", null));
+    assertNPE("of", () -> List.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", null));
+  }
+
+  protected static void assertIsImmutableListOf(List<String> list, String... contents) {
+    assertEquals(contents, list);
+
+    // quick test that the list impl is sane
+    if (contents.length == 0) {
+      assertFalse(list.iterator().hasNext());
+    } else {
+      Iterator<String> itr = list.iterator();
+      assertTrue(itr.hasNext());
+      assertEquals(contents[0], itr.next());
+      assertEquals(contents.length > 1, itr.hasNext());
+    }
+
+    // quick check that the list is immutable
+    try {
+      list.add("another item");
+      fail("List should be unmodifiable: add(T)");
+    } catch (UnsupportedOperationException ignored) {
+      // success
+    }
+
+    try {
+      list.remove(0);
+      fail("List should be unmodifiable: remove(int)");
+    } catch (UnsupportedOperationException ignored) {
+      // success
+    }
+
+    // if any, remove an item actually in the list
+    if (contents.length > 0) {
+      // Without any items, remove(T) defaults to iterating items present, so we only test from
+      // present items
+      try {
+        list.remove(contents[0]);
+        fail("List should be unmodifiable: remove(T)");
+      } catch (UnsupportedOperationException ignored) {
+        // success
+      }
+    }
+
+    // Remove an item that will not be in the list
+    try {
+      list.remove("not present");
+      fail("List should be unmodifiable: remove(T)");
+    } catch (UnsupportedOperationException ignored) {
+      // success
+    }
+
+    try {
+      list.clear();
+      fail("List should be unmodifiable: clear()");
+    } catch (UnsupportedOperationException ignored) {
+      // success
+    }
+  }
+}

jre/javatests/com/google/j2cl/jre/java9/util/SetTest.java
@@ -1,0 +1,162 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.j2cl.jre.java9.util;
+
+import com.google.j2cl.jre.java.util.EmulTestBase;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Tests for java.util.Set Java 9 API emulation.
+ */
+public class SetTest extends EmulTestBase {
+
+  public void testOf() {
+    assertIsImmutableSetOf(Set.of());
+    assertIsImmutableSetOf(Set.of("a"), "a");
+    assertIsImmutableSetOf(
+        Set.of("a", "b"),
+        "a", "b"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c"),
+        "a", "b", "c"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c", "d"),
+        "a", "b", "c", "d"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c", "d", "e"),
+        "a", "b", "c", "d", "e"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c", "d", "e", "f"),
+        "a", "b", "c", "d", "e", "f"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c", "d", "e", "f", "g"),
+        "a", "b", "c", "d", "e", "f", "g"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c", "d", "e", "f", "g", "h"),
+        "a", "b", "c", "d", "e", "f", "g", "h"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c", "d", "e", "f", "g", "h", "i"),
+        "a", "b", "c", "d", "e", "f", "g", "h", "i"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j"),
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"
+    );
+    assertIsImmutableSetOf(
+        Set.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"),
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"
+    );
+
+    // ensure NPE if any element is null
+    assertNPE("Set.of(1)", () -> Set.of((String) null));
+    assertNPE("Set.of(2)", () -> Set.of("a", null));
+    assertNPE("Set.of(3)", () -> Set.of("a", "b", null));
+    assertNPE("Set.of(4)", () -> Set.of("a", "b", "c", null));
+    assertNPE("Set.of(5)", () -> Set.of("a", "b", "c", "d", null));
+    assertNPE("Set.of(6)", () -> Set.of("a", "b", "c", "d", "e", null));
+    assertNPE("Set.of(7)", () -> Set.of("a", "b", "c", "d", "e", "f", null));
+    assertNPE("Set.of(8)", () -> Set.of("a", "b", "c", "d", "e", "f", "g", null));
+    assertNPE("Set.of(9)", () -> Set.of("a", "b", "c", "d", "e", "f", "g", "h", null));
+    assertNPE("Set.of(10)", () -> Set.of("a", "b", "c", "d", "e", "f", "g", "h", "i", null));
+    assertNPE("Set.of(...)", () -> Set.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", null));
+
+    // ensure IAE if any element is duplicated
+    assertIAE("Set.of(2)", () -> Set.of("a", "a"));
+    assertIAE("Set.of(3)", () -> Set.of("a", "b", "a"));
+    assertIAE("Set.of(4)", () -> Set.of("a", "b", "c", "a"));
+    assertIAE("Set.of(5)", () -> Set.of("a", "b", "c", "d", "a"));
+    assertIAE("Set.of(6)", () -> Set.of("a", "b", "c", "d", "e", "a"));
+    assertIAE("Set.of(7)", () -> Set.of("a", "b", "c", "d", "e", "f", "a"));
+    assertIAE("Set.of(8)", () -> Set.of("a", "b", "c", "d", "e", "f", "g", "a"));
+    assertIAE("Set.of(9)", () -> Set.of("a", "b", "c", "d", "e", "f", "g", "h", "a"));
+    assertIAE("Set.of(10)", () -> Set.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "a"));
+    assertIAE("Set.of(...)", () -> Set.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "a"));
+  }
+
+  protected static void assertIsImmutableSetOf(Set<String> set, String... contents) {
+    assertEquals(contents.length, set.size());
+    for (int i = 0; i < contents.length; i++) {
+      assertTrue(set.contains(contents[i]));
+      assertFalse(set.contains(contents[i] + "nope"));
+    }
+
+    // quick test that the set impl is sane, aside from the above
+    if (contents.length == 0) {
+      assertFalse(set.iterator().hasNext());
+    } else {
+      Iterator<String> itr = set.iterator();
+      assertTrue(itr.hasNext());
+
+      assertContains(contents, itr.next());
+
+      assertEquals(contents.length > 1, itr.hasNext());
+    }
+
+    // quick check that the set is immutable
+    try {
+      set.add("another item");
+      fail("Set should be unmodifiable: add(T)");
+    } catch (UnsupportedOperationException ignored) {
+      // success
+    }
+
+    // if any, remove an item actually in the set
+    if (contents.length > 1) {
+      // Without any items, remove(T) defaults to iterating items present, so we only test from
+      // present items
+      try {
+        set.remove(contents[0]);
+        fail("Set should be unmodifiable: remove(T)");
+      } catch (UnsupportedOperationException ignored) {
+        // success
+      }
+    }
+
+    // Remove an item that will not be in the set
+    try {
+      set.remove("not present");
+      fail("Set should be unmodifiable: remove(T)");
+    } catch (UnsupportedOperationException ignored) {
+      // success
+    }
+
+    try {
+      set.clear();
+      fail("Set should be unmodifiable: clear()");
+    } catch (UnsupportedOperationException ignored) {
+      // success
+    }
+  }
+
+  private static void assertContains(String[] contents, String value) {
+    for (String item : contents) {
+      if (item.equals(value)) {
+        return;
+      }
+    }
+    fail("Failed to find '" + value + "' in " + Arrays.toString(contents));
+  }
+}