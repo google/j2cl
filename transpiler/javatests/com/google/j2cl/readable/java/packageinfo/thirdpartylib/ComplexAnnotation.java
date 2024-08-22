da1a85602e18409f833f03b3430b41113c7b0f8d
Goktug Gokdogan <goktug@google.com>
2024-08-21 22:33:43 -0700

    Add a test for a package-info.java with a complex annotation.
    This is to reproduce the problem where Turbine generates invalid byte code and we should never load it in JVM.
    
    PiperOrigin-RevId: 666189589

--- transpiler/javatests/com/google/j2cl/readable/java/packageinfo/thirdpartylib/ComplexAnnotation.java
+++ transpiler/javatests/com/google/j2cl/readable/java/packageinfo/thirdpartylib/ComplexAnnotation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Google Inc.
+ * Copyright 2024 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@jsinterop.annotations.JsPackage(namespace = "readable.thirdpartylib")
-@ComplexAnnotation
 package packageinfo.thirdpartylib;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ComplexAnnotation {
+  // An annotation with enum value to make resulting Turbine code invalid to load in JVM.
+  SomeEnum value() default SomeEnum.X;
+
+  enum SomeEnum {
+    X
+  }
+}
