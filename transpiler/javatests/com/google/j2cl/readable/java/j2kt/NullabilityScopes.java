ffd50136ddca33036f620433593a14bbe5fec599
Googler <noreply@google.com>
2024-09-05 06:01:33 -0700

    [J2KT] Create NullMarkedScopes readable to cover type inference problems when crossing nullability scopes.
    PiperOrigin-RevId: 671342510

---
+++ transpiler/javatests/com/google/j2cl/readable/java/j2kt/NullabilityScopes.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2024 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package j2kt;
+
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
+public class NullabilityScopes {
+  @NullMarked
+  public static class NullMarkedScope {
+    public static class Foo<K extends @Nullable Object> {
+      public Foo() {}
+
+      public Foo(Foo<? extends K> foo) {}
+    }
+
+    public void testNullMarkedWildcardConstructor(NullMarkedScope.Foo<String> nullMarkedFoo) {
+      new NullMarkedScope.Foo(nullMarkedFoo);
+      // TODO(b/361769898): Uncomment when fixed
+      // new NullMarkedScope.Foo<>(nullMarkedFoo);
+      // new NullMarkedScope.Foo<String>(nullMarkedFoo);
+    }
+
+    public void testNonNullMarkedWildcardConstructor(
+        NonNullMarkedScope.Foo<String> nonNullMarkedFoo) {
+      new NonNullMarkedScope.Foo(nonNullMarkedFoo);
+      // TODO(b/361769898): Uncomment when fixed
+      // new NonNullMarkedScope.Foo<>(nonNullMarkedFoo);
+      // new NonNullMarkedScope.Foo<String>(nonNullMarkedFoo);
+    }
+  }
+
+  public static class NonNullMarkedScope {
+    public static class Foo<K extends @Nullable Object> {
+      public Foo() {}
+
+      public Foo(Foo<? extends K> foo) {}
+    }
+
+    public void testNullMarkedWildcardConstructor(NullMarkedScope.Foo<String> nullMarkedFoo) {
+      new NullMarkedScope.Foo(nullMarkedFoo);
+      new NullMarkedScope.Foo<>(nullMarkedFoo);
+      new NullMarkedScope.Foo<String>(nullMarkedFoo);
+    }
+
+    public void testNonNullMarkedWildcardConstructor(
+        NonNullMarkedScope.Foo<String> nonNullMarkedFoo) {
+      new NonNullMarkedScope.Foo(nonNullMarkedFoo);
+      new NonNullMarkedScope.Foo<>(nonNullMarkedFoo);
+      new NonNullMarkedScope.Foo<String>(nonNullMarkedFoo);
+    }
+  }
+}
