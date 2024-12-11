14291c090e63d591e8aa6d962b3fee9398a3e3f5
Googler <noreply@google.com>
2024-12-11 12:51:21 -0800

    [J2KT] Reproduce a problem with nullability inference in `Optional.orElse()`, which occurs on j2kt-native only.
    PiperOrigin-RevId: 705206366

---
+++ transpiler/javatests/com/google/j2cl/readable/java/j2kt/NullabilityInferenceProblem.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2024 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package j2kt;
+
+import static java.util.Comparator.comparing;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
+@NullMarked
+class NullabilityInferenceProblem {
+  interface ImmutableList<T> extends List<T> {}
+
+  interface User {
+    Optional<String> getName();
+  }
+
+  private static <E> ImmutableList<E> sortedCopyOf(
+      Comparator<? super E> comparator, Iterable<? extends E> elements) {
+    throw new RuntimeException();
+  }
+
+  private static <T> T checkNotNull(@Nullable T reference) {
+    throw new RuntimeException();
+  }
+
+  // TODO(b/383581076): Uncomment when fixed.
+  // private static ImmutableList<User> testImplicitTypeArguments(Iterable<User> users) {
+  //   return sortedCopyOf(comparing(user -> user.getName().orElse("")), users);
+  // }
+
+  private static ImmutableList<User> testExplicitTypeArguments(Iterable<User> users) {
+    return sortedCopyOf(
+        Comparator.<User, String>comparing(user -> user.getName().orElse("")), users);
+  }
+
+  private static ImmutableList<User> testCheckNotNull(Iterable<User> users) {
+    return sortedCopyOf(comparing(user -> checkNotNull(user.getName().orElse(""))), users);
+  }
+}
