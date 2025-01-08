bb309fbab1025a0cfb8781fadfaf03e07f86fb14
Googler <noreply@google.com>
2025-01-08 03:44:14 -0800

    [J2KT] Update `annotation` readable to cover valid annotation method return types.
    PiperOrigin-RevId: 713232400

--- transpiler/javatests/com/google/j2cl/readable/java/annotation/Main.java
+++ transpiler/javatests/com/google/j2cl/readable/java/annotation/Main.java
@@ -17,9 +17,40 @@ package annotation;
 
 public class Main {
   @interface Foo {
-    int CONSTANT = 123;
+    boolean BOOLEAN_CONSTANT = false;
 
-    int bar() default CONSTANT;
+    int INT_CONSTANT = 123;
+
+    String STRING_CONSTANT = "foo";
+
+    boolean booleanValue() default BOOLEAN_CONSTANT;
+
+    int intValue() default INT_CONSTANT;
+
+    String stringValue() default STRING_CONSTANT;
+
+    Class<?> classValue();
+
+    SomeEnum enumValue();
+
+    Zoo annotationValue();
+
+    boolean[] booleanArray();
+
+    int[] intArray();
+
+    String[] stringArray();
+
+    Class<?>[] classArray();
+
+    SomeEnum[] enumArray();
+
+    Zoo[] annotationArray();
+  }
+
+  enum SomeEnum {
+    ZERO,
+    ONE
   }
 
   // Note that this inherits the single abstract method defined in its implicit parent
@@ -27,12 +58,23 @@ public class Main {
   // requirements to be a functional interface.
   @interface Zoo {}
 
-  class Bar {
+  static void test(Foo foo) {
+    boolean booleanConstant = Foo.BOOLEAN_CONSTANT;
+    int intConstant = Foo.INT_CONSTANT;
+    String stringConstant = Foo.STRING_CONSTANT;
 
-    int baz() {
-      Foo foo = null;
-      foo.bar();
-      return Foo.CONSTANT;
-    }
+    boolean booleanValue = foo.booleanValue();
+    int intValue = foo.intValue();
+    String stringValue = foo.stringValue();
+    Class<?> classValue = foo.classValue();
+    SomeEnum enumValue = foo.enumValue();
+    Zoo annotationValue = foo.annotationValue();
+
+    boolean[] booleanArray = foo.booleanArray();
+    int[] intArray = foo.intArray();
+    String[] stringArray = foo.stringArray();
+    Class<?>[] classArray = foo.classArray();
+    SomeEnum[] enumArray = foo.enumArray();
+    Zoo[] annotationArray = foo.annotationArray();
   }
 }
