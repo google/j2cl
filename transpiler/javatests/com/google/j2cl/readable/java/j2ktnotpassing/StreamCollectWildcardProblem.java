b34bd0c9bf2c7129ba19cf91918cc99db858f0a9
Googler <noreply@google.com>
2025-01-29 06:18:37 -0800

    [J2KT] Add `StreamCollectWildcardProblem` readable in `j2ktnotpassing`, to cover use-case with `Stream`, `Collector` and wildcards.
    PiperOrigin-RevId: 720948272

---
+++ transpiler/javatests/com/google/j2cl/readable/java/j2ktnotpassing/StreamCollectWildcardProblem.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2024 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package j2ktnotpassing;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.jspecify.annotations.Nullable;
+
+public class StreamCollectWildcardProblem {
+  interface Foo<T extends @Nullable Object> {}
+
+  public static void test(Stream<Foo<?>> list) {
+    accept(list.collect(toList()));
+  }
+
+  public static void accept(List<Foo<?>> list) {
+    throw new RuntimeException();
+  }
+}
