16e2bf1efc519a8df453a6b2f4f07867209c78ab
Googler <noreply@google.com>
2025-02-11 09:59:00 -0800

    [J2KT] Add `NullabilityInferenceProblem5` which covers wildcard type inference case.
    PiperOrigin-RevId: 725671885

---
+++ transpiler/javatests/com/google/j2cl/readable/java/j2kt/NullabilityInferenceProblem5.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2025 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package j2kt;
+
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
+@NullMarked
+public class NullabilityInferenceProblem5 {
+  interface Future<V extends @Nullable Object> {}
+
+  interface Function<I extends @Nullable Object, O extends @Nullable Object> {
+    O apply(I i);
+  }
+
+  static <I extends @Nullable Object, O extends @Nullable Object> Future<O> transform(
+      Future<I> future, Function<? super I, ? extends O> fn) {
+    throw new RuntimeException();
+  }
+
+  static <V> Future<@Nullable V> nullableFuture(V v) {
+    throw new RuntimeException();
+  }
+
+  interface Foo<V extends @Nullable Object> {}
+
+  static Future<@Nullable Void> testVoid(Foo<?> foo) {
+    return transform(nullableFuture(foo), it -> null);
+  }
+}
