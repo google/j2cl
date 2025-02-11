7a502a5bd833fb8922d7b7d34a1dfb565e03302b
Googler <noreply@google.com>
2025-02-11 05:21:32 -0800

    [J2KT] Add `CompileTimeConstantInInnerClass` readable to `j2ktnotpassing`, to cover b/395623845.
    PiperOrigin-RevId: 725588549

---
+++ transpiler/javatests/com/google/j2cl/readable/java/j2kt/CompileTimeConstantsInInnerClass.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2025 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package j2kt;
+
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
+public class CompileTimeConstantsInInnerClass {
+  public class Inner {
+    // TODO(b/395623845): Uncomment when fixed.
+    // public static final int i = 0;
+    // public static final String s = "";
+  }
+}
