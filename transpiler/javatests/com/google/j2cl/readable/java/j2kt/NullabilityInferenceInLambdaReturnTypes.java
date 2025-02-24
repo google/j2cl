0846c32a9e94faf532bca8523cc5db6dedb02d92
Googler <noreply@google.com>
2025-02-24 03:30:45 -0800

    [J2KT] Add `NullabilityInferenceInLambdaReturnTypes` readable.
    PiperOrigin-RevId: 730380022

---
+++ transpiler/javatests/com/google/j2cl/readable/java/j2kt/NullabilityInferenceInLambdaReturnTypes.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2025 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package j2kt;
+
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
+@NullMarked
+public class NullabilityInferenceInLambdaReturnTypes {
+  interface Supplier<V extends @Nullable Object> {
+    V get();
+  }
+
+  static <V extends @Nullable Object> void accept(Supplier<V> supplier) {
+    throw new RuntimeException();
+  }
+
+  static <V extends @Nullable Object> Supplier<V> transform(Supplier<V> supplier) {
+    throw new RuntimeException();
+  }
+
+  private void testAccept() {
+    accept(() -> null);
+  }
+
+  private void testTransformAndAccept() {
+    accept(transform(() -> null));
+  }
+}
