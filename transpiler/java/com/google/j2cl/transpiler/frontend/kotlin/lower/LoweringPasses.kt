0bf488ec1079097dfb14e750db56a13d7a56ce2d
Kevin O'Connor <kevinoconnor@google.com>
2025-02-28 14:53:34 -0800

    Split lowering passes to be run per-file or per-module
    In the per-file case the lowering pass will be reinstantiated for each file.
    
    PiperOrigin-RevId: 732268961

--- transpiler/java/com/google/j2cl/transpiler/frontend/kotlin/lower/LoweringPasses.kt
+++ transpiler/java/com/google/j2cl/transpiler/frontend/kotlin/lower/LoweringPasses.kt
@@ -21,7 +21,8 @@ import com.google.j2cl.transpiler.frontend.kotlin.ir.IntrinsicMethods
 import com.google.j2cl.transpiler.frontend.kotlin.ir.IrProviderFromPublicSignature
 import com.google.j2cl.transpiler.frontend.kotlin.ir.JvmIrDeserializerImpl
 import com.google.j2cl.transpiler.frontend.kotlin.ir.populate
-import org.jetbrains.kotlin.backend.common.CompilationException
+import org.jetbrains.kotlin.backend.common.BodyLoweringPass
+import org.jetbrains.kotlin.backend.common.CommonBackendContext
 import org.jetbrains.kotlin.backend.common.FileLoweringPass
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
@@ -40,7 +41,7 @@ import org.jetbrains.kotlin.backend.common.phaser.CompilerPhase
 import org.jetbrains.kotlin.backend.common.phaser.PhaseConfig
 import org.jetbrains.kotlin.backend.common.phaser.PhaseConfigurationService
 import org.jetbrains.kotlin.backend.common.phaser.PhaserState
-import org.jetbrains.kotlin.backend.common.wrapWithCompilationException
+import org.jetbrains.kotlin.backend.common.runOnFilePostfix
 import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
 import org.jetbrains.kotlin.backend.jvm.JvmBackendExtension
 import org.jetbrains.kotlin.backend.jvm.JvmGeneratorExtensionsImpl
@@ -50,50 +51,50 @@ import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
 import org.jetbrains.kotlin.ir.backend.js.lower.cleanup.CleanupLowering
 import org.jetbrains.kotlin.ir.backend.js.lower.inline.RemoveInlineDeclarationsWithReifiedTypeParametersLowering
+import org.jetbrains.kotlin.ir.declarations.IrFile
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.symbols.UnsafeDuringIrConstructionAPI
 import org.jetbrains.kotlin.ir.util.SymbolTable
-import org.jetbrains.kotlin.progress.ProgressIndicatorAndCompilationCanceledStatus
 
 /** The list of lowering passes to run in execution order. */
-private val loweringPassFactories: List<J2clLoweringPassFactory> = buildList {
+private val loweringPhase = loweringPhase {
   // Remove expect declarations, the actuals will be used instead.
-  add(::ExpectDeclarationsRemoveLowering)
+  moduleLowering(::ExpectDeclarationsRemoveLowering)
   // The K2 frontend does not create a FileKt class  for referenced/ top-level members defined in
   // another compilation unit. Instead, it attaches them to an IrPackageFragment.
   // This lowering addresses this by creating the necessary file class and moving the top-level
   // members into it. This is needed to ensure the serialized IR for top-level inline functions
   // can be deserialized correctly. (The serialized IR being part of the metadata of the enclosing
   // class after Kotlin/JVM compilation.)
-  add(::ExternalPackageParentPatcherLowering)
+  moduleLowering(::ExternalPackageParentPatcherLowering)
   // Remove assignments to definedExternally.
-  add(::DefinedExternallyLowering)
+  moduleLowering(::DefinedExternallyLowering)
   // Remove typealias declarations from the IR.
-  add(::StripTypeAliasDeclarationsLowering)
+  perFileLowering(::StripTypeAliasDeclarationsLowering)
   // Put file level function and property declaration into a class.
-  add(::FileClassLowering)
+  moduleLowering(::FileClassLowering)
   // Invent names for local classes and anonymous objects. Later passes may require all classes
   // to have a name for computing function signature.
-  add(::JvmInventNamesForLocalClasses)
+  perFileLowering(::JvmInventNamesForLocalClasses)
   // Transform all callable reference (including defaults) to inline lambdas, mark inline lambdas
   // for later passes.
-  add(::J2clInlineCallableReferenceToLambdaPhase)
+  perFileLowering(::J2clInlineCallableReferenceToLambdaPhase)
   // Rewrites `Array(size) { index -> value }` using type-specific initializer lambdas.
-  add(::ArrayConstructorLowering)
+  perFileLowering(::ArrayConstructorLowering)
   // Create nullable backing fields and insert nullability checks for lateinit properties and
   // variables.
-  add(::JvmLateinitLowering)
+  perFileLowering(::JvmLateinitLowering)
   // Patch calls to `throwUninitializedPropertyAccessException()` to match our definition of this
   // function.
-  add(::PatchThrowUninitializedPropertyExceptionCalls)
+  perFileLowering(::PatchThrowUninitializedPropertyExceptionCalls)
   // Extract anonymous classes from inline lambdas.
-  add(::LocalClassesInInlineLambdasLowering)
+  perFileLowering(::LocalClassesInInlineLambdasLowering)
   // Resolve captures for anonymous class defined in inline functions.
-  add(::LocalClassesInInlineFunctionsLowering)
+  perFileLowering(::LocalClassesInInlineFunctionsLowering)
   // Move the anonymous classes from inline functions into the nearest declaration container.
-  add(::LocalClassesExtractionFromInlineFunctionsLowering.asPostfix())
+  perFileLowering(::LocalClassesExtractionFromInlineFunctionsLowering.asPostfix())
   // Create public bridge for private top level function called from inline functions.
-  add(::SyntheticAccessorLowering)
+  perFileLowering(::SyntheticAccessorLowering)
   // Replace reference to inline function with reified parameter with reference to a synthetic
   // non-inline function where types parameters have been substituted so there is no reference to
   // inline functions anymore.
@@ -101,58 +102,58 @@ private val loweringPassFactories: List<J2clLoweringPassFactory> = buildList {
   // inline fun <reified T> castTo(param: Any): T  = param as T
   // reference like `::castTo<String>` is replaced with `::castTo$wrap` where
   // fun castTo$wrap(param: Any) String = castTo<String>(param)
-  add(::WrapInlineDeclarationsWithReifiedTypeParametersLowering)
+  perFileLowering(::WrapInlineDeclarationsWithReifiedTypeParametersLowering)
   // Perform function inlining.
-  add(::J2clFunctionInlining)
+  moduleLowering(::J2clFunctionInlining)
   // Remove inline functions with reified type parameters as these functions cannot be called from
   // Java
-  add(::RemoveInlineDeclarationsWithReifiedTypeParametersLowering)
+  perFileLowering(::RemoveInlineDeclarationsWithReifiedTypeParametersLowering)
   // Replace `emptyArray()` calls with `arrayOf()` calls.
-  add(::EmptyArrayLowering)
+  perFileLowering(::EmptyArrayLowering)
   // Remove functions that contain unsigned varargs in the signature as a temporary workaround for
   // b/242573966.
   // TODO(b/242573966): Remove this when we can handle unsigned vararg types.
-  add(::RemoveFunctionsWithUnsignedVarargsLowering)
+  perFileLowering(::RemoveFunctionsWithUnsignedVarargsLowering)
   // Inline function bodies are inlined in a IrReturnableBlock that can contain a return
   // statement. Lower IrReturnableBlocks as labelled blocks, introduce a temporary variable for
   // keeping the returned value and lower return statements inside the block to break statement.
-  add(::ReturnableBlockLowering)
+  perFileLowering(::ReturnableBlockLowering)
   // Optimize for loops on arrays and integer like progressions.
-  add(::ForLoopsLowering)
+  perFileLowering(::ForLoopsLowering)
   // Replace null varargs with empty array calls.
-  add(::EmptyVarargLowering)
+  perFileLowering(::EmptyVarargLowering)
   // Replace constant property accessors with a direct reference to the constant value.
-  add(::ConstLowering)
+  perFileLowering(::ConstLowering)
   // Lowers unsigned const values to be instantiations of their boxed value types.
-  add(::UnsignedConstLowering)
+  perFileLowering(::UnsignedConstLowering)
   // Move and/or copy companion object fields to static fields of companion's owner.
-  add(::MoveOrCopyCompanionObjectFieldsLowering)
+  perFileLowering(::MoveOrCopyCompanionObjectFieldsLowering)
   // Inline property accessors where the field can be referenced directly
   // (ex. private properties).
-  add(::JvmPropertiesLowering)
+  perFileLowering(::JvmPropertiesLowering)
   // Make IrGetField/IrSetField to objects' fields point to the static versions
-  add(::RemapObjectFieldAccesses)
+  perFileLowering(::RemapObjectFieldAccesses)
   // Extract local functions and move them up to the closest enclosing type. Rewrite local
   // function calls accordingly.
-  add(::LocalFunctionLowering)
-  add(::JvmLocalClassPopupLowering)
+  perFileLowering(::LocalFunctionLowering)
+  perFileLowering(::JvmLocalClassPopupLowering)
   // Adds stub methods to the implementations of the read-only collection types to properly
   // implement the Java collection APIs.
-  add(::CollectionStubMethodLowering)
+  perFileLowering(::CollectionStubMethodLowering)
   // Add static field to hold singleton object instance.
-  add(::ObjectClassLowering)
+  perFileLowering(::ObjectClassLowering)
   // Copies static fields from companion objects onto the enclosing interface if a const property
   // is present.
-  // add(::CopyInterfaceCompanionFieldsLowering)
+  // perFileLowering(::CopyInterfaceCompanionFieldsLowering)
   // Drops field initializers when they initialize the field to it's default value.
-  add(::RemoveFieldInitializerToDefault)
+  perFileLowering(::RemoveFieldInitializerToDefault)
   // Move code from object init blocks and static field initializers to a new <clinit> function.
-  add(::StaticInitializersLowering)
+  perFileLowering(::StaticInitializersLowering)
   // Lower field/block initializers into the primary kotlin constructor.
   // TODO(b/233909092): Only lower classes that contain primary ctors to avoid duplicating
   // code in secondary ctors.
-  add(::InitializersLowering)
-  add { j2clBackendContext ->
+  perFileLowering(::InitializersLowering)
+  perFileLowering { j2clBackendContext: J2clBackendContext ->
     InitializersCleanupLowering(j2clBackendContext.jvmBackendContext) {
       // Only remove initializers that are non-constants and non-static.
       // Modified from: org.jetbrains.kotlin.backend.jvm.JvmLower.kt
@@ -161,13 +162,13 @@ private val loweringPassFactories: List<J2clLoweringPassFactory> = buildList {
     }
   }
   // Synthesize static proxy functions for JvmStatic functions in companion objects.
-  add(::JvmStaticInCompanionLowering)
+  perFileLowering(::JvmStaticInCompanionLowering)
   // Generate synthetic stubs for functions with default parameter values
-  add(::JvmDefaultArgumentStubGenerator)
+  perFileLowering(::JvmDefaultArgumentStubGenerator)
   // Transform calls with default arguments into calls to stubs
-  add(::JvmDefaultParameterInjector)
+  perFileLowering(::JvmDefaultParameterInjector)
   // Replace default values arguments with stubs
-  add { j2clBackendContext ->
+  perFileLowering { j2clBackendContext: J2clBackendContext ->
     DefaultParameterCleaner(
       j2clBackendContext.jvmBackendContext,
       replaceDefaultValuesWithStubs = true,
@@ -175,64 +176,56 @@ private val loweringPassFactories: List<J2clLoweringPassFactory> = buildList {
   }
   // TODO(b/377502016): Remove this pass once smart cast's bug is fixed.
   // Cleanup IMPLICIT_CAST introduced by smart casts that cast a expression to a parent type.
-  add(::SmartCastCleaner)
-  add(::BridgeLowering)
+  perFileLowering(::SmartCastCleaner)
+  perFileLowering(::BridgeLowering)
   // Lowers calls to functions that return unit into a block of the call and a unit object ref.
-  add(::KotlinUnitValueLowering)
+  perFileLowering(::KotlinUnitValueLowering)
   // Replaces IrExpressionBody with IrBlockBody returning the expression.
-  add(::ExpressionBodyTransformer)
+  perFileLowering(::ExpressionBodyTransformer)
   // Place calls that return `Nothing` calls into a synthetic block of `{ foo(); Unit }`.
-  add(::KotlinNothingValueCallsLowering)
+  perFileLowering(::KotlinNothingValueCallsLowering)
   // Mange the names of functions that are shadowing those in super types.
-  add(::MangleWellKnownShadowingFunctionsLowering)
+  perFileLowering(::MangleWellKnownShadowingFunctionsLowering)
   // Implement intrinsic. Must run after function inlining.
-  add(::IntrinsicFunctionCallsLowering)
+  perFileLowering(::IntrinsicFunctionCallsLowering)
   // Make JvmStatic functions in non-companion objects static and replace all call sites in the
   // module. Must run after IntrinsicFunctionCallsLowering and before TypeOperatorLowering.
-  add(::JvmStaticInObjectLowering)
+  moduleLowering(::JvmStaticInObjectLowering)
   // Transforms some cast/instanceof operations.
-  add(::TypeOperatorLowering)
+  perFileLowering(::TypeOperatorLowering)
   // Rewrites numeric conversion calls (toInt(), toShort(), etc) to be simple casts instead.
-  add(::NumericConversionLowering)
+  perFileLowering(::NumericConversionLowering)
   // Removes enum super constructor calls and cleans up effectively empty constructors.
-  add(::EnumClassConstructorLowering)
+  perFileLowering(::EnumClassConstructorLowering)
   // Rewrites calls to KFunction.invoke() as FunctionN.invoke().
-  add(::RewriteKFunctionInvokeLowering)
+  perFileLowering(::RewriteKFunctionInvokeLowering)
 
   // BLOCK DECOMPOSITION
   // Transforms statement-like-expression nodes into pure-statement. This should be the last
   // major modification to the AST before it's cleaned up.
-  add(::BlockDecomposerLowering.asPostfix())
+  perFileLowering(::BlockDecomposerLowering.asPostfix())
 
   // CLEANUP PHASE
   // This is the last set of passes to touchup the AST into a form that the CompilationUnitBuilder
   // is going to expect. Lowerings should have very limited scope.
 
   // Replace singleton object references with static field references.
-  add(::SingletonReferencesLowering)
+  perFileLowering(::SingletonReferencesLowering)
   // Reconstruct `for-loop` node for iterations over array and non-overflowing ranges.
-  add(::CreateForLoopLowering)
+  perFileLowering(::CreateForLoopLowering)
   // Convert some `when` statements to a `switch` java-like statement.
-  add(::CreateSwitchLowering)
+  perFileLowering(::CreateSwitchLowering)
   // Lower Kotlin annotation in a Java-like format expected by the J2CL ast.
-  add(::AnnotationLowering)
+  perFileLowering(::AnnotationLowering)
   // Ensures that any top-level referenced members from another compilation unit have an enclosing
   // class. The inliner can introduce references to top-level members from another compilation unit
   // that were not referenced before.
-  add(::ExternalPackageParentPatcherLowering)
+  moduleLowering(::ExternalPackageParentPatcherLowering)
   // Cleanup the unreachable code that exist after the statement-like-expression
   // transformation to make the IrTree valid.
-  add(::CleanupLowering)
-}
-
-private fun MutableList<J2clLoweringPassFactory>.add(
-  factory: (JvmBackendContext) -> FileLoweringPass
-) {
-  add(factory.toJ2clLoweringPassFactory())
-}
-
-private fun MutableList<J2clLoweringPassFactory>.add(factory: () -> FileLoweringPass) {
-  add(factory.toJ2clLoweringPassFactory())
+  perFileLowering(::CleanupLowering)
+  // Generate facade classes for JvmMultifileClass parts.
+  moduleLowering(::GenerateMultifileFacades)
 }
 
 class LoweringPasses(
@@ -249,28 +242,7 @@ class LoweringPasses(
 
     val j2clBackendContext = J2clBackendContext(jvmBackendContext, intrinsics)
 
-    loweringPassFactories.forEach { moduleFragment.lower(j2clBackendContext, it) }
-
-    // Generate facade classes for JvmMultifileClass parts.
-    GenerateMultifileFacades(jvmBackendContext).lower(moduleFragment)
-  }
-}
-
-/** Lowers each file in a module, instantiating a new lowering for each file. */
-private fun IrModuleFragment.lower(
-  context: J2clBackendContext,
-  loweringFactory: J2clLoweringPassFactory,
-) {
-  for (f in files) {
-    try {
-      ProgressIndicatorAndCompilationCanceledStatus.checkCanceled()
-      loweringFactory(context).lower(f)
-    } catch (e: CompilationException) {
-      e.initializeFileDetails(f)
-      throw e
-    } catch (e: Throwable) {
-      throw e.wrapWithCompilationException("Internal error in file lowering", f, null)
-    }
+    loweringPhase.lower(j2clBackendContext, moduleFragment)
   }
 }
 
@@ -317,3 +289,13 @@ private fun createJvmBackendContext(
     irPluginContext = pluginContext,
   )
 }
+
+private fun <T : CommonBackendContext> ((T) -> BodyLoweringPass).asPostfix():
+  (T) -> FileLoweringPass = { context: T -> invoke(context).asPostfix() }
+
+private fun BodyLoweringPass.asPostfix(): FileLoweringPass =
+  object : FileLoweringPass {
+    override fun lower(irFile: IrFile) {
+      this@asPostfix.runOnFilePostfix(irFile, withLocalDeclarations = true)
+    }
+  }
---
+++ transpiler/java/com/google/j2cl/transpiler/frontend/kotlin/lower/LoweringPhase.kt
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2025 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.j2cl.transpiler.frontend.kotlin.lower
+
+import com.google.common.collect.ImmutableList
+import org.jetbrains.kotlin.backend.common.BackendContext
+import org.jetbrains.kotlin.backend.common.CommonBackendContext
+import org.jetbrains.kotlin.backend.common.CompilationException
+import org.jetbrains.kotlin.backend.common.FileLoweringPass
+import org.jetbrains.kotlin.backend.common.ModuleLoweringPass
+import org.jetbrains.kotlin.backend.common.wrapWithCompilationException
+import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
+import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
+import org.jetbrains.kotlin.progress.ProgressIndicatorAndCompilationCanceledStatus
+
+internal class LoweringPhase
+private constructor(
+  private val loweringPassFactories: ImmutableList<(J2clBackendContext) -> ModuleLoweringPass>
+) {
+  fun lower(context: J2clBackendContext, module: IrModuleFragment) {
+    for (factory in loweringPassFactories) {
+      ProgressIndicatorAndCompilationCanceledStatus.checkCanceled()
+      factory(context).lower(module)
+    }
+  }
+
+  internal class Builder {
+    private val loweringPassFactories = mutableListOf<(J2clBackendContext) -> ModuleLoweringPass>()
+
+    @JvmName("moduleLoweringJ2cl")
+    fun moduleLowering(factory: (J2clBackendContext) -> ModuleLoweringPass) {
+      loweringPassFactories.add(factory)
+    }
+
+    @JvmName("moduleLoweringJvm")
+    fun moduleLowering(factory: (JvmBackendContext) -> ModuleLoweringPass) {
+      loweringPassFactories.add(factory.withJ2clContext())
+    }
+
+    @JvmName("moduleLoweringCommon")
+    fun moduleLowering(factory: (CommonBackendContext) -> ModuleLoweringPass) {
+      loweringPassFactories.add(factory)
+    }
+
+    @JvmName("perFileLoweringJ2cl")
+    fun perFileLowering(factory: (J2clBackendContext) -> FileLoweringPass) {
+      loweringPassFactories.add(factory.asPerFileLowering())
+    }
+
+    @JvmName("perFileLoweringJvm")
+    fun perFileLowering(factory: (JvmBackendContext) -> FileLoweringPass) {
+      loweringPassFactories.add(factory.asPerFileLowering().withJ2clContext())
+    }
+
+    @JvmName("perFileLoweringCommon")
+    fun perFileLowering(factory: (CommonBackendContext) -> FileLoweringPass) {
+      loweringPassFactories.add(factory.asPerFileLowering())
+    }
+
+    fun perFileLowering(factory: () -> FileLoweringPass) {
+      loweringPassFactories.add({ _: CommonBackendContext -> factory() }.asPerFileLowering())
+    }
+
+    fun build() = LoweringPhase(ImmutableList.copyOf(loweringPassFactories))
+  }
+}
+
+internal fun loweringPhase(builder: LoweringPhase.Builder.() -> Unit): LoweringPhase =
+  LoweringPhase.Builder().apply(builder).build()
+
+private class PerFileLowering<T : BackendContext>(
+  private val context: T,
+  private val fileLoweringFactory: (T) -> FileLoweringPass,
+) : ModuleLoweringPass {
+  override fun lower(irModule: IrModuleFragment) {
+    for (f in irModule.files) {
+      try {
+        ProgressIndicatorAndCompilationCanceledStatus.checkCanceled()
+        fileLoweringFactory(context).lower(f)
+      } catch (e: CompilationException) {
+        e.initializeFileDetails(f)
+        throw e
+      } catch (e: Throwable) {
+        throw e.wrapWithCompilationException("Internal error in file lowering", f, null)
+      }
+    }
+  }
+}
+
+private fun <T : BackendContext> ((T) -> FileLoweringPass).asPerFileLowering() = { context: T ->
+  PerFileLowering(context, this@asPerFileLowering)
+}
+
+private fun ((JvmBackendContext) -> ModuleLoweringPass).withJ2clContext():
+  (J2clBackendContext) -> ModuleLoweringPass = { context -> this.invoke(context.jvmBackendContext) }
