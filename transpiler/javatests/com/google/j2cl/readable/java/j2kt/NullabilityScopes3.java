2a541e7d9cb4111ecd9ced9a9c60b33fbbcf779d
Googler <noreply@google.com>
2025-03-18 23:58:36 -0700

    [J2KT] Reproduce a problem with nullability of explicitly declared type arguments, involving incompatible null-marked scopes.
    PiperOrigin-RevId: 738278420

---
+++ transpiler/javatests/com/google/j2cl/readable/java/j2kt/NullabilityScopes3.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2025 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package j2kt;
+
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
+public class NullabilityScopes3 {
+  @NullMarked
+  static final class Optional<T> {
+    static <T> Optional<T> of(T value) {
+      throw new RuntimeException();
+    }
+  }
+
+  @NullMarked
+  interface List<T extends @Nullable Object> {}
+
+  @NullMarked
+  static class ImmutableList<T> implements List<T> {
+    static <T> ImmutableList<T> of() {
+      throw new RuntimeException();
+    }
+  }
+
+  void testNonNullMarked() {
+    Optional.<List<String>>of(ImmutableList.of());
+  }
+}
