bcdedb83a90759360d412ce2c71824c83d9522ad
Googler <noreply@google.com>
2025-04-15 10:39:20 -0700

    [J2KT] Add `HiddenFromObjCTypes` to `j2kt` readables and `j2ktiosonterop` integration test to reproduce Objective-C name collision problem with Kotlin stdlib types containing overloads, like `Appendable`, `StringBuilder` etc...
    PiperOrigin-RevId: 747934236

---
+++ transpiler/javatests/com/google/j2cl/readable/java/j2kt/HiddenFromObjCTypes.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2022 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package j2kt;
+
+import java.io.IOException;
+import java.io.Writer;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
+// Test for type and members which should be hidden from ObjC because of b/407538927
+@NullMarked
+public final class HiddenFromObjCTypes {
+  public static @Nullable Appendable appendable;
+  public static @Nullable StringBuilder stringBuilder;
+  public static @Nullable Writer writer;
+
+  public interface Generic<T extends @Nullable Object> {}
+
+  public interface GenericWithStringBuilder<T extends @Nullable StringBuilder> {}
+
+  public static StringBuilder returnsStringBuilder(int i) {
+    throw new RuntimeException();
+  }
+
+  public static void takesStringBuilder(int i, StringBuilder stringBuilder) {
+    throw new RuntimeException();
+  }
+
+  public static <T extends StringBuilder> void hasStringBuilderParameter(int i, T stringBuilder) {
+    throw new RuntimeException();
+  }
+
+  public static void hasStringBuilderTypeVariable(int i, Generic<StringBuilder> stringBuilder) {
+    throw new RuntimeException();
+  }
+
+  public static void hasStringBuilderWildcard(
+      int i, Generic<? extends StringBuilder> stringBuilder) {
+    throw new RuntimeException();
+  }
+
+  public static final class CustomAppendable implements Appendable {
+    @Override
+    public Appendable append(@Nullable CharSequence csq) throws IOException {
+      throw new IOException();
+    }
+
+    @Override
+    public Appendable append(@Nullable CharSequence csq, int start, int end) throws IOException {
+      throw new IOException();
+    }
+
+    @Override
+    public Appendable append(char c) throws IOException {
+      throw new IOException();
+    }
+  }
+}
