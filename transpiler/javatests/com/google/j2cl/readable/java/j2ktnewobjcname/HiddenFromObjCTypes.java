cca3f2e841368835d1b942b553cce6055b216cd5
Googler <noreply@google.com>
2025-05-07 15:42:55 -0700

    [J2KT] Move `HiddenFromObjCTypes.java` readable from `j2kt` to `j2ktobjcname` / `j2ktnewobjcname`, because `@ObjCName` and `@HiddenFromObjC` annotations are only enabled there.
    PiperOrigin-RevId: 756035476

--- transpiler/javatests/com/google/j2cl/readable/java/j2ktnewobjcname/HiddenFromObjCTypes.java
+++ transpiler/javatests/com/google/j2cl/readable/java/j2ktnewobjcname/HiddenFromObjCTypes.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package j2kt;
+package j2ktnewobjcname;
 
 import java.io.IOException;
 import java.io.Writer;
