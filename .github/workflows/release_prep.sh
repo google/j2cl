979802ef741802371aca4f8f6ad7e4f180a55d2b
Julien Dramaix <dramaix@google.com>
2025-07-21 17:43:52 +0000

    Foof

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-# This file is used to prepare the release.
+# This file is used to prepare the githubrelease.
 # It is not intended to be run directly but called from the github release
-# workflow.
+# workflow: bazel-contrib/.github/workflows/release_ruleset.yaml\
+set -euo pipefail
+
 tag="$1"
 # The prefix is used to determine the directory structure of the archive. We strip the 'v'
 # prefix from the version number.
 directory="j2cl-${tag#v}"
 archive="j2cl-${tag}.tar.gz"
 
-git archive --format=tar --prefix=${directory}/ -o "${archive}" ${tag}  
+git archive --format=tar --prefix=${directory}/ -o "${archive}" ${tag}
 
-sha256=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
+sha256=$(shasum -a 256 "${archive}" | awk '{print $1}')
 
 # The stdout of this program will be used as the top of the release notes for this release.
 cat << EOF
@@ -21,9 +23,9 @@ cat << EOF
 2. Add to your \`MODULE.bazel\` file:
 
 \`\`\`starlark
-bazel_dep(    
+bazel_dep(
     name = "j2cl",
-    repo_name = "com_google_j2cl", 
+    repo_name = "com_google_j2cl",
     version = "${tag}")
 \`\`\`
 
