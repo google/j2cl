08f76bd147422e32d847826bef52a8e007020b31
Julien Dramaix <dramaix@google.com>
2025-07-30 14:40:15 -0700

    Make j2cl/release_prep.sh repo agnostic so we can reuse it in all our repos.
    PiperOrigin-RevId: 789049494

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -5,18 +5,24 @@
 set -euo pipefail
 
 tag="$1"
-# The prefix is used to determine the directory structure of the archive. We strip the 'v'
-# prefix from the version number.
-directory="j2cl-${tag#v}"
-archive="j2cl-${tag}.tar.gz"
+# GITHUB_REPOSITORY looks like user/repository_name, let strip the user part.
+repository=${GITHUB_REPOSITORY#*/}
+# The prefix is used to determine the directory structure of the archive. We
+# strip the 'v'prefix from the version number.
+directory="${repository}-${tag#v}"
+archive="${repository}-${tag}.tar.gz"
 
 git archive --format=tar --prefix=${directory}/ -o "${archive}" ${tag}
 
-# The stdout of this program will be used as the top of the release notes for this release.
+# Replace hyphens with underscores in the repository name to match our Bazel
+# module naming conventions.
+bazel_module_name="${repository//-/_}"
+# The stdout of this program will be used as the top of the release notes for
+# this release.
 cat << EOF
 ## Using Bazel 8 or later, add to your \`MODULE.bazel\` file:
 
 \`\`\`starlark
-bazel_dep(name = "j2cl", version = "${tag}")
+bazel_dep(name = "${bazel_module_name}", version = "${tag}")
 \`\`\`
 EOF
