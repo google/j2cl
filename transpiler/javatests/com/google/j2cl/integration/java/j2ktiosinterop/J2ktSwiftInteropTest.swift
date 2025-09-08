088ed63be0c117a6db30b77d9b6e3952df09cc26
Googler <noreply@google.com>
2025-09-08 11:50:03 -0700

    [J2KT] Fix broken `j2ktiosinterop` integration test.
    PiperOrigin-RevId: 804525697

--- transpiler/javatests/com/google/j2cl/integration/java/j2ktiosinterop/J2ktSwiftInteropTest.swift
+++ transpiler/javatests/com/google/j2cl/integration/java/j2ktiosinterop/J2ktSwiftInteropTest.swift
@@ -23,8 +23,7 @@ final class J2ktSwiftInteropTest: XCTestCase {
 
     obj = J2ktiosinteropDefaultNames()
     obj = J2ktiosinteropDefaultNames(int: 1)
-    // TODO(b/374280337): Should be J2ktiosinteropDefaultNames(int:with:)
-    obj = J2ktiosinteropDefaultNames(int: 1, withNSString: "")
+    obj = J2ktiosinteropDefaultNames(int: 1, with: "")
 
     obj.method()
     obj.booleanMethod(withBoolean: true)
