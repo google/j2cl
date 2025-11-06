google/j2cl

commit 265d32f5b058b91cfed0cb9e0afdb946f024ee1a

[J2KT] Remove underscore from `@ObjCName` on static fields.
PiperOrigin-RevId: 825155399

transpiler/java/com/google/j2cl/transpiler/backend/kotlin/ObjCNames.kt
@@ -222,7 +222,7 @@ internal val Variable.objCParameterName: String
   get() = "with$objCName"
 
 internal val FieldDescriptor.objCName: String
-  get() = name!!.objCName.escapeJ2ObjCKeyword.letIf(!isEnumConstant) { it + "_" }
+  get() = name!!.objCName.escapeJ2ObjCKeyword.letIf(!isEnumConstant && !isStatic) { it + "_" }
 
 internal fun MethodObjCNames.escapeObjCMethod(isConstructor: Boolean): MethodObjCNames =
   copy(

transpiler/javatests/com/google/j2cl/integration/java/j2ktiosinterop/J2ktObjCInteropTest.m
@@ -74,9 +74,9 @@
   i = obj.intField_;
   obj.intField_ = i;
 
-  i = J2ktJ2ktiosinteropDefaultNamesCompanion.shared.STATIC_FINAL_INT_FIELD_;
-  i = J2ktJ2ktiosinteropDefaultNamesCompanion.shared.staticIntField_;
-  J2ktJ2ktiosinteropDefaultNamesCompanion.shared.staticIntField_ = i;
+  i = J2ktJ2ktiosinteropDefaultNamesCompanion.shared.STATIC_FINAL_INT_FIELD;
+  i = J2ktJ2ktiosinteropDefaultNamesCompanion.shared.staticIntField;
+  J2ktJ2ktiosinteropDefaultNamesCompanion.shared.staticIntField = i;
 
   i = J2ktiosinteropDefaultNames_get_STATIC_FINAL_INT_FIELD();
   i = J2ktiosinteropDefaultNames_get_staticIntField();

commit b55852546002e25a881737921ae251b6e96b0620

[J2KT] Reproduce a problem with field and method name conflict in ObjC, introduced in https://github.com/google/j2cl/commit/265d32f5b058b91cfed0cb9e0afdb946f024ee1a.
PiperOrigin-RevId: 828923198

transpiler/java/com/google/j2cl/transpiler/backend/kotlin/ObjCNames.kt
@@ -225,6 +225,9 @@ internal val Variable.objCParameterName: String
   get() = "with$objCName"
 
 internal val FieldDescriptor.objCName: String
+  // TODO(b/458058192): Bring back rendering underscore for non-final static fields, because of ObjC
+  // name conflict between field and method of the same name. Static final fields should follow
+  // naming convention and use SNAKE_CASE, which should not conflict with methods.
   get() = name!!.objCName.escapeJ2ObjCKeyword.letIf(!isEnumConstant && !isStatic) { it + "_" }
 
 internal fun MethodObjCNames.escapeObjCMethod(isConstructor: Boolean): MethodObjCNames =