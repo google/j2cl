544fff904c92df06e9b968fc331e2f449a2c5232
Roberto Lublinerman <rluble@google.com>
2026-03-21 06:54:33 -0700

    Make `GwtIncompatibleStripperTests` easier to read.
    PiperOrigin-RevId: 887293240

--- tools/javatests/com/google/j2cl/tools/gwtincompatible/GwtIncompatibleStripperTest.java
+++ tools/javatests/com/google/j2cl/tools/gwtincompatible/GwtIncompatibleStripperTest.java
@@ -16,10 +16,12 @@
 package com.google.j2cl.tools.gwtincompatible;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.stream.Collectors.joining;
 import static org.junit.Assert.assertEquals;
 
-import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
+import java.util.function.Function;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -29,405 +31,317 @@ public class GwtIncompatibleStripperTest {
 
   @Test
   public void testNoProcess() {
-    String content = "public class Foo {}";
-    assertEquals(
-        content, GwtIncompatibleStripper.strip(content, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        public class Foo {}
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testNoProcessOtherIncompatible() {
-    String content = "@J2kIncompatible\npublic class Foo {}";
-    assertEquals(
-        content, GwtIncompatibleStripper.strip(content, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        @J2kIncompatible
+        public class Foo {}
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testNoProcessString() {
-    String content = "public class Foo {String a = \"@GwtIncompatible\");}";
-    assertEquals(
-        content, GwtIncompatibleStripper.strip(content, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        public class Foo {String a = "@GwtIncompatible");}
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testProcessClass() {
-    String before =
-        lines(
-            "import a.b.X;",
-            "@GwtIncompatible",
-            "public class Foo {",
-            "  public X m() {return null;}",
-            "}");
-    String after =
-        lines(
-            stripped("import a.b.X;"),
-            stripped("@GwtIncompatible"),
-            stripped("public class Foo {"),
-            stripped("  public X m() {return null;}"),
-            stripped("}"));
-    assertEquals(after, GwtIncompatibleStripper.strip(before, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        -import a.b.X;
+        -@GwtIncompatible
+        -public class Foo {
+        -  public X m() {return null;}
+        -}
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testProcessJ2ktIncompatibleClass() {
-    String before =
-        lines(
-            "import a.b.X;",
-            "@J2ktIncompatible",
-            "public class Foo {",
-            "  public X m() {return null;}",
-            "}");
-    String after =
-        lines(
-            stripped("import a.b.X;"),
-            stripped("@J2ktIncompatible"),
-            stripped("public class Foo {"),
-            stripped("  public X m() {return null;}"),
-            stripped("}"));
-    assertEquals(
-        after, GwtIncompatibleStripper.strip(before, ImmutableList.of("J2ktIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        -import a.b.X;
+        -@J2ktIncompatible
+        -public class Foo {
+        -  public X m() {return null;}
+        -}
+        """,
+        "J2ktIncompatible");
   }
 
   @Test
   public void testProcessMultipleAnnotationsPresent() {
-    String before =
-        lines(
-            "import a.b.X;",
-            "@GwtIncompatible",
-            "@J2ktIncompatible",
-            "public class Foo {",
-            "  public X m() {return null;}",
-            "}");
-    String after =
-        lines(
-            stripped("import a.b.X;"),
-            stripped("@GwtIncompatible"),
-            stripped("@J2ktIncompatible"),
-            stripped("public class Foo {"),
-            stripped("  public X m() {return null;}"),
-            stripped("}"));
-    assertEquals(
-        after, GwtIncompatibleStripper.strip(before, ImmutableList.of("J2ktIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        -import a.b.X;
+        -@GwtIncompatible
+        -@J2ktIncompatible
+        -public class Foo {
+        -  public X m() {return null;}
+        -}
+        """,
+        "J2ktIncompatible");
   }
 
   @Test
   public void testProcessMultipleAnnotationsToStrip() {
-    String before =
-        lines(
-            "import a.b.X;",
-            "import a.b.Y;",
-            "import a.b.Z;",
-            "public class Foo {",
-            "  @GwtIncompatible",
-            "  @J2ktIncompatible",
-            "  public X m() {return null;}",
-            "  @GwtIncompatible",
-            "  public Y j2ktOnly() {return null;}",
-            "  @J2ktIncompatible",
-            "  public Z gwtOnly() {return null;}",
-            "}");
-    String after =
-        lines(
-            stripped("import a.b.X;"),
-            stripped("import a.b.Y;"),
-            stripped("import a.b.Z;"),
-            "public class Foo {",
-            stripped("  @GwtIncompatible"),
-            stripped("  @J2ktIncompatible"),
-            stripped("  public X m() {return null;}"),
-            stripped("  @GwtIncompatible"),
-            stripped("  public Y j2ktOnly() {return null;}"),
-            stripped("  @J2ktIncompatible"),
-            stripped("  public Z gwtOnly() {return null;}"),
-            "}");
-    assertEquals(
-        after,
-        GwtIncompatibleStripper.strip(
-            before, ImmutableList.of("J2ktIncompatible", "GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        -import a.b.X;
+        -import a.b.Y;
+        -import a.b.Z;
+        public class Foo {
+        -  @GwtIncompatible
+        -  @J2ktIncompatible
+        -  public X m() {return null;}
+        -  @GwtIncompatible
+        -  public Y j2ktOnly() {return null;}
+        -  @J2ktIncompatible
+        -  public Z gwtOnly() {return null;}
+        }
+        """,
+        "J2ktIncompatible",
+        "GwtIncompatible");
   }
 
   @Test
   public void testProcessMethod() {
-    String before =
-        lines(
-            "import a.b.C;",
-            "import a.b.D;",
-            "public class Foo {",
-            "  public C m() {}",
-            "  @Nullable",
-            "  @GwtIncompatible",
-            "  public D n() {}",
-            "}");
-    String after =
-        lines(
-            "import a.b.C;",
-            stripped("import a.b.D;"),
-            "public class Foo {",
-            "  public C m() {}",
-            stripped("  @Nullable"),
-            stripped("  @GwtIncompatible"),
-            stripped("  public D n() {}"),
-            "}");
-    assertEquals(after, GwtIncompatibleStripper.strip(before, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        import a.b.C;
+        -import a.b.D;
+        public class Foo {
+          public C m() {}
+        -  @Nullable
+        -  @GwtIncompatible
+        -  public D n() {}
+        }
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testProcessField() {
-    String before =
-        lines(
-            "public class Foo {",
-            "  public String a;",
-            "  @GwtIncompatible",
-            "  public String b;",
-            "  public String c;",
-            "}");
-    String after =
-        lines(
-            "public class Foo {",
-            "  public String a;",
-            stripped("  @GwtIncompatible"),
-            stripped("  public String b;"),
-            "  public String c;",
-            "}");
-    assertEquals(after, GwtIncompatibleStripper.strip(before, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        public class Foo {
+          public String a;
+        -  @GwtIncompatible
+        -  public String b;
+          public String c;
+        }
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testProcessEnumType() {
-    String before = "@GwtIncompatible public enum Foo {}";
-    String after = stripped("@GwtIncompatible public enum Foo {}");
-    assertEquals(after, GwtIncompatibleStripper.strip(before, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        -@GwtIncompatible public enum Foo {}
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testProcessEnumConstant() {
-    String before = lines("public enum Foo {", "  A,", "  @GwtIncompatible", "  B,", "  C;", "}");
-    String after =
-        lines(
-            "public enum Foo {",
-            "  A,",
-            stripped("  @GwtIncompatible"),
-            stripped("  B,"),
-            "  C;",
-            "}");
-    assertEquals(after, GwtIncompatibleStripper.strip(before, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        public enum Foo {
+          A,
+        -  @GwtIncompatible
+        -  B,
+          C;
+         }
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testProcessAnnotationType() {
-    String before = "@GwtIncompatible public @interface Foo {}";
-    String after = stripped("@GwtIncompatible public @interface Foo {}");
-    assertEquals(after, GwtIncompatibleStripper.strip(before, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        -@GwtIncompatible public @interface Foo {}
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testProcessAnnotationMember() {
-    String before =
-        lines(
-            "public @interface Foo {",
-            "  public String m();",
-            "  @GwtIncompatible",
-            "  public String n();",
-            "}");
-    String after =
-        lines(
-            "public @interface Foo {",
-            "  public String m();",
-            stripped("  @GwtIncompatible"),
-            stripped("  public String n();"),
-            "}");
-    assertEquals(after, GwtIncompatibleStripper.strip(before, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        public @interface Foo {
+          public String m();
+        -  @GwtIncompatible
+        -  public String n();
+        }
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testProcessMultiple() {
-    String before =
-        lines(
-            "public class Foo {",
-            "  @GwtIncompatible",
-            "  public void m();",
-            "  public String c;",
-            "  @GwtIncompatible",
-            "  public void n();",
-            "  public void o();",
-            "  @GwtIncompatible",
-            "  private static class X {",
-            "    void q() {}",
-            "    @GwtIncompatible",
-            "    void r() {}",
-            "  }",
-            "  String s;",
-            "}");
-    String after =
-        lines(
-            "public class Foo {",
-            stripped("  @GwtIncompatible"),
-            stripped("  public void m();"),
-            "  public String c;",
-            stripped("  @GwtIncompatible"),
-            stripped("  public void n();"),
-            "  public void o();",
-            stripped("  @GwtIncompatible"),
-            stripped("  private static class X {"),
-            stripped("    void q() {}"),
-            stripped("    @GwtIncompatible"),
-            stripped("    void r() {}"),
-            stripped("  }"),
-            "  String s;",
-            "}");
-    assertEquals(after, GwtIncompatibleStripper.strip(before, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        public class Foo {
+        -  @GwtIncompatible
+        -  public void m();
+          public String c;
+        -  @GwtIncompatible
+        -  public void n();
+          public void o();
+        -  @GwtIncompatible
+        -  private static class X {
+        -    void q() {}
+        -    @GwtIncompatible
+        -    void r() {}
+        -  }
+          String s;
+        }
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testProcessUnrelatedAnnotations() {
-    String before =
-        lines(
-            "public class Foo {",
-            "  @GwtIncompatible",
-            "  @Override",
-            "  public void m() {}",
-            "  @Override",
-            "  @GwtIncompatible",
-            "  public void n() {}",
-            "}");
-    String after =
-        lines(
-            "public class Foo {",
-            stripped("  @GwtIncompatible"),
-            stripped("  @Override"),
-            stripped("  public void m() {}"),
-            stripped("  @Override"),
-            stripped("  @GwtIncompatible"),
-            stripped("  public void n() {}"),
-            "}");
-    assertEquals(after, GwtIncompatibleStripper.strip(before, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        public class Foo {
+        -  @GwtIncompatible
+        -  @Override
+        -  public void m() {}
+        -  @Override
+        -  @GwtIncompatible
+        -  public void n() {}
+        }
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testProcessJavadoc() {
-    String before =
-        lines(
-            "public class Foo {",
-            "  /**",
-            "   * doc",
-            "   */",
-            "  @GwtIncompatible",
-            "  public void m() {}",
-            "}");
-    String after =
-        lines(
-            "public class Foo {",
-            stripped("  /**"),
-            stripped("   * doc"),
-            stripped("   */"),
-            stripped("  @GwtIncompatible"),
-            stripped("  public void m() {}"),
-            "}");
-    assertEquals(after, GwtIncompatibleStripper.strip(before, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        public class Foo {
+        -  /**
+        -   * doc
+        -   */
+        -  @GwtIncompatible
+        -  public void m() {}
+        }
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testProcessMultiVariable() {
-    String before = lines("public class Foo {", "  @GwtIncompatible", "  public String a, b;", "}");
-    String after =
-        lines(
-            "public class Foo {",
-            stripped("  @GwtIncompatible"),
-            stripped("  public String a, b;"),
-            "}");
-    assertEquals(after, GwtIncompatibleStripper.strip(before, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        public class Foo {
+        -  @GwtIncompatible
+        -  public String a, b;
+        }
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testProcessMultiVariableJavadoc() {
-    String before =
-        lines(
-            "public class Foo {",
-            "  /**",
-            "   * doc",
-            "   */",
-            "  @GwtIncompatible",
-            "  public String a, b;",
-            "}");
-    String after =
-        lines(
-            "public class Foo {",
-            stripped("  /**"),
-            stripped("   * doc"),
-            stripped("   */"),
-            stripped("  @GwtIncompatible"),
-            stripped("  public String a, b;"),
-            "}");
-    assertEquals(after, GwtIncompatibleStripper.strip(before, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        public class Foo {
+        -  /**
+        -   * doc
+        -   */
+        -  @GwtIncompatible
+        -  public String a, b;
+        }
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testNestedComment() {
-    String before =
-        lines(
-            "public class Foo {",
-            "  public void m() {}",
-            "  @GwtIncompatible",
-            "  public void n() {foo(x /* the value of x */);}",
-            "}");
-    String after =
-        lines(
-            "public class Foo {",
-            "  public void m() {}",
-            stripped("  @GwtIncompatible"),
-            stripped("  public void n() {foo(x /* the value of x */);}"),
-            "}");
-    assertEquals(after, GwtIncompatibleStripper.strip(before, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        public class Foo {
+          public void m() {}
+        -  @GwtIncompatible
+        -  public void n() {foo(x /* the value of x */);}
+        }
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testTabs() {
-    String before =
-        lines(
-            "public class Foo {",
-            "  public A m() {}",
-            "  @GwtIncompatible",
-            "  \tpublic B n() {}",
-            "}");
-    String after =
-        lines(
-            "public class Foo {",
-            "  public A m() {}",
-            stripped("  @GwtIncompatible"),
-            "  \t" + stripped("public B n() {}"),
-            "}");
-    assertEquals(after, GwtIncompatibleStripper.strip(before, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        public class Foo {
+          public A m() {}
+        -  @GwtIncompatible
+        -  \tpublic B n() {}
+        }
+        """,
+        "GwtIncompatible");
   }
 
   @Test
   public void testMultibyteChars() {
-    String before =
-        lines(
-            "public class Foo {",
-            "  public A m() {}",
-            "  @GwtIncompatible",
-            "  //மெ.பை.",
-            "  public B n() {}",
-            "}");
-    String after =
-        lines(
-            "public class Foo {",
-            "  public A m() {}",
-            stripped("  @GwtIncompatible"),
-            stripped("  //மெ.பை."),
-            stripped("  public B n() {}"),
-            "}");
-    assertEquals(after, GwtIncompatibleStripper.strip(before, ImmutableList.of("GwtIncompatible")));
+    assertAnnotatedCodeStripped(
+        """
+        public class Foo {
+          public A m() {}
+        -  @GwtIncompatible
+        -  //மெ.பை.
+        -  public B n() {}
+        }
+        """,
+        "GwtIncompatible");
   }
 
-  private static String lines(String... lines) {
-    return LINE_JOINER.join(lines);
+  private static void assertAnnotatedCodeStripped(String code, String... annotations) {
+    assertEquals(
+        after(code),
+        GwtIncompatibleStripper.strip(before(code), ImmutableList.copyOf(annotations)));
   }
 
-  private static String stripped(String lineContent) {
+  private static String before(String code) {
+    return transformMarkedLines(code, Function.identity());
+  }
+
+  private static String after(String code) {
+    return transformMarkedLines(code, GwtIncompatibleStripperTest::stripLine);
+  }
+
+  private static String transformMarkedLines(String code, Function<String, String> transformer) {
+    return Splitter.on('\n')
+        .splitToStream(code)
+        .map(
+            line ->
+                !line.isEmpty() && line.charAt(0) == '-'
+                    ? transformer.apply(line.substring(1))
+                    : line)
+        .collect(joining("\n"));
+  }
+
+  private static String stripLine(String lineContent) {
     checkArgument(
-        !lineContent.contains("\n"), "stripped() should only be called with one line at a time.");
-    return " ".repeat(lineContent.length());
+        !lineContent.contains("\n"), "strip() should only be called with one line at a time.");
+    return lineContent.replaceAll("[^\\s]", " ");
   }
-
-  private static final Joiner LINE_JOINER = Joiner.on('\n');
 }
