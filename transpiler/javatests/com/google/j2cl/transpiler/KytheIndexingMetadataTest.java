faff3f5067d3449f7536688d1f62c95506a5b246
Kevin O'Connor <kevinoconnor@google.com>
2026-04-23 11:12:01 -0700

    Add tests for asserting specific Kythe ranges are being renerated
    Besides just asserting that the kythe metadata exists, we can now also test that
    specific anchor ranges are mapped.
    
    I've also added repro of us incorrectly mapping ranges when non-ASCII characters
    are present in the source.
    
    PiperOrigin-RevId: 904545226

--- transpiler/javatests/com/google/j2cl/transpiler/KytheIndexingMetadataTest.java
+++ transpiler/javatests/com/google/j2cl/transpiler/KytheIndexingMetadataTest.java
@@ -15,40 +15,106 @@
  */
 package com.google.j2cl.transpiler;
 
+import static com.google.common.truth.Truth.assertThat;
 import static com.google.j2cl.transpiler.TranspilerTester.newTesterWithDefaults;
 
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import com.google.j2cl.transpiler.TranspilerTester.TranspileResult;
 import java.util.List;
-import junit.framework.TestCase;
+import java.util.regex.Pattern;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-public class KytheIndexingMetadataTest extends TestCase {
+@RunWith(JUnit4.class)
+public class KytheIndexingMetadataTest {
+
+  private static final Pattern PATH_PATTERN = Pattern.compile("\"path\":\"([^\"]+)\"");
+
+  @Test
   public void testKytheIndexingMetadata() throws Exception {
     TranspileResult result =
         newTesterWithDefaults()
             .addFile(
                 "test/KytheIndexingMetadata.java",
-                "package test; public class KytheIndexingMetadata {}")
+                """
+                package test;
+                public class KytheIndexingMetadata {}
+                """)
             .addArgs("-generatekytheindexingmetadata")
             .assertTranspileSucceeds()
             .assertNoWarnings();
 
-    assertLinesContainsKytheMetadata(result.getOutputSource("test/KytheIndexingMetadata.java.js"));
-    assertLinesContainsKytheMetadata(
-        result.getOutputSource("test/KytheIndexingMetadata.impl.java.js"));
+    assertKytheMetadata(result.getOutputSource("test/KytheIndexingMetadata.java.js"));
+
+    String kytheMetadata =
+        assertKytheMetadata(result.getOutputSource("test/KytheIndexingMetadata.impl.java.js"));
+
+    // Ensures there's an anchor on the name of the class declaration.
+    assertImputesEdgeExists(
+        kytheMetadata,
+        /* sourceBegin= */ 27,
+        /* sourceEnd= */ 48,
+        /* targetBegin= */ 171,
+        /* targetEnd= */ 192);
   }
 
-  private void assertLinesContainsKytheMetadata(List<String> lines) {
-    assertTrue(lines.size() > 1);
-    assertTrue(lines.get(lines.size() - 2).equals("// Kythe Indexing Metadata:"));
-    String kytheMetadataLine = lines.get(lines.size() - 1);
-    assertTrue(
-        kytheMetadataLine.startsWith(
-            "// {\"type\":\"KYTHE0\",\"meta\":[{\"type\":\"ANCHOR_ANCHOR\",\"source_begin\":"));
-    assertTrue(
-        kytheMetadataLine.contains(
-            "\"edge\":\"/kythe/edge/imputes\",\"source_vname\":{\"corpus\":\"google3\",\"path\":"));
-    assertTrue(kytheMetadataLine.contains("\"source_end\":"));
-    assertTrue(kytheMetadataLine.contains("\"target_begin\":"));
-    assertTrue(kytheMetadataLine.contains("\"target_end\":"));
+  @Test
+  public void testKytheIndexingMetadata_multibyteChars() throws Exception {
+    TranspileResult result =
+        newTesterWithDefaults()
+            .addFile(
+                "test/KytheIndexingMetadata.java",
+                """
+                package test;
+                // °
+                public class KytheIndexingMetadata {}
+                """)
+            .addArgs("-generatekytheindexingmetadata")
+            .assertTranspileSucceeds()
+            .assertNoWarnings();
+
+    String kytheMetadata =
+        assertKytheMetadata(result.getOutputSource("test/KytheIndexingMetadata.impl.java.js"));
+
+    // Ensures there's an anchor on the name of the class declaration.
+    assertImputesEdgeExists(
+        kytheMetadata,
+        // TODO(b/505491718): This source range is off by one due the earlier ° character occupying
+        // two bytes in the source file.
+        /* sourceBegin= */ 32,
+        /* sourceEnd= */ 53,
+        /* targetBegin= */ 171,
+        /* targetEnd= */ 192);
+  }
+
+  @CanIgnoreReturnValue
+  private static String assertKytheMetadata(List<String> lines) {
+    assertThat(lines.size()).isGreaterThan(1);
+    assertThat(lines.get(lines.size() - 2)).isEqualTo("// Kythe Indexing Metadata:");
+    String kytheMetadataLine = lines.getLast();
+    assertThat(kytheMetadataLine).startsWith("// {\"type\":\"KYTHE0\"");
+    // return the JSON string without the "// " prefix.
+    String kytheMedataJson = kytheMetadataLine.substring(3);
+    // Snip out paths as they are not stable across different runs/evironments.
+    return PATH_PATTERN.matcher(kytheMedataJson).replaceAll("\"path\":\"<snip>\"");
+  }
+
+  private static void assertImputesEdgeExists(
+      String actualMetadata, int sourceBegin, int sourceEnd, int targetBegin, int targetEnd) {
+    String expectedAnchor =
+        String.format(
+            """
+            {\
+            "type":"ANCHOR_ANCHOR",\
+            "source_begin":%d,\
+            "source_end":%d,\
+            "target_begin":%d,\
+            "target_end":%d,\
+            "edge":"/kythe/edge/imputes",\
+            "source_vname":{"corpus":"google3","path":"<snip>"}}\
+            """,
+            sourceBegin, sourceEnd, targetBegin, targetEnd);
+    assertThat(actualMetadata).contains(expectedAnchor);
   }
 }
