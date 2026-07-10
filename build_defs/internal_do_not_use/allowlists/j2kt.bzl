google/j2cl

commit 14b894ea8070be7db0fd0e9ab55a0a8b6d174b65

[J2KT] Replace `j2kt_emit_actual_visibility` flag with `EMIT_RELAXED_VISIBILITY_ALLOWLIST`.
This is now possible, since by default everything is built with actual visibilities, except for the exclusion list in `Visibilities.kt`. The flag is applied to `j2cl` sources.

PiperOrigin-RevId: 945732079

build_defs/internal_do_not_use/allowlists/j2kt.bzl
@@ -4,6 +4,46 @@ load(":allowlists.bzl", "allowlists")
 
 visibility(["//build_defs/internal_do_not_use/..."])
 
+# Labels which will have relaxed visibility emitted from J2KT:
+# - non-public types will be made public
+# - protected methods and overrides will be made public
+EMIT_RELAXED_VISIBILITY_ALLOWLIST = allowlists.of_packages([
+    # go/keep-sorted start
+    "//jre/java",
+    "//jre/javatests",
+    "//samples/box2d/src/main/java",
+    "//transpiler/javatests/com/google/j2cl/integration/java/allsimplebridges",
+    "//transpiler/javatests/com/google/j2cl/integration/java/autovalue",
+    "//transpiler/javatests/com/google/j2cl/integration/java/backwardbridgemethod",
+    "//transpiler/javatests/com/google/j2cl/integration/java/cyclicclinits",
+    "//transpiler/javatests/com/google/j2cl/integration/java/innerclassinitorder",
+    "//transpiler/javatests/com/google/j2cl/integration/java/instanceinnerclass",
+    "//transpiler/javatests/com/google/j2cl/integration/java/j2kt/relaxedvisibilitydep",
+    "//transpiler/javatests/com/google/j2cl/integration/java/jsbridgebackward",
+    "//transpiler/javatests/com/google/j2cl/integration/java/jsbridgemultipleaccidental",
+    "//transpiler/javatests/com/google/j2cl/integration/java/jsbridgemultipleexposing",
+    "//transpiler/javatests/com/google/j2cl/integration/java/multipleroottypes",
+    "//transpiler/javatests/com/google/j2cl/readable/java/accidentaloverride",
+    "//transpiler/javatests/com/google/j2cl/readable/java/autovalue",
+    "//transpiler/javatests/com/google/j2cl/readable/java/bridgemethods",
+    "//transpiler/javatests/com/google/j2cl/readable/java/cast",
+    "//transpiler/javatests/com/google/j2cl/readable/java/genericanddefaultmethods",
+    "//transpiler/javatests/com/google/j2cl/readable/java/genericmethod",
+    "//transpiler/javatests/com/google/j2cl/readable/java/gwtincompatible",
+    "//transpiler/javatests/com/google/j2cl/readable/java/innerclassinheritance",
+    "//transpiler/javatests/com/google/j2cl/readable/java/innerclassinitorder",
+    "//transpiler/javatests/com/google/j2cl/readable/java/j2kt/relaxedvisibilitydep",
+    "//transpiler/javatests/com/google/j2cl/readable/java/jsbridgebackward",
+    "//transpiler/javatests/com/google/j2cl/readable/java/multipletopclasses",
+    "//transpiler/javatests/com/google/j2cl/readable/java/qualifiedsupercall",
+    "//transpiler/javatests/com/google/j2cl/readable/java/staticfieldimport",
+    "//transpiler/javatests/com/google/j2cl/readable/java/subclassgenericclass",
+    "//transpiler/javatests/com/google/j2cl/readable/java/subnativejstype",
+    "//transpiler/javatests/com/google/j2cl/readable/java/supercallnondefault",
+    "//transpiler/javatests/com/google/j2cl/readable/java/supermethodcall",
+    # go/keep-sorted end
+])
+
 EXEMPT_FROM_NULLMARKED_ALLOWLIST = allowlists.of_packages([
     "//jre/javatests",
     "//junit/...",

transpiler/java/com/google/j2cl/transpiler/backend/kotlin/Visibilities.kt
@@ -17,36 +17,15 @@ package com.google.j2cl.transpiler.backend.kotlin
 
 import com.google.j2cl.transpiler.ast.MemberDescriptor
 import com.google.j2cl.transpiler.ast.TypeDeclaration
+import java.lang.Boolean.getBoolean
+
+/** Whether to emit relaxed visibility to workaround Kotlin/Java visibility rule differences. */
+val emitRelaxedVisibility: Boolean =
+  getBoolean("com.google.j2cl.transpiler.backend.kotlin.emitRelaxedVisibility")
 
 /** Set containing packages for which J2KT should not translate actual visibility. */
 private val EXCLUDED_VISIBILITY_PACKAGES: Set<String> =
   setOf(
-    // Packages from excluded readable/integration tests.
-    // TODO(b/206898384): Implement j2kt_emit_relaxed_visibility flag to avoid this list.
-    "accidentaloverride",
-    "autovalue",
-    "backwardbridgemethod",
-    "bridgemethods",
-    "cast",
-    "cyclicclinits",
-    "genericanddefaultmethods",
-    "genericmethod",
-    "gwtincompatible",
-    "innerclassinheritance",
-    "innerclassinitorder",
-    "instanceinnerclass",
-    "j2kt.relaxedvisibility",
-    "jsbridgebackward",
-    "jsbridgemultipleaccidental",
-    "jsbridgemultipleexposing",
-    "multipleroottypes",
-    "multipletopclasses",
-    "qualifiedsupercall",
-    "staticfieldimport.staticimports",
-    "subclassgenericclass",
-    "subnativejstype",
-    "supercallnondefault",
-    "supermethodcall",
   )
 
 /** List containing package prefixes for which J2KT should not translate actual visibility. */
@@ -55,18 +34,18 @@ private val EXCLUDE_VISIBILITY_PACKAGE_PREFIXES: List<String> =
   listOf(
   )
 
-private val TypeDeclaration.useActualKtVisibilityForPackage: Boolean
-  get() =
-    packageName !in EXCLUDED_VISIBILITY_PACKAGES &&
-      packageName.plus(".").let { packageNamePlusDot ->
-        EXCLUDE_VISIBILITY_PACKAGE_PREFIXES.none { packageNamePlusDot.startsWith(it) }
-      }
+private fun useActualKtVisibilityForPackage(packageName: String): Boolean =
+  packageName !in EXCLUDED_VISIBILITY_PACKAGES &&
+    packageName.plus(".").let { packageNamePlusDot ->
+      EXCLUDE_VISIBILITY_PACKAGE_PREFIXES.none { packageNamePlusDot.startsWith(it) }
+    }
 
 /** Returns whether J2KT should translate actual visibility for the given type declaration. */
 // TODO(b/206898384): Remove this once the bug is fixed.
 internal val TypeDeclaration.useActualKtVisibility: Boolean
   get() =
-    useActualKtVisibilityForPackage &&
+    !emitRelaxedVisibility &&
+      useActualKtVisibilityForPackage(packageName) &&
       (!isAutoConverter || hasAutoValueOrBuilderSuperType) &&
       !isVisibilityWarningSuppressed
 

commit d2f75a95fb7c7933f499b26660e0a1c6bcb8eec7

[J2KT] Fix octane benchmark build broken by https://github.com/google/j2cl/commit/14b894ea8070be7db0fd0e9ab55a0a8b6d174b65.
PiperOrigin-RevId: 945862560

build_defs/internal_do_not_use/allowlists/j2kt.bzl
@@ -41,6 +41,7 @@ EMIT_RELAXED_VISIBILITY_ALLOWLIST = allowlists.of_packages([
     "//transpiler/javatests/com/google/j2cl/readable/java/subnativejstype",
     "//transpiler/javatests/com/google/j2cl/readable/java/supercallnondefault",
     "//transpiler/javatests/com/google/j2cl/readable/java/supermethodcall",
+    "//third_party/java_src/xplat/kmpbench/java",
     # go/keep-sorted end
 ])
 