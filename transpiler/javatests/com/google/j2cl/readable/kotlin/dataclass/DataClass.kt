2032f03867a2f294235bebdd293c7a276515b816
Goktug Gokdogan <goktug@google.com>
2026-09-23 17:44:44 -0700

    [KT] Add readable test for body properties in @JvmRecord data classes.
    Covers body properties defined inside a `@JvmRecord` data class, including properties that override interface properties and `is`-prefixed properties.
    
    PiperOrigin-RevId: 987122279

--- transpiler/javatests/com/google/j2cl/readable/kotlin/dataclass/DataClass.kt
+++ transpiler/javatests/com/google/j2cl/readable/kotlin/dataclass/DataClass.kt
@@ -51,14 +51,34 @@ data class IntValueHolder(override val backingValue: Int) : ValueHolder<Int>
 
 data class ArrayMembers(private val a: IntArray, private val b: Array<String>)
 
+// Both actual component properties and computed properties are special treated in Kotlin to follow
+// record naming convention (e.g. f()).
+// However they still override parent properties which have regular getter name pattern (e.g.
+// getF()) as a result they require a bridge.
+// Note that generally in Kotlin explicit getter functions (e.g. f() or getF()) are not overridable
+// by subclass properties even if the generated getter name match - will be rejected as
+// "declarations having the same JVM signature".
 interface DataClassInterface {
-  val f: Int
+  val f: Int // overridden by component which requires getF() bridge.
+  val other: String // overridden by computed property which requires getOther() bridge.
+  val isBar: Any // overridden by computed property but "is" methods doesn't require bridge.
 
-  fun component2(): String
+  fun getZ(): Int = 42 // Note that this is NOT overridden by "val z: String" component.
+
+  fun component2(): String // overridden by synthetic Data.component2() method.
 }
 
 @kotlin.jvm.JvmRecord
 data class JvmRecordDataClass(override val f: Int, val z: String) : DataClassInterface {
+  override val other: String
+    get() = "other"
+
+  override val isBar: String
+    get() = "bar"
+
+  val other2: String
+    get() = "other2"
+
   companion object {
     @kotlin.jvm.JvmField val staticField = 1
   }
